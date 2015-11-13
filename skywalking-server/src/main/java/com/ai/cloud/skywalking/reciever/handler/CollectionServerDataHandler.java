f8ccbef2b16b74edbd7867c4c4a9ad793b3e1b04
zhangxin10 <curtain.xin@gmail.com>
2015-11-13 22:26:30 +0800

    同上追加

--- skywalking-server/src/main/java/com/ai/cloud/skywalking/reciever/handler/CollectionServerDataHandler.java
+++ skywalking-server/src/main/java/com/ai/cloud/skywalking/reciever/handler/CollectionServerDataHandler.java
@@ -1,12 +1,13 @@
 package com.ai.cloud.skywalking.reciever.handler;
 
 import com.ai.cloud.skywalking.reciever.buffer.DataBufferThreadContainer;
+import com.ai.cloud.skywalking.reciever.util.ByteArrayUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class CollectionServerDataHandler extends SimpleChannelInboundHandler {
+public class CollectionServerDataHandler extends SimpleChannelInboundHandler<byte[]> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        DataBufferThreadContainer.getDataBufferThread().doCarry(msg.toString().getBytes());
+    protected void channelRead0(ChannelHandlerContext ctx, byte[] msg) throws Exception {
+        DataBufferThreadContainer.getDataBufferThread().doCarry(msg);
     }
 }
