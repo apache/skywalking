1b98a8cf9a3d6586bd1434496f88d68719b442af
wusheng <wu.sheng@foxmail.com>
2015-11-16 20:47:40 +0800

    1.提交可追踪的JDBC程序 2.使用com.ai.cloud.skywalking.plugin.jdbc.mysql.MySQLTracingDriver,用于支持mysql的jdbc驱动，代替com.mysql.jdbc.Driver 注：部分JDBC代码未完成，待后续提交

---
+++ skywalking-sdk-plugin/jdbc-plugin/src/main/java/com/ai/cloud/skywalking/plugin/jdbc/tracing/StatementTracing.java
@@ -0,0 +1,42 @@
+package com.ai.cloud.skywalking.plugin.jdbc.tracing;
+
+import java.sql.SQLException;
+
+import com.ai.cloud.skywalking.buriedpoint.RPCBuriedPointSender;
+import com.ai.cloud.skywalking.model.Identification;
+
+/**
+ * 连接级追踪，用于追踪用于Statement的操作追踪
+ * 
+ * @author wusheng
+ *
+ */
+public class StatementTracing {
+	private static RPCBuriedPointSender sender = new RPCBuriedPointSender();
+
+	public static <R> R execute(java.sql.Statement realStatement,
+			String connectInfo, String method, String sql, Executable<R> exec)
+			throws SQLException {
+		try {
+			sender.beforeSend(Identification
+					.newBuilder()
+					.viewPoint(connectInfo)
+					.businessKey(
+							"statement."
+									+ method
+									+ (sql == null || sql.length() == 0 ? ""
+											: ":" + sql)).spanType('J').build());
+			return exec.exe(realStatement, sql);
+		} catch (SQLException e) {
+			sender.handleException(e);
+			throw e;
+		} finally {
+			sender.afterSend();
+		}
+	}
+
+	public interface Executable<R> {
+		public R exe(java.sql.Statement realStatement, String sql)
+				throws SQLException;
+	}
+}
