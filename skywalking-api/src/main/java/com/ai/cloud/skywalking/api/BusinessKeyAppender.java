77f71d5cbbddcd4471a59a43e5c6e75af8878d40
wusheng <wu.sheng@foxmail.com>
2015-11-17 21:21:17 +0800

    1.修复设置业务字段的方法 2.增加获取TraceId的方法

--- skywalking-api/src/main/java/com/ai/cloud/skywalking/api/BusinessKeyAppender.java
+++ skywalking-api/src/main/java/com/ai/cloud/skywalking/api/BusinessKeyAppender.java
@@ -1,26 +1,28 @@
-package com.ai.cloud.skywalking.util;
+package com.ai.cloud.skywalking.api;
 
 import com.ai.cloud.skywalking.conf.AuthDesc;
 import com.ai.cloud.skywalking.conf.Config;
 import com.ai.cloud.skywalking.context.Context;
 import com.ai.cloud.skywalking.context.Span;
 
-public final class BusinessKeyTracing {
-
+public final class BusinessKeyAppender {
     private static final char spiltChar = '^';
 
-
-    private BusinessKeyTracing() {
+    private BusinessKeyAppender() {
         // Non
     }
 
+    /**
+     * 在当前埋点上下文中设置业务级信息
+     * 
+     * @param businessKey
+     */
     public static void trace(String businessKey) {
-
         if (!AuthDesc.isAuth())
             return;
 
         Span spanData = Context.getLastSpan();
-        if (spanData != null) {
+        if (spanData == null) {
             return;
         }
         spanData.setBusinessKey(businessKey.replace('-', spiltChar).substring(0, Config.BusinessKey.MAX_LENGTH));
---
+++ skywalking-api/src/main/java/com/ai/cloud/skywalking/api/Tracing.java
@@ -0,0 +1,23 @@
+package com.ai.cloud.skywalking.api;
+
+import com.ai.cloud.skywalking.conf.AuthDesc;
+import com.ai.cloud.skywalking.context.Context;
+import com.ai.cloud.skywalking.context.Span;
+
+public class Tracing {
+	/**
+	 * 获取当前上下文中的TraceId
+	 * @return
+	 */
+	public static String getTraceId(){
+		if (!AuthDesc.isAuth())
+            return "";
+
+        Span spanData = Context.getLastSpan();
+        if (spanData == null) {
+            return "";
+        }
+        
+        return spanData.getTraceId();
+	}
+}
