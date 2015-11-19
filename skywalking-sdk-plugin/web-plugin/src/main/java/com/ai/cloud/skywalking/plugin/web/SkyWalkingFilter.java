c2fb772e625023f19bdb2e948ec68c3adf767a25
zhangxin10 <curtain.xin@gmail.com>
2015-11-19 21:10:52 +0800

    提交web请求埋点

---
+++ skywalking-sdk-plugin/web-plugin/src/main/java/com/ai/cloud/skywalking/plugin/web/SkyWalkingFilter.java
@@ -0,0 +1,56 @@
+package com.ai.cloud.skywalking.plugin.web;
+
+
+import com.ai.cloud.skywalking.buriedpoint.RPCBuriedPointReceiver;
+import com.ai.cloud.skywalking.model.ContextData;
+import com.ai.cloud.skywalking.model.Identification;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SkyWalkingFilter implements Filter {
+
+    private String tracingName;
+    private static final String DEFAULT_TRACE_NAME = "SkyWalking-TRACING-NAME";
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        tracingName = filterConfig.getInitParameter("tracing-name");
+        if (tracingName == null || tracingName.length() <= 0) {
+            tracingName = DEFAULT_TRACE_NAME;
+        }
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        RPCBuriedPointReceiver receiver = null;
+        try {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            String contextDataStr = request.getHeader(tracingName);
+            ContextData contextData = null;
+            if (contextDataStr != null && contextDataStr.length() > 0) {
+                contextData = new ContextData(contextDataStr);
+            }
+            receiver = new RPCBuriedPointReceiver();
+            receiver.beforeReceived(contextData, generateIdentification(request));
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (Throwable e) {
+            receiver.handleException(e);
+            throw new ServletException(e);
+        } finally {
+            receiver.afterReceived();
+        }
+
+    }
+
+
+    private Identification generateIdentification(HttpServletRequest request) {
+        return Identification.newBuilder()
+                .viewPoint(request.getRequestURL().toString())
+                .spanType('W')
+                .build();
+    }
+
+    public void destroy() {
+        // do-nothing
+    }
+}
