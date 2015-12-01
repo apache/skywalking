d19b0fe709c18cd96c54300e5987585810a5724f
zhangxin10 <curtain.xin@gmail.com>
2015-12-01 12:56:59 +0800

    新增HttpClient插件

---
+++ skywalking-sdk-plugin/httpclient-plugin/src/main/java/com/ai/cloud/skywalking/plugin/httpclient/SWTracingHttpClient.java
@@ -0,0 +1,132 @@
+package com.ai.cloud.skywalking.plugin.httpclient;
+
+import com.ai.cloud.skywalking.plugin.httpclient.trace.HttpClientTracing;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+
+public class SWTracingHttpClient implements HttpClient {
+
+    private static final String DEFAULT_TRACE_NAME = "SkyWalking-TRACING-NAME";
+
+    private HttpClient client;
+    private String traceName;
+
+    public SWTracingHttpClient(HttpClient client, String traceName) {
+        this.client = client;
+        this.traceName = traceName;
+    }
+
+    public SWTracingHttpClient(String traceName) {
+        super();
+        this.traceName = traceName;
+    }
+
+    public SWTracingHttpClient() {
+        client = new DefaultHttpClient();
+        this.traceName = DEFAULT_TRACE_NAME;
+    }
+
+    @Override
+    public HttpParams getParams() {
+        return client.getParams();
+    }
+
+    @Override
+    public ClientConnectionManager getConnectionManager() {
+        return client.getConnectionManager();
+    }
+
+    @Override
+    public HttpResponse execute(final HttpUriRequest httpUriRequest) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpUriRequest.getURI().toString(), traceName, httpUriRequest, new HttpClientTracing.Executor<HttpResponse>() {
+            @Override
+            public HttpResponse execute() throws IOException {
+                return client.execute(httpUriRequest);
+            }
+        });
+    }
+
+    @Override
+    public HttpResponse execute(final HttpUriRequest httpUriRequest, final HttpContext httpContext) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpUriRequest.getURI().toString(), traceName, httpUriRequest, new HttpClientTracing.Executor<HttpResponse>() {
+            @Override
+            public HttpResponse execute() throws IOException {
+                return client.execute(httpUriRequest, httpContext);
+            }
+        });
+    }
+
+    @Override
+    public HttpResponse execute(final HttpHost httpHost, final HttpRequest httpRequest) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpHost.toURI(), traceName, httpRequest, new HttpClientTracing.Executor<HttpResponse>() {
+            @Override
+            public HttpResponse execute() throws IOException {
+                return client.execute(httpHost, httpRequest);
+            }
+        });
+    }
+
+    @Override
+    public HttpResponse execute(final HttpHost httpHost, final HttpRequest httpRequest, final HttpContext httpContext) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpHost.toURI(), traceName, httpRequest, new HttpClientTracing.Executor<HttpResponse>() {
+            @Override
+            public HttpResponse execute() throws IOException {
+                return client.execute(httpHost, httpRequest, httpContext);
+            }
+        });
+    }
+
+    @Override
+    public <T> T execute(final HttpUriRequest httpUriRequest, final ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpUriRequest.getURI().toString(), traceName, httpUriRequest, new HttpClientTracing.Executor<T>() {
+            @Override
+            public T execute() throws IOException {
+                return client.execute(httpUriRequest, responseHandler);
+            }
+        });
+
+    }
+
+    @Override
+    public <T> T execute(final HttpUriRequest httpUriRequest, final ResponseHandler<? extends T> responseHandler, final HttpContext httpContext) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpUriRequest.getURI().toString(), traceName, httpUriRequest, new HttpClientTracing.Executor<T>() {
+            @Override
+            public T execute() throws IOException {
+                return client.execute(httpUriRequest, responseHandler, httpContext);
+            }
+        });
+
+    }
+
+    @Override
+    public <T> T execute(final HttpHost httpHost, final HttpRequest httpRequest, final ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpHost.toURI(), traceName, httpRequest, new HttpClientTracing.Executor<T>() {
+            @Override
+            public T execute() throws IOException {
+                return client.execute(httpHost, httpRequest, responseHandler);
+            }
+        });
+
+    }
+
+    @Override
+    public <T> T execute(final HttpHost httpHost, final HttpRequest httpRequest, final ResponseHandler<? extends T> responseHandler, HttpContext httpContext) throws IOException, ClientProtocolException {
+        return HttpClientTracing.execute(httpHost.toURI(), traceName, httpRequest, new HttpClientTracing.Executor<T>() {
+            @Override
+            public T execute() throws IOException {
+                return client.execute(httpHost, httpRequest, responseHandler);
+            }
+        });
+    }
+}
---
+++ skywalking-sdk-plugin/httpclient-plugin/src/main/java/com/ai/cloud/skywalking/plugin/httpclient/trace/HttpClientTracing.java
@@ -0,0 +1,35 @@
+package com.ai.cloud.skywalking.plugin.httpclient.trace;
+
+import com.ai.cloud.skywalking.buriedpoint.RPCBuriedPointSender;
+import com.ai.cloud.skywalking.model.Identification;
+import org.apache.http.HttpRequest;
+
+import java.io.IOException;
+
+public class HttpClientTracing {
+
+    private static RPCBuriedPointSender sender = new RPCBuriedPointSender();
+
+    public static <R> R execute(String url, String traceHearName, HttpRequest httpRequest, Executor<R> executor) throws IOException {
+        try {
+            httpRequest.setHeader(traceHearName,
+                    sender.beforeSend(Identification.newBuilder()
+                            .viewPoint(url)
+                            .spanType('W')
+                            .build())
+                            .toString());
+            return executor.execute();
+        } catch (IOException e) {
+            sender.handleException(e);
+            throw e;
+        } finally {
+            sender.afterSend();
+        }
+    }
+
+    public interface Executor<R> {
+        R execute() throws IOException;
+    }
+
+
+}
