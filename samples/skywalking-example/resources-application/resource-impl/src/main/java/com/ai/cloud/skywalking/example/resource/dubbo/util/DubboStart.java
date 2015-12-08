2bc7da01a1908464340c4702c14c57a1e78e00e4
zhangxin10 <curtain.xin@gmail.com>
2015-12-08 14:07:32 +0800

    删除无用的工程，这个工程已经移到sample文件夹底下

--- samples/skywalking-example/resources-application/resource-impl/src/main/java/com/ai/cloud/skywalking/example/resource/dubbo/util/DubboStart.java
+++
@@ -1,8 +0,0 @@
-package com.ai.cloud.skywalking.example.resource.dubbo.util;
-
-public class DubboStart {
-
-    public static void main(String[] args) {
-
-    }
-}
--- skywalking-example/web-application/src/main/java/com/ai/cloud/skywalking/example/web/SaveOrderServlet.java
+++
@@ -1,7 +0,0 @@
-package com.ai.cloud.skywalking.example.web;
-
-/**
- * Created by astraea on 2015/11/26.
- */
-public class SaveOrderServlet {
-}
