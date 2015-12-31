167b92f67288d84a663eef139f2ce05268f24cc9
zhangxin10 <curtain.xin@gmail.com>
2015-12-31 13:13:48 +0800

    上传pom.xml文件以及测试代码

---
+++ skywalking-log/log4j-1.x-plugin/src/test/java/com/ai/TestLog.java
@@ -0,0 +1,10 @@
+package com.ai;
+
+import org.apache.log4j.Logger;
+
+public class TestLog {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(TestLog.class);
+        logger.info("TTTTTTTTTTTTTTTTTTTTTTTT");
+    }
+}
