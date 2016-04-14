8f5fcc7d61ef4437056e74364ef963e990984ac0
wusheng <wu.sheng@foxmail.com>
2016-04-14 09:51:04 +0800

    修复原有的方法入参为null时，spring方法拦截错误的问题。感觉 @汽车之家_泉 的异常反馈。

--- skywalking-sdk-plugin/spring-plugin/src/test/java/com/ai/cloud/skywalking/plugin/spring/test/TestBean.java
+++ skywalking-sdk-plugin/spring-plugin/src/test/java/com/ai/cloud/skywalking/plugin/spring/test/TestBean.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+//@Transactional
 public class TestBean/* implements TestInterface*/ {
     private String value;
 
