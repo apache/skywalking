59e2f69d357782cd0de5598ccf73ebf1f77d392a
ascrutae <ascrutae@gmail.com>
2016-06-05 11:21:56 +0800

    1.添加redis测试工程 2. 移除log4j shade

---
+++ test/skywalking-agent-test/redis-test/src/main/java/com/ai/cloud/skywalking/agent/test/RedisPluginTest.java
@@ -0,0 +1,7 @@
+package com.ai.cloud.skywalking.agent.test;
+
+public class RedisPluginTest {
+    public static void main(String[] args) {
+        RedisOperator.setData("key1", "value1");
+    }
+}
