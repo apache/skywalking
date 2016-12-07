dd2484477c9ba17f45b54604038b2d1c647d1967
wusheng <wu.sheng@foxmail.com>
2016-12-07 09:46:53 +0800

    Webui use register center to find routing server. and use searchClient to search traceId. Waiting for integration test.

--- skywalking-webui/src/main/java/com/a/eye/skywalking/web/client/routing/SearchClient.java
+++ skywalking-webui/src/main/java/com/a/eye/skywalking/web/client/routing/SearchClient.java
@@ -1,8 +1,13 @@
 package com.a.eye.skywalking.web.client.routing;
 
+import com.a.eye.skywalking.network.grpc.QueryTask;
+import com.a.eye.skywalking.network.grpc.SearchResult;
+import com.a.eye.skywalking.network.grpc.TraceId;
 import com.a.eye.skywalking.network.grpc.client.TraceSearchClient;
+import com.a.eye.skywalking.web.dto.TraceNodeInfo;
 import com.a.eye.skywalking.web.dto.TraceNodesResult;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -13,15 +18,31 @@ public class SearchClient {
 
     public SearchClient() {
         TraceSearchClient[] allSearchClients = RoutingServerWatcher.getAllSearchClient();
-
+        if ((allSearchClients.length == 0)) {
+            throw new RuntimeException("no active routing servers");
+        }
         client = allSearchClients[ThreadLocalRandom.current().nextInt(0, allSearchClients.length)];
     }
 
     public TraceNodesResult searchSpan(String traceId){
         String[] traceIdSegments = traceId.split(".");
+        TraceNodesResult traceNodesResult = new TraceNodesResult();
         if(traceIdSegments.length != 6){
-            return new TraceNodesResult();
+            return traceNodesResult;
         }
-        return new TraceNodesResult();
+
+        TraceId.Builder builder = TraceId.newBuilder();
+        for (String traceIdSegment : traceIdSegments) {
+            builder.addSegments(Long.parseLong(traceIdSegment));
+        }
+
+        QueryTask.Builder queryTaskBuilder = QueryTask.newBuilder().setTraceId(builder.build());
+        SearchResult result = client.search(queryTaskBuilder.build());
+
+        List<TraceNodeInfo> traceNodes = traceNodesResult.getResult();
+        result.getSpansList().forEach((span -> {
+            traceNodes.add(new TraceNodeInfo((span)));
+        }));
+        return traceNodesResult;
     }
 }
