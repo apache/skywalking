7553dff4df64b321d863cadf61ccf6ed542f3c23
wusheng <wu.sheng@foxmail.com>
2016-12-11 20:41:59 +0800

    set a new interceptor point for log4j2 to output traceid.

---
+++ skywalking-sniffer/skywalking-toolkit-activation/skywalking-toolkit-log4j-2.x-activation/src/main/java/com/a/eye/skywalking/toolkit/activation/log4j/v2/x/Log4j2OutputAppenderActivation.java
@@ -0,0 +1,31 @@
+package com.a.eye.skywalking.toolkit.activation.log4j.v2.x;
+
+import com.a.eye.skywalking.plugin.interceptor.MethodMatcher;
+import com.a.eye.skywalking.plugin.interceptor.StaticMethodsInterceptPoint;
+import com.a.eye.skywalking.plugin.interceptor.enhance.ClassStaticMethodsEnhancePluginDefine;
+import com.a.eye.skywalking.plugin.interceptor.matcher.SimpleMethodMatcher;
+
+/**
+ * Created by wusheng on 2016/12/7.
+ */
+public class Log4j2OutputAppenderActivation extends ClassStaticMethodsEnhancePluginDefine {
+    @Override
+    protected String enhanceClassName() {
+        return "com.a.eye.skywalking.toolkit.log4j.v2.x.Log4j2OutputAppender";
+    }
+
+    @Override
+    protected StaticMethodsInterceptPoint[] getStaticMethodsInterceptPoints() {
+        return new StaticMethodsInterceptPoint[]{new StaticMethodsInterceptPoint() {
+            @Override
+            public MethodMatcher[] getMethodsMatchers() {
+                return new MethodMatcher[]{new SimpleMethodMatcher("append")};
+            }
+
+            @Override
+            public String getMethodsInterceptor() {
+                return "com.a.eye.skywalking.toolkit.activation.log4j.v2.x.PrintTraceIdInterceptor";
+            }
+        }};
+    }
+}
