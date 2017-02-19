04638c51776dfdc1f48f4b59ed976996beecaa26
wusheng <wu.sheng@foxmail.com>
2017-02-19 08:24:58 +0800

    Rename and refactor SimapleObjectFirstInvokeInterceptor to NoCocurrencyAceessObject

--- skywalking-sniffer/skywalking-sdk-plugin/jedis-2.x-plugin/src/main/java/com/a/eye/skywalking/plugin/jedis/v2/JedisMethodInterceptor.java
+++ skywalking-sniffer/skywalking-sdk-plugin/jedis-2.x-plugin/src/main/java/com/a/eye/skywalking/plugin/jedis/v2/JedisMethodInterceptor.java
@@ -1,44 +1,47 @@
 package com.a.eye.skywalking.plugin.jedis.v2;
 
-import com.a.eye.skywalking.invoke.monitor.RPCClientInvokeMonitor;
 import com.a.eye.skywalking.model.Identification;
 import com.a.eye.skywalking.plugin.interceptor.EnhancedClassInstanceContext;
-import com.a.eye.skywalking.plugin.interceptor.assist.SimpleObjectFirstInvokeInterceptor;
+import com.a.eye.skywalking.plugin.interceptor.assist.NoCocurrencyAceessObject;
 import com.a.eye.skywalking.plugin.interceptor.enhance.InstanceMethodInvokeContext;
 import com.a.eye.skywalking.plugin.interceptor.enhance.MethodInterceptResult;
 
-public class JedisMethodInterceptor extends SimpleObjectFirstInvokeInterceptor {
+public class JedisMethodInterceptor extends NoCocurrencyAceessObject {
     protected static final String REDIS_CONN_INFO_KEY = "redisClusterConnInfo";
 
-    private static RPCClientInvokeMonitor rpcClientInvokeMonitor = new RPCClientInvokeMonitor();
 
     @Override
     public void beforeMethod(EnhancedClassInstanceContext context, InstanceMethodInvokeContext interceptorContext, MethodInterceptResult result) {
-        if (this.isFirstBeforeMethod(context)) {
-            /**
-             * redis server wouldn't process rpc context. ignore the
-             * return(ContextData) of sender's beforeSend
-             */
-            Identification.IdentificationBuilder builder = Identification
+        this.whenEnter(context, new Runnable() {
+            @Override public void run() {
+                /**
+                 * redis server wouldn't process rpc context. ignore the
+                 * return(ContextData) of sender's beforeSend
+                 */
+                Identification.IdentificationBuilder builder = Identification
                     .newBuilder()
                     .viewPoint(
-                            context.get(REDIS_CONN_INFO_KEY, String.class)
-                                    + " " + interceptorContext.methodName())
+                        context.get(REDIS_CONN_INFO_KEY, String.class)
+                            + " " + interceptorContext.methodName())
                     .spanType(RedisBuriedPointType.INSTANCE);
-            if (interceptorContext.allArguments().length > 0
+                if (interceptorContext.allArguments().length > 0
                     && interceptorContext.allArguments()[0] instanceof String) {
-                builder.businessKey("key="
+                    builder.businessKey("key="
                         + interceptorContext.allArguments()[0]);
+                }
+                rpcClientInvokeMonitor.beforeInvoke(builder.build());
             }
-            rpcClientInvokeMonitor.beforeInvoke(builder.build());
-        }
+        });
     }
 
     @Override
     public Object afterMethod(EnhancedClassInstanceContext context, InstanceMethodInvokeContext interceptorContext, Object ret) {
-        if (this.isLastAfterMethod(context)) {
-            rpcClientInvokeMonitor.afterInvoke();
-        }
+        this.whenExist(context, new Runnable(){
+            @Override public void run() {
+                rpcClientInvokeMonitor.afterInvoke();
+            }
+        });
+
         return ret;
     }
 
