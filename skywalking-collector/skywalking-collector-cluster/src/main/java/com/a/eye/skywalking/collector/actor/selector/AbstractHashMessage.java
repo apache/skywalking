daa753a6a7f6f5cd9429953da15cc50f5f4bba3a
pengys5 <8082209@qq.com>
2017-04-10 21:00:27 +0800

    add comments class in selector package

--- skywalking-collector/skywalking-collector-cluster/src/main/java/com/a/eye/skywalking/collector/actor/selector/AbstractHashMessage.java
+++ skywalking-collector/skywalking-collector-cluster/src/main/java/com/a/eye/skywalking/collector/actor/selector/AbstractHashMessage.java
@@ -1,6 +1,12 @@
 package com.a.eye.skywalking.collector.actor.selector;
 
 /**
+ * The <code>AbstractHashMessage</code> should be implemented by any class whose instances
+ * are intended to provide send message with {@link HashCodeSelector}.
+ * <p>
+ * Usually the implemented class used to persistence data to database
+ * or aggregation the metric,
+ *
  * @author pengys5
  */
 public abstract class AbstractHashMessage {
