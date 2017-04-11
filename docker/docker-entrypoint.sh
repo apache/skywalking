90d812e54c93e80520cd0e5993d3040393d07526
zhangxin <zhangxin@oneapm.com>
2017-04-11 21:26:19 +0800

    add image config options

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "collector services : $COLLECTOR_SERVERS"
+IFS=', ' read -r -a array <<< "$COLLECTOR_SERVERS"
+
+echo "clear config.properties"
+echo "" > /usr/local/skywalking-web/config/config/config.properties
+
+for index in "${!array[@]}"
+do
+    echo "collector.servers[$index]=${array[index]}">> /usr/local/skywalking-web/config/config/config.properties
+done
+
+echo "config.properties as following"
+cat /usr/local/skywalking-web/config/config/config.properties
+
+exec "$@"
