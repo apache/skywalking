1c4905ef58a58e7e98f7c4cd70879a1083c6f0ea
柏杨 Bai Yang <baiyang_java@163.com>
2017-05-10 19:33:30 +0800

    add resin plugin (#187)
    * add plugins for resin-3.x and resin-4.x

---
+++ apm-sniffer/apm-sdk-plugin/resin-4.x-plugin/src/main/java/org/skywalking/apm/plugin/resin/v4/ResinV4Interceptor.java
@@ -0,0 +1,70 @@
+package org.skywalking.apm.plugin.resin.v4;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.caucho.server.http.HttpRequest;
+import org.skywalking.apm.agent.core.context.ContextCarrier;
+import org.skywalking.apm.agent.core.context.ContextManager;
+import org.skywalking.apm.agent.core.plugin.interceptor.EnhancedClassInstanceContext;
+import org.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodInvokeContext;
+import org.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
+import org.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
+import org.skywalking.apm.trace.Span;
+import org.skywalking.apm.trace.tag.Tags;
+import org.skywalking.apm.util.StringUtil;
+
+/**
+ * Created by Baiyang on 2017/5/2.
+ */
+public class ResinV4Interceptor implements InstanceMethodsAroundInterceptor {
+    /**
+     * Header name that the serialized context data stored in
+     * {@link HttpRequest#getHeader(String)}.
+     */
+    public static final String HEADER_NAME_OF_CONTEXT_DATA = "SWTraceContext";
+    /**
+     * Resin component.
+     */
+    public static final String RESIN_COMPONENT = "Resin";
+
+    @Override
+    public void beforeMethod(EnhancedClassInstanceContext context, InstanceMethodInvokeContext interceptorContext,
+        MethodInterceptResult result) {
+        Object[] args = interceptorContext.allArguments();
+        HttpServletRequest request = (HttpServletRequest)args[0];
+        Span span = ContextManager.createSpan(request.getRequestURI());
+        Tags.COMPONENT.set(span, RESIN_COMPONENT);
+        Tags.PEER_HOST.set(span, request.getServerName());
+        Tags.PEER_PORT.set(span, request.getServerPort());
+        Tags.SPAN_KIND.set(span, Tags.SPAN_KIND_SERVER);
+        Tags.URL.set(span, request.getRequestURL().toString());
+        Tags.SPAN_LAYER.asHttp(span);
+
+        String tracingHeaderValue = request.getHeader(HEADER_NAME_OF_CONTEXT_DATA);
+        if (!StringUtil.isEmpty(tracingHeaderValue)) {
+            ContextManager.extract(new ContextCarrier().deserialize(tracingHeaderValue));
+        }
+    }
+
+    @Override
+    public Object afterMethod(EnhancedClassInstanceContext context, InstanceMethodInvokeContext interceptorContext,
+        Object ret) {
+        HttpServletResponse response = (HttpServletResponse)interceptorContext.allArguments()[1];
+        Span span = ContextManager.activeSpan();
+        Tags.STATUS_CODE.set(span, response.getStatus());
+
+        if (response.getStatus() != 200) {
+            Tags.ERROR.set(span, true);
+        }
+        ContextManager.stopSpan();
+        return ret;
+    }
+
+    @Override
+    public void handleMethodException(Throwable t, EnhancedClassInstanceContext context,
+        InstanceMethodInvokeContext interceptorContext) {
+        Span span = ContextManager.activeSpan();
+        span.log(t);
+        Tags.ERROR.set(span, true);
+    }
+}
