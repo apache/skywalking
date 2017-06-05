e384f1c56e174ac11aa3083fd9bc903c7f9e66a5
pengys5 <8082209@qq.com>
2017-06-05 21:22:29 +0800

    1. format the date: startTime, endTime 2. format the log data, change \n\t to </br>

--- src/main/resources/static/trace/span.js
+++ src/main/resources/static/trace/span.js
@@ -5,11 +5,26 @@
 function showSpanModal(spanSegId) {
     $.getJSON("/spanDataLoad?spanSegId=" + spanSegId, function (data) {
         var spanModalTemplate = $.templates("#spanModalTemplate");
-
-        data.startTime = moment(data.startTime).format("YYYY/MM/DD HH:mm:ss SSS");
-        data.endTime = moment(data.endTime).format("YYYY/MM/DD HH:mm:ss SSS");
-
         spanModalTemplate.link("#spanModalDiv", data);
         $("#spanModalDiv").modal("toggle");
     });
+}
+
+$.views.converters({
+    dateFormat: function (val) {
+        return moment(val).format("YYYY/MM/DD HH:mm:ss SSS");
+    },
+
+    tabCharacter: function (val) {
+        console.log(val);
+        return replaceAll(val);
+    }
+});
+
+function replaceAll(str) {
+    if (str != null){
+        str = str.replace(/\\n\\t/ig, "<br />");
+        str = str.replace(/\\n/ig, "<br />");
+    }
+    return str;
 }
