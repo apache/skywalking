991bcd5db6857347e1b2fe7c45e59069ae5eab13
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2017-06-14 08:55:31 +0800

    Update comment about ArgumentsParseException

--- apm-collector/apm-collector-worker/src/main/java/org/skywalking/apm/collector/worker/httpserver/ArgumentsParseException.java
+++ apm-collector/apm-collector-worker/src/main/java/org/skywalking/apm/collector/worker/httpserver/ArgumentsParseException.java
@@ -3,7 +3,7 @@ package org.skywalking.apm.collector.worker.httpserver;
 import org.skywalking.apm.collector.actor.WorkerException;
 
 /**
- * This exception is raised when argument not found or data type conversion from request.
+ * This exception is raised when can't find the required argument or data type conversion fails from request.
  *
  * @author pengys5
  * @since v3.1-2017
