1c8200116d28f12f3997767bce71734f24a4c200
wusheng <wu.sheng@foxmail.com>
2017-07-02 08:13:21 +0800

    Fix compile issue.

--- apm-sniffer/apm-agent-core/src/main/java/org/skywalking/apm/agent/core/context/TracingContext.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/skywalking/apm/agent/core/context/TracingContext.java
@@ -256,6 +256,7 @@ public class TracingContext implements AbstractTracerContext {
      */
     private AbstractTracingSpan push(AbstractTracingSpan span) {
         activeSpanStack.addLast(span);
+        return span;
     }
 
     /**
