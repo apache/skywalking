a9d416c61df92dace1fa6d7f0bb7f21d6ecd754f
qiujiayu <qiu_jy@suixingpay.com>
2017-08-03 23:26:37 +0800

    补回index ++; 使用尝试机制代替同步机制。

--- src/main/java/org/skywalking/apm/ui/tools/ServerSelector.java
+++ src/main/java/org/skywalking/apm/ui/tools/ServerSelector.java
@@ -15,16 +15,29 @@ public class ServerSelector {
     private int index = 0;
 
     public String select(List<String> serverList) {
-        int size = serverList.size();
-        int selectIndex = Math.abs(index) % size;
-
-        if (index > MAX_INDEX) {
-            index = 0;
-        }
-        try {
-            return serverList.get(selectIndex);
-        } catch (Exception e) {
-        }
-        return serverList.get(0);
+        String server = null;
+        int tryCnt = 0;
+        do {
+            int size = serverList.size();
+            int selectIndex = Math.abs(index) % size;
+            index ++;
+            if (index > MAX_INDEX) {
+                index = 0;
+            }
+            try {
+                server = serverList.get(selectIndex);
+            } catch (Exception e) {
+            }
+            if (null == server) {
+                tryCnt++;
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            } else {
+                return server;
+            }
+        } while (tryCnt < 3);
+        throw new RuntimeException("select server fail.");
     }
 }
