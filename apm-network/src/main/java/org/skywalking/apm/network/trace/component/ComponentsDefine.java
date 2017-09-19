e78e7e4d00fc4b789eb3a23665f8659b5a407b4b
ascrutae <nzt48.xin@gmail.com>
2017-09-19 23:35:38 +0800

    support struts 2 plugin

--- apm-network/src/main/java/org/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-network/src/main/java/org/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -35,6 +35,8 @@ public class ComponentsDefine {
 
     public static final OfficialComponent SPRING_MVC_ANNOTATION = new OfficialComponent(14, "SpringMVCAnnotation");
 
+    public static final OfficialComponent STRUTS2 = new OfficialComponent(14, "Struts2");
+
     private static ComponentsDefine instance = new ComponentsDefine();
 
     private String[] components;
@@ -44,7 +46,7 @@ public class ComponentsDefine {
     }
 
     public ComponentsDefine() {
-        components = new String[15];
+        components = new String[16];
         addComponent(TOMCAT);
         addComponent(HTTPCLIENT);
         addComponent(DUBBO);
@@ -59,6 +61,7 @@ public class ComponentsDefine {
         addComponent(OKHTTP);
         addComponent(SPRING_REST_TEMPLATE);
         addComponent(SPRING_MVC_ANNOTATION);
+        addComponent(STRUTS2);
     }
 
     private void addComponent(OfficialComponent component) {
