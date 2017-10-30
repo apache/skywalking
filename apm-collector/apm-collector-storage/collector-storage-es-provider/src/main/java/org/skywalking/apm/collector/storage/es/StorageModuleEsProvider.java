6e5789c327c9d65440578e6b1ec356228947d354
peng-yongsheng <8082209@qq.com>
2017-10-30 23:10:07 +0800

    Add elasticsearch and h2 storage provider implement

--- apm-collector/apm-collector-storage/collector-storage-es-provider/src/main/java/org/skywalking/apm/collector/storage/es/StorageModuleEsProvider.java
+++ apm-collector/apm-collector-storage/collector-storage-es-provider/src/main/java/org/skywalking/apm/collector/storage/es/StorageModuleEsProvider.java
@@ -18,16 +18,37 @@
 
 package org.skywalking.apm.collector.storage.es;
 
+import java.util.List;
 import java.util.Properties;
+import org.skywalking.apm.collector.client.ClientException;
+import org.skywalking.apm.collector.client.elasticsearch.ElasticSearchClient;
+import org.skywalking.apm.collector.core.define.DefineException;
 import org.skywalking.apm.collector.core.module.Module;
 import org.skywalking.apm.collector.core.module.ModuleProvider;
 import org.skywalking.apm.collector.core.module.ServiceNotProvidedException;
+import org.skywalking.apm.collector.storage.StorageException;
 import org.skywalking.apm.collector.storage.StorageModule;
+import org.skywalking.apm.collector.storage.dao.DAOContainer;
+import org.skywalking.apm.collector.storage.es.dao.EsDAO;
+import org.skywalking.apm.collector.storage.es.dao.EsDAODefineLoader;
+import org.skywalking.apm.collector.storage.es.define.ElasticSearchStorageInstaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author peng-yongsheng
  */
 public class StorageModuleEsProvider extends ModuleProvider {
+
+    private final Logger logger = LoggerFactory.getLogger(StorageModuleEsProvider.class);
+
+    private static final String CLUSTER_NAME = "cluster_name";
+    private static final String CLUSTER_TRANSPORT_SNIFFER = "cluster_transport_sniffer";
+    private static final String CLUSTER_NODES = "cluster_nodes";
+    private static final String INDEX_SHARDS_NUMBER = "index_shards_number";
+    private static final String INDEX_REPLICAS_NUMBER = "index_replicas_number";
+
+    private ElasticSearchClient elasticSearchClient;
 
     @Override public String name() {
         return "elasticsearch";
@@ -38,11 +59,31 @@ public class StorageModuleEsProvider extends ModuleProvider {
     }
 
     @Override public void prepare(Properties config) throws ServiceNotProvidedException {
-
+        String clusterName = config.getProperty(CLUSTER_NAME);
+        Boolean clusterTransportSniffer = (Boolean)config.get(CLUSTER_TRANSPORT_SNIFFER);
+        String clusterNodes = config.getProperty(CLUSTER_NODES);
+        elasticSearchClient = new ElasticSearchClient(clusterName, clusterTransportSniffer, clusterNodes);
     }
 
     @Override public void start(Properties config) throws ServiceNotProvidedException {
+        Integer indexShardsNumber = (Integer)config.get(INDEX_SHARDS_NUMBER);
+        Integer indexReplicasNumber = (Integer)config.get(INDEX_REPLICAS_NUMBER);
+        try {
+            elasticSearchClient.initialize();
 
+            EsDAODefineLoader loader = new EsDAODefineLoader();
+            List<EsDAO> esDAOs = loader.load();
+            esDAOs.forEach(esDAO -> {
+                esDAO.setClient(elasticSearchClient);
+                String interFaceName = esDAO.getClass().getInterfaces()[0].getName();
+                DAOContainer.INSTANCE.put(interFaceName, esDAO);
+            });
+
+            ElasticSearchStorageInstaller installer = new ElasticSearchStorageInstaller(indexShardsNumber, indexReplicasNumber);
+            installer.install(elasticSearchClient);
+        } catch (ClientException | DefineException | StorageException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     @Override public void notifyAfterCompleted() throws ServiceNotProvidedException {
--- apm-collector/apm-collector-storage/collector-storage-h2-provider/src/main/java/org/skywalking/apm/collector/storage/h2/StorageModuleH2Provider.java
+++ apm-collector/apm-collector-storage/collector-storage-h2-provider/src/main/java/org/skywalking/apm/collector/storage/h2/StorageModuleH2Provider.java
@@ -18,16 +18,35 @@
 
 package org.skywalking.apm.collector.storage.h2;
 
+import java.util.List;
 import java.util.Properties;
+import org.skywalking.apm.collector.client.h2.H2Client;
+import org.skywalking.apm.collector.client.h2.H2ClientException;
+import org.skywalking.apm.collector.core.define.DefineException;
 import org.skywalking.apm.collector.core.module.Module;
 import org.skywalking.apm.collector.core.module.ModuleProvider;
 import org.skywalking.apm.collector.core.module.ServiceNotProvidedException;
+import org.skywalking.apm.collector.storage.StorageException;
 import org.skywalking.apm.collector.storage.StorageModule;
+import org.skywalking.apm.collector.storage.dao.DAOContainer;
+import org.skywalking.apm.collector.storage.h2.dao.H2DAO;
+import org.skywalking.apm.collector.storage.h2.dao.H2DAODefineLoader;
+import org.skywalking.apm.collector.storage.h2.define.H2StorageInstaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author peng-yongsheng
  */
 public class StorageModuleH2Provider extends ModuleProvider {
+
+    private final Logger logger = LoggerFactory.getLogger(StorageModuleH2Provider.class);
+
+    private static final String URL = "url";
+    private static final String USER_NAME = "user_name";
+    private static final String PASSWORD = "password";
+
+    private H2Client client;
 
     @Override public String name() {
         return "h2";
@@ -38,11 +57,29 @@ public class StorageModuleH2Provider extends ModuleProvider {
     }
 
     @Override public void prepare(Properties config) throws ServiceNotProvidedException {
-
+        String url = config.getProperty(URL);
+        String userName = config.getProperty(USER_NAME);
+        String password = config.getProperty(PASSWORD);
+        client = new H2Client(url, userName, password);
     }
 
     @Override public void start(Properties config) throws ServiceNotProvidedException {
+        try {
+            client.initialize();
 
+            H2DAODefineLoader loader = new H2DAODefineLoader();
+            List<H2DAO> h2DAOs = loader.load();
+            h2DAOs.forEach(h2DAO -> {
+                h2DAO.setClient(client);
+                String interFaceName = h2DAO.getClass().getInterfaces()[0].getName();
+                DAOContainer.INSTANCE.put(interFaceName, h2DAO);
+            });
+
+            H2StorageInstaller installer = new H2StorageInstaller();
+            installer.install(client);
+        } catch (H2ClientException | DefineException | StorageException e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     @Override public void notifyAfterCompleted() throws ServiceNotProvidedException {
