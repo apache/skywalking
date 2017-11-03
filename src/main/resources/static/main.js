77e130846b9a41a54c9178141d897e042274569c
zuohl <wozixunwodao@qq.com>
2017-11-04 00:08:42 +0800

    revert momentjs reference

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -27,7 +27,7 @@ requirejs.config({
         "bootstrap": "/webjars/bootstrap/3.3.6/js/bootstrap.min",
         "metisMenu": "/webjars/metisMenu/2.5.2/dist/metisMenu.min",
         "slimscroll": "/webjars/jquery-slimscroll/1.3.6/jquery.slimscroll.min",
-        "moment": "/webjars/momentjs/2.19.1/min/moment.min",
+        "moment": "/webjars/momentjs/2.18.1/min/moment.min",
         "vis": "/webjars/vis/4.19.1/dist/vis.min",
         "datatables": "/webjars/datatables/1.10.13/media/js/jquery.dataTables.min",
         "head": "/head/head",
