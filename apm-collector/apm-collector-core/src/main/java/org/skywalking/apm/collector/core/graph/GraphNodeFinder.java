e3e06ec3cfbaa8e13becb0fb46fcb9849a10865d
wu-sheng <wu.sheng@foxmail.com>
2017-11-07 14:14:44 +0800

    Move all find methods into graphFinder.

--- apm-collector/apm-collector-core/src/main/java/org/skywalking/apm/collector/core/graph/GraphNodeFinder.java
+++ apm-collector/apm-collector-core/src/main/java/org/skywalking/apm/collector/core/graph/GraphNodeFinder.java
@@ -23,21 +23,40 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * @author wu-sheng
  */
-public class GraphBuilder {
+public class GraphNodeFinder {
     private Graph graph;
 
-    GraphBuilder(Graph graph) {
+    GraphNodeFinder(Graph graph) {
         this.graph = graph;
     }
 
+    /**
+     * Find an exist node to build the graph.
+     *
+     * @param handlerId of specific node in graph.
+     * @param outputClass of the found node
+     * @param <NODE_OUTPUT> type of given output class
+     * @return Node instance.
+     */
     public <NODE_OUTPUT> Node<?, NODE_OUTPUT> findNode(int handlerId, Class<NODE_OUTPUT> outputClass) {
         ConcurrentHashMap<Integer, Node> graphNodeIndex = graph.getNodeIndex();
         Node node = graphNodeIndex.get(handlerId);
         if (node == null) {
             throw new NodeNotFoundException("Can't find node with handlerId="
                 + handlerId
-                + " in graph[" + handlerId + "]");
+                + " in graph[" + graph.getId() + "]");
         }
         return node;
+    }
+
+    public Next findNext(int handlerId) {
+        ConcurrentHashMap<Integer, Node> graphNodeIndex = graph.getNodeIndex();
+        Node node = graphNodeIndex.get(handlerId);
+        if (node == null) {
+            throw new NodeNotFoundException("Can't find node with handlerId="
+                + handlerId
+                + " in graph[" + graph.getId() + "]");
+        }
+        return node.getNext();
     }
 }
