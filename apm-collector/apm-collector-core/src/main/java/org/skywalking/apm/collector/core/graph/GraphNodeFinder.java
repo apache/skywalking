3898429b28de0acd8b1880c312da37193d580c6e
wu-sheng <wu.sheng@foxmail.com>
2017-12-07 11:59:51 +0800

    Close GraphNodeFinder#findNode, if need to build a graph across modules, it is module/provider 's responsibility, who add node, who provides service to continue after this node.

--- apm-collector/apm-collector-core/src/main/java/org/skywalking/apm/collector/core/graph/GraphNodeFinder.java
+++ apm-collector/apm-collector-core/src/main/java/org/skywalking/apm/collector/core/graph/GraphNodeFinder.java
@@ -30,25 +30,6 @@ public class GraphNodeFinder {
         this.graph = graph;
     }
 
-    /**
-     * Find an exist node to build the graph.
-     *
-     * @param handlerId of specific node in graph.
-     * @param outputClass of the found node
-     * @param <NODEOUTPUT> type of given output class
-     * @return Node instance.
-     */
-    public <NODEOUTPUT> Node<?, NODEOUTPUT> findNode(int handlerId, Class<NODEOUTPUT> outputClass) {
-        ConcurrentHashMap<Integer, Node> graphNodeIndex = graph.getNodeIndex();
-        Node node = graphNodeIndex.get(handlerId);
-        if (node == null) {
-            throw new NodeNotFoundException("Can't find node with handlerId="
-                + handlerId
-                + " in graph[" + graph.getId() + "]");
-        }
-        return node;
-    }
-
     public Next findNext(int handlerId) {
         ConcurrentHashMap<Integer, Node> graphNodeIndex = graph.getNodeIndex();
         Node node = graphNodeIndex.get(handlerId);
