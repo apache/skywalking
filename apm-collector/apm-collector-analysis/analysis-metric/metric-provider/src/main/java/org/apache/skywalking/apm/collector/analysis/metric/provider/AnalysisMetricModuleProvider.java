b0e81888885638a35d3f6ff695686c3cdbbea8b7
peng-yongsheng <8082209@qq.com>
2017-12-19 18:07:46 +0800

    Analysis metric module finished.

--- apm-collector/apm-collector-analysis/analysis-metric/metric-provider/src/main/java/org/apache/skywalking/apm/collector/analysis/metric/provider/AnalysisMetricModuleProvider.java
+++ apm-collector/apm-collector-analysis/analysis-metric/metric-provider/src/main/java/org/apache/skywalking/apm/collector/analysis/metric/provider/AnalysisMetricModuleProvider.java
@@ -20,6 +20,25 @@ package org.apache.skywalking.apm.collector.analysis.metric.provider;
 
 import java.util.Properties;
 import org.apache.skywalking.apm.collector.analysis.metric.define.AnalysisMetricModule;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationComponentGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationComponentSpanListener;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationMappingGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationMappingSpanListener;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.application.ApplicationReferenceMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.global.GlobalTraceGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.global.GlobalTraceSpanListener;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.instance.InstanceMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.instance.InstanceReferenceMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.segment.SegmentCostGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.segment.SegmentCostSpanListener;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.service.ServiceEntryGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.service.ServiceEntrySpanListener;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.service.ServiceMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.service.ServiceReferenceMetricGraph;
+import org.apache.skywalking.apm.collector.analysis.metric.provider.worker.service.ServiceReferenceMetricSpanListener;
+import org.apache.skywalking.apm.collector.analysis.segment.parser.define.AnalysisSegmentParserModule;
+import org.apache.skywalking.apm.collector.analysis.segment.parser.define.service.ISegmentParserListenerRegister;
 import org.apache.skywalking.apm.collector.core.module.Module;
 import org.apache.skywalking.apm.collector.core.module.ModuleProvider;
 import org.apache.skywalking.apm.collector.core.module.ServiceNotProvidedException;
@@ -43,7 +62,40 @@ public class AnalysisMetricModuleProvider extends ModuleProvider {
     }
 
     @Override public void start(Properties config) throws ServiceNotProvidedException {
+        segmentParserListenerRegister();
 
+        ServiceReferenceMetricGraph serviceReferenceMetricGraph = new ServiceReferenceMetricGraph(getManager());
+        serviceReferenceMetricGraph.create();
+
+        InstanceReferenceMetricGraph instanceReferenceMetricGraph = new InstanceReferenceMetricGraph(getManager());
+        instanceReferenceMetricGraph.create();
+
+        ApplicationReferenceMetricGraph applicationReferenceMetricGraph = new ApplicationReferenceMetricGraph(getManager());
+        applicationReferenceMetricGraph.create();
+
+        ServiceMetricGraph serviceMetricGraph = new ServiceMetricGraph(getManager());
+        serviceMetricGraph.create();
+
+        InstanceMetricGraph instanceMetricGraph = new InstanceMetricGraph(getManager());
+        instanceMetricGraph.create();
+
+        ApplicationMetricGraph applicationMetricGraph = new ApplicationMetricGraph(getManager());
+        applicationMetricGraph.create();
+
+        ApplicationComponentGraph applicationComponentGraph = new ApplicationComponentGraph(getManager());
+        applicationComponentGraph.create();
+
+        ApplicationMappingGraph applicationMappingGraph = new ApplicationMappingGraph(getManager());
+        applicationMappingGraph.create();
+
+        ServiceEntryGraph serviceEntryGraph = new ServiceEntryGraph(getManager());
+        serviceEntryGraph.create();
+
+        GlobalTraceGraph globalTraceGraph = new GlobalTraceGraph(getManager());
+        globalTraceGraph.create();
+
+        SegmentCostGraph segmentCostGraph = new SegmentCostGraph(getManager());
+        segmentCostGraph.create();
     }
 
     @Override public void notifyAfterCompleted() throws ServiceNotProvidedException {
@@ -52,5 +104,15 @@ public class AnalysisMetricModuleProvider extends ModuleProvider {
 
     @Override public String[] requiredModules() {
         return new String[0];
+    }
+
+    private void segmentParserListenerRegister() {
+        ISegmentParserListenerRegister segmentParserListenerRegister = getManager().find(AnalysisSegmentParserModule.NAME).getService(ISegmentParserListenerRegister.class);
+        segmentParserListenerRegister.register(new ServiceReferenceMetricSpanListener());
+        segmentParserListenerRegister.register(new ApplicationComponentSpanListener());
+        segmentParserListenerRegister.register(new ApplicationMappingSpanListener());
+        segmentParserListenerRegister.register(new ServiceEntrySpanListener(getManager()));
+        segmentParserListenerRegister.register(new GlobalTraceSpanListener());
+        segmentParserListenerRegister.register(new SegmentCostSpanListener(getManager()));
     }
 }
