704f4e146f95f3c82a66fba2bcf161b51f67a3a2
hanahmily <hanahmily@gmail.com>
2018-02-26 11:32:37 +0800

    Add mock

---
+++ mock/service.js
@@ -0,0 +1,93 @@
+import mockjs from 'mockjs';
+
+export default {
+  searchService(req, res) {
+    res.json(mockjs.mock(
+      {
+        data: {
+          'searchService|5': [
+            {
+              'key|+1': 3,
+              label: function() { return `service-${this.key}`; }, // eslint-disable-line
+            },
+          ],
+        },
+      }
+    ));
+  },
+  getService(req, res) {
+    res.json(mockjs.mock(
+      {
+        data: {
+          'searchService|5': [{}],
+          getServiceResponseTimeTrend: {
+            'trendList|60': ['@natural(100, 1000)'],
+          },
+          getServiceTPSTrend: {
+            'trendList|60': ['@natural(500, 10000)'],
+          },
+          getServiceSLATrend: {
+            'trendList|60': ['@natural(8000, 10000)'],
+          },
+          getServiceTopology: () => {
+            const upNodes = mockjs.mock({
+              'nodes|1-5': [
+                {
+                  'id|+1': 100,
+                  name: '@name',
+                  'type|1': ['DUBBO', 'USER', 'SPRINGMVC'],
+                  'calls|1000-2000': 1,
+                  'sla|1-100.1-2': 1,
+                  'apdex|0.2': 1,
+                  'numOfServiceAlarm|1-100': 1,
+                },
+              ],
+            });
+            const centerNodes = mockjs.mock({
+              nodes: [
+                {
+                  'id|+1': 1,
+                  name: '@name',
+                  'type|1': ['DUBBO', 'tomcat', 'SPRINGMVC'],
+                  'calls|1000-2000': 1,
+                  'sla|1-100.1-2': 1,
+                  'apdex|0.2': 1,
+                  'numOfServiceAlarm|1-100': 1,
+                },
+              ],
+            });
+            const downNodes = mockjs.mock({
+              'nodes|2-5': [
+                {
+                  'id|+1': 200,
+                  name: '@name',
+                  'type|1': ['Oracle', 'MYSQL', 'REDIS'],
+                },
+              ],
+            });
+            const nodes = upNodes.nodes.concat(centerNodes.nodes, downNodes.nodes);
+            const calls = upNodes.nodes.map(node => (mockjs.mock({
+              source: node.id,
+              target: 1,
+              'isAlarm|1': true,
+              'callType|1': ['rpc', 'http', 'dubbo'],
+              'callsPerSec|100-2000': 1,
+              'responseTimePerSec|500-5000': 1,
+            }))).concat(downNodes.nodes.map(node => (mockjs.mock({
+              source: 1,
+              target: node.id,
+              'isAlarm|1': true,
+              'callType|1': ['rpc', 'http', 'dubbo'],
+              'callsPerSec|100-2000': 1,
+              'responseTimePerSec|500-5000': 1,
+            }))));
+            return {
+              nodes,
+              calls,
+            };
+          },
+        },
+      }
+    ));
+  },
+};
---
+++ mock/trace.js
@@ -0,0 +1,148 @@
+import mockjs from 'mockjs';
+
+export default {
+  getAllApplication(req, res) {
+    res.json(mockjs.mock(
+      {
+        data: {
+          'applicationCodes|20-50': [{ 'key|+1': 3, label: function() { return `app-${this.key}`; } }],  // eslint-disable-line
+        },
+      }
+    ));
+  },
+  getTrace(req, res) {
+    res.json(mockjs.mock(
+      {
+        data: {
+          queryBasicTraces: {
+            'traces|10': [{
+              operationName: '@url',
+              duration: '@natural(100, 1000)',
+              start: '@datetime',
+              'isError|1': true,
+              traceId: '@guid',
+            }],
+            total: '@natural(5, 50)',
+          },
+        },
+      }
+    ));
+  },
+  getSpans(req, res) {
+    res.json(mockjs.mock(
+      {
+        data: {
+          queryTrace: {
+            spans: [
+              {
+                spanId: 1,
+                segmentId: 1,
+                startTime: 1516151345000,
+                applicationCode: 'xx',
+                endTime: 1516151355000,
+                operationName: '/user/tt',
+                'type|1': ['Local', 'Entry', 'Exit'],
+                'component|1': ['MySQL', 'H2', 'Spring'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'db.type', value: 'aa' }],
+                'logs|2-10': [{ 'time|+1': 1516151345000, 'data|3-8': [{ key: 'db.type', value: 'aa' }] }],
+              },
+              {
+                spanId: 2,
+                parentSpanId: 1,
+                segmentId: 1,
+                applicationCode: 'yy',
+                startTime: 1516151348000,
+                endTime: 1516151351000,
+                operationName: '/sql/qq',
+                'type|1': ['Local', 'Entry', 'Exit'],
+                'component|1': ['MySQL', 'H2', 'Spring'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'db.type', value: 'aa' }],
+              },
+              {
+                spanId: 3,
+                parentSpanId: 2,
+                segmentId: 1,
+                applicationCode: 'yy',
+                startTime: 1516151349312,
+                endTime: 1516151350728,
+                operationName: '/sql/qq',
+                'type|1': ['Local', 'Entry', 'Exit'],
+                'component|1': ['MySQL', 'H2', 'Spring'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'db.type', value: 'aa' }],
+              },
+              {
+                spanId: 4,
+                parentSpanId: 1,
+                segmentId: 1,
+                applicationCode: 'zz',
+                startTime: 1516151351000,
+                endTime: 1516151354000,
+                operationName: '/sql/qq',
+                'type|1': ['Local', 'Entry', 'Exit'],
+                'component|1': ['MySQL', 'H2', 'Spring'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'db.type', value: 'aa' }],
+              },
+              {
+                spanId: 5,
+                parentSpanId: 1,
+                segmentId: 1,
+                applicationCode: 'zz',
+                startTime: 1516151351000,
+                endTime: 1516151354000,
+                operationName: '/mq/producer',
+                'type|1': ['Exit'],
+                'component|1': ['RockerMQ'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'producer', value: 'tt' }],
+              },
+              {
+                spanId: 6,
+                segmentId: 1,
+                applicationCode: 'kk',
+                startTime: 1516151355000,
+                endTime: 1516151360000,
+                operationName: '/mq/consumer',
+                'type|1': ['Entry'],
+                'component|1': ['RockerMQ'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'consumer', value: 'tt' }],
+                refs: [
+                  {
+                    parentSpanId: 5,
+                    parentSegmentId: 1,
+                  },
+                ],
+              },
+              {
+                spanId: 6,
+                segmentId: 1,
+                applicationCode: 'kk',
+                startTime: 1516151355000,
+                endTime: 1516151360000,
+                operationName: '/mq/consumer',
+                'type|1': ['Entry'],
+                'component|1': ['Kafka'],
+                peer: '@ip',
+                'tags|1-5': [{ key: 'consumer', value: 'tt' }],
+                refs: [
+                  {
+                    traceId: 121212,
+                    type: 'CROSS_PROCESS',
+                  },
+                  {
+                    traceId: 22223333,
+                    type: 'CROSS_THREAD',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      }
+    ));
+  },
+};
