19d83476dbeed32adc6ac5c3510245b20e88f783
28707699@qq.com <8m*ivy00>
2018-03-02 15:13:11 +0800

    modify by jiwengang

--- apm-collector/apm-collector-storage/collector-storage-h2-provider/src/main/java/org/apache/skywalking/apm/collector/storage/h2/dao/ui/InstanceMetricH2UIDAO.java
+++ apm-collector/apm-collector-storage/collector-storage-h2-provider/src/main/java/org/apache/skywalking/apm/collector/storage/h2/dao/ui/InstanceMetricH2UIDAO.java
@@ -18,10 +18,6 @@
 
 package org.apache.skywalking.apm.collector.storage.h2.dao.ui;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 import org.apache.skywalking.apm.collector.client.h2.H2Client;
 import org.apache.skywalking.apm.collector.client.h2.H2ClientException;
 import org.apache.skywalking.apm.collector.core.util.Const;
@@ -37,13 +33,21 @@ import org.apache.skywalking.apm.collector.storage.utils.TimePyramidTableNameBui
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author peng-yongsheng, clevertension
  */
 public class InstanceMetricH2UIDAO extends H2DAO implements IInstanceMetricUIDAO {
-
     private final Logger logger = LoggerFactory.getLogger(InstanceMetricH2UIDAO.class);
     private static final String GET_TPS_METRIC_SQL = "select * from {0} where {1} = ?";
+    private static final String GET_SERVER_THROUGHPUT_SQL=
+            "select {0}, sum({1}) as transaction_calls,sum({2}) as transaction_error_calls " +
+            "from {3} where {4} >= ? and {4} <= ? " +
+            "and {5}=? and {6}=? group by {0} limit ?";
 
     public InstanceMetricH2UIDAO(H2Client client) {
         super(client);
@@ -51,8 +55,32 @@ public class InstanceMetricH2UIDAO extends H2DAO implements IInstanceMetricUIDAO
 
     @Override public List<AppServerInfo> getServerThroughput(int applicationId, Step step, long startTimeBucket, long endTimeBucket,
         int secondBetween, int topN, MetricSource metricSource) {
-        return null;
+        H2Client client = getClient();
+        String tableName = TimePyramidTableNameBuilder.build(step, InstanceMetricTable.TABLE);
+        String sql = SqlBuilder.buildSql(GET_SERVER_THROUGHPUT_SQL,InstanceMetricTable.COLUMN_INSTANCE_ID,
+                InstanceMetricTable.COLUMN_TRANSACTION_CALLS,InstanceMetricTable.COLUMN_TRANSACTION_ERROR_CALLS,tableName,
+                InstanceMetricTable.COLUMN_TIME_BUCKET,InstanceMetricTable.COLUMN_APPLICATION_ID,InstanceMetricTable.COLUMN_SOURCE_VALUE);
+        List<AppServerInfo> appServerInfos = new LinkedList<>();
+        Object[] params = new Object[] {startTimeBucket, endTimeBucket,applicationId,metricSource.getValue(),topN};
+        try (ResultSet rs = client.executeQuery(sql, params)) {
+            while (rs.next()) {
+                int instanceId = rs.getInt(InstanceMetricTable.COLUMN_INSTANCE_ID);
+                int transactionCallsSum = rs.getInt(InstanceMetricTable.COLUMN_TRANSACTION_CALLS);
+                int transactionErrorCallsSum = rs.getInt(InstanceMetricTable.COLUMN_TRANSACTION_ERROR_CALLS);
+                int simpleValue = (transactionCallsSum -transactionErrorCallsSum)/secondBetween;
+                AppServerInfo appServerInfo = new AppServerInfo();
+                appServerInfo.setId(instanceId);
+                appServerInfo.setCallsPerSec(simpleValue);
+                appServerInfos.add(appServerInfo);
+            }
+        } catch (SQLException | H2ClientException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return appServerInfos;
     }
+
+
+
 
     @Override public List<Integer> getServerTPSTrend(int instanceId, Step step, List<DurationPoint> durationPoints) {
         H2Client client = getClient();
