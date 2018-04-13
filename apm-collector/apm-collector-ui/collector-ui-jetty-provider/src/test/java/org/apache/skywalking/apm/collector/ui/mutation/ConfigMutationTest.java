d1b35bde85de8106bc9e3291faf26f6588647b52
candyleer <295198088@qq.com>
2018-04-13 18:11:18 +0800

    add some null testcase

--- apm-collector/apm-collector-ui/collector-ui-jetty-provider/src/test/java/org/apache/skywalking/apm/collector/ui/mutation/ConfigMutationTest.java
+++ apm-collector/apm-collector-ui/collector-ui-jetty-provider/src/test/java/org/apache/skywalking/apm/collector/ui/mutation/ConfigMutationTest.java
@@ -1,9 +1,12 @@
 package org.apache.skywalking.apm.collector.ui.mutation;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * this class may not be implemented ,so just test if it's null
+ * if update the class ,please update the testcase
  * @author lican
  * @date 2018/4/13
  */
@@ -15,9 +18,15 @@ public class ConfigMutationTest {
 
     @Test
     public void setDataTTLConfigs() {
+        ConfigMutation configMutation = new ConfigMutation();
+        Boolean aBoolean = configMutation.setDataTTLConfigs(null);
+        Assert.assertNull(aBoolean);
     }
 
     @Test
     public void setAlarmThreshold() {
+        ConfigMutation configMutation = new ConfigMutation();
+        Boolean aBoolean = configMutation.setAlarmThreshold(null);
+        Assert.assertNull(aBoolean);
     }
 }
