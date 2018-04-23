69ebe56024e469720b9a1cdda8576f16705ffa00
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2018-04-23 09:31:39 +0800

    GraphQL for Thermodynamic in overview page. (#1068)
    * Provide graphQL for Thermodynamic in overview page.
    
    * Finish the Thermodynamic.
    
    * `nodes` should not be null.
    
    * Add initialized java bean for graphql protocol.
    
    * Add license head.
    
    * Fixed bean definition fault.

--- apm-collector/apm-collector-ui/collector-ui-jetty-provider/src/main/java/org/apache/skywalking/apm/collector/ui/query/OverViewLayerQuery.java
+++ apm-collector/apm-collector-ui/collector-ui-jetty-provider/src/main/java/org/apache/skywalking/apm/collector/ui/query/OverViewLayerQuery.java
@@ -18,20 +18,25 @@
 
 package org.apache.skywalking.apm.collector.ui.query;
 
+import java.text.ParseException;
+import java.util.List;
 import org.apache.skywalking.apm.collector.core.module.ModuleManager;
 import org.apache.skywalking.apm.collector.storage.ui.common.Duration;
 import org.apache.skywalking.apm.collector.storage.ui.common.Topology;
+import org.apache.skywalking.apm.collector.storage.ui.common.ValueType;
 import org.apache.skywalking.apm.collector.storage.ui.overview.AlarmTrend;
 import org.apache.skywalking.apm.collector.storage.ui.overview.ApplicationTPS;
 import org.apache.skywalking.apm.collector.storage.ui.overview.ClusterBrief;
 import org.apache.skywalking.apm.collector.storage.ui.overview.ConjecturalAppBrief;
+import org.apache.skywalking.apm.collector.storage.ui.overview.Thermodynamic;
 import org.apache.skywalking.apm.collector.storage.ui.service.ServiceMetric;
 import org.apache.skywalking.apm.collector.ui.graphql.Query;
-import org.apache.skywalking.apm.collector.ui.service.*;
+import org.apache.skywalking.apm.collector.ui.service.AlarmService;
+import org.apache.skywalking.apm.collector.ui.service.ApplicationService;
+import org.apache.skywalking.apm.collector.ui.service.ClusterTopologyService;
+import org.apache.skywalking.apm.collector.ui.service.NetworkAddressService;
+import org.apache.skywalking.apm.collector.ui.service.ServiceNameService;
 import org.apache.skywalking.apm.collector.ui.utils.DurationUtils;
-
-import java.text.ParseException;
-import java.util.List;
 
 import static java.util.Objects.isNull;
 
@@ -142,5 +147,9 @@ public class OverViewLayerQuery implements Query {
         long endTimeBucket = DurationUtils.INSTANCE.exchangeToTimeBucket(duration.getEnd());
 
         return getApplicationService().getTopNApplicationThroughput(duration.getStep(), startTimeBucket, endTimeBucket, topN);
+    }
+
+    public Thermodynamic getThermodynamic(Duration duration, ValueType type) throws ParseException {
+        return new Thermodynamic();
     }
 }
