2cb9d1a3063ca1c19c8f0b494a44330cea71908a
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-04-25 18:02:37 -0700

    Redirecting apm-webapp log to rotatable file

--- apm-dist/bin/webappService.sh
+++ apm-dist/bin/webappService.sh
@@ -28,12 +28,15 @@ if [ ! -d "${WEBAPP_HOME}/logs" ]; then
     mkdir -p "${WEBAPP_LOG_DIR}"
 fi
 
+LOG_FILE_LOCATION=${WEBAPP_LOG_DIR}/webapp.log
+
 _RUNJAVA=${JAVA_HOME}/bin/java
 [ -z "$JAVA_HOME" ] && _RUNJAVA=java
 
 eval exec "\"$_RUNJAVA\" ${JAVA_OPTS} -jar ${JAR_PATH}/skywalking-webapp.jar \
          --server.port=8080 --collector.ribbon.listOfServers=127.0.0.1:10800 \
-        2>${WEBAPP_LOG_DIR}/webapp.log 1> /dev/null &"
+         --logging.file=${LOG_FILE_LOCATION} \
+        2>${WEBAPP_LOG_DIR}/webapp-console.log 1> /dev/null &"
 
 if [ $? -eq 0 ]; then
     sleep 1
