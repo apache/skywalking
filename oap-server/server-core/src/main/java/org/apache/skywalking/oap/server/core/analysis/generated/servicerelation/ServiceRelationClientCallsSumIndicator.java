800a5353def28b37b7580f0c32de87534ae56f22
彭勇升 pengys <8082209@qq.com>
2018-09-07 11:59:55 +0800

    Add generate indicator and dispatcher about service topology but not test. (#1638)
    * Add generate indicator and dispatcher about service topology but not test.
    
    * Delete call type from service relation.

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/generated/servicerelation/ServiceRelationClientCallsSumIndicator.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/generated/servicerelation/ServiceRelationClientCallsSumIndicator.java
@@ -22,7 +22,7 @@ import java.util.*;
 import lombok.*;
 import org.apache.skywalking.oap.server.core.Const;
 import org.apache.skywalking.oap.server.core.alarm.*;
-import org.apache.skywalking.oap.server.core.analysis.indicator.LongAvgIndicator;
+import org.apache.skywalking.oap.server.core.analysis.indicator.SumIndicator;
 import org.apache.skywalking.oap.server.core.analysis.indicator.annotation.IndicatorType;
 import org.apache.skywalking.oap.server.core.remote.annotation.StreamData;
 import org.apache.skywalking.oap.server.core.remote.grpc.proto.RemoteData;
@@ -37,8 +37,8 @@ import org.apache.skywalking.oap.server.core.storage.annotation.*;
  */
 @IndicatorType
 @StreamData
-@StorageEntity(name = "servicerelation_avg", builder = ServiceRelationAvgIndicator.Builder.class)
-public class ServiceRelationAvgIndicator extends LongAvgIndicator implements AlarmSupported {
+@StorageEntity(name = "service_relation_client_calls_sum", builder = ServiceRelationClientCallsSumIndicator.Builder.class)
+public class ServiceRelationClientCallsSumIndicator extends SumIndicator implements AlarmSupported {
 
     @Setter @Getter @Column(columnName = "source_service_id") private int sourceServiceId;
     @Setter @Getter @Column(columnName = "dest_service_id") private int destServiceId;
@@ -66,7 +66,7 @@ public class ServiceRelationAvgIndicator extends LongAvgIndicator implements Ala
         if (getClass() != obj.getClass())
             return false;
 
-        ServiceRelationAvgIndicator indicator = (ServiceRelationAvgIndicator)obj;
+        ServiceRelationClientCallsSumIndicator indicator = (ServiceRelationClientCallsSumIndicator)obj;
         if (sourceServiceId != indicator.sourceServiceId)
             return false;
         if (destServiceId != indicator.destServiceId)
@@ -81,53 +81,45 @@ public class ServiceRelationAvgIndicator extends LongAvgIndicator implements Ala
     @Override public RemoteData.Builder serialize() {
         RemoteData.Builder remoteBuilder = RemoteData.newBuilder();
 
-        remoteBuilder.setDataLongs(0, getSummation());
-        remoteBuilder.setDataLongs(1, getValue());
-        remoteBuilder.setDataLongs(2, getTimeBucket());
+        remoteBuilder.setDataLongs(0, getValue());
+        remoteBuilder.setDataLongs(1, getTimeBucket());
 
 
         remoteBuilder.setDataIntegers(0, getSourceServiceId());
         remoteBuilder.setDataIntegers(1, getDestServiceId());
-        remoteBuilder.setDataIntegers(2, getCount());
 
         return remoteBuilder;
     }
 
     @Override public void deserialize(RemoteData remoteData) {
 
-        setSummation(remoteData.getDataLongs(0));
-        setValue(remoteData.getDataLongs(1));
-        setTimeBucket(remoteData.getDataLongs(2));
+        setValue(remoteData.getDataLongs(0));
+        setTimeBucket(remoteData.getDataLongs(1));
 
 
         setSourceServiceId(remoteData.getDataIntegers(0));
         setDestServiceId(remoteData.getDataIntegers(1));
-        setCount(remoteData.getDataIntegers(2));
     }
 
     @Override public AlarmMeta getAlarmMeta() {
-        return new AlarmMeta("ServiceRelation_Avg", Scope.ServiceRelation, sourceServiceId, destServiceId);
+        return new AlarmMeta("Service_Relation_Client_Calls_Sum", Scope.ServiceRelation, sourceServiceId, destServiceId);
     }
 
-    public static class Builder implements StorageBuilder<ServiceRelationAvgIndicator> {
+    public static class Builder implements StorageBuilder<ServiceRelationClientCallsSumIndicator> {
 
-        @Override public Map<String, Object> data2Map(ServiceRelationAvgIndicator storageData) {
+        @Override public Map<String, Object> data2Map(ServiceRelationClientCallsSumIndicator storageData) {
             Map<String, Object> map = new HashMap<>();
             map.put("source_service_id", storageData.getSourceServiceId());
             map.put("dest_service_id", storageData.getDestServiceId());
-            map.put("summation", storageData.getSummation());
-            map.put("count", storageData.getCount());
             map.put("value", storageData.getValue());
             map.put("time_bucket", storageData.getTimeBucket());
             return map;
         }
 
-        @Override public ServiceRelationAvgIndicator map2Data(Map<String, Object> dbMap) {
-            ServiceRelationAvgIndicator indicator = new ServiceRelationAvgIndicator();
+        @Override public ServiceRelationClientCallsSumIndicator map2Data(Map<String, Object> dbMap) {
+            ServiceRelationClientCallsSumIndicator indicator = new ServiceRelationClientCallsSumIndicator();
             indicator.setSourceServiceId(((Number)dbMap.get("source_service_id")).intValue());
             indicator.setDestServiceId(((Number)dbMap.get("dest_service_id")).intValue());
-            indicator.setSummation(((Number)dbMap.get("summation")).longValue());
-            indicator.setCount(((Number)dbMap.get("count")).intValue());
             indicator.setValue(((Number)dbMap.get("value")).longValue());
             indicator.setTimeBucket(((Number)dbMap.get("time_bucket")).longValue());
             return indicator;
---
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/service/ServiceMappingIndicator.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.oap.server.core.analysis.manual.service;
+
+import java.util.*;
+import lombok.*;
+import org.apache.skywalking.oap.server.core.Const;
+import org.apache.skywalking.oap.server.core.analysis.indicator.Indicator;
+import org.apache.skywalking.oap.server.core.analysis.indicator.annotation.IndicatorType;
+import org.apache.skywalking.oap.server.core.remote.annotation.StreamData;
+import org.apache.skywalking.oap.server.core.remote.grpc.proto.RemoteData;
+import org.apache.skywalking.oap.server.core.storage.StorageBuilder;
+import org.apache.skywalking.oap.server.core.storage.annotation.*;
+
+/**
+ * @author peng-yongsheng
+ */
+@IndicatorType
+@StreamData
+@StorageEntity(name = "service_mapping", builder = ServiceMappingIndicator.Builder.class)
+public class ServiceMappingIndicator extends Indicator {
+
+    private static final String SERVICE_ID = "service_id";
+    private static final String MAPPING_SERVICE_ID = "mapping_service_id";
+
+    @Setter @Getter @Column(columnName = SERVICE_ID) private int serviceId;
+    @Setter @Getter @Column(columnName = MAPPING_SERVICE_ID) private int mappingServiceId;
+
+    @Override public String id() {
+        String splitJointId = String.valueOf(getTimeBucket());
+        splitJointId += Const.ID_SPLIT + String.valueOf(serviceId);
+        splitJointId += Const.ID_SPLIT + String.valueOf(mappingServiceId);
+        return splitJointId;
+    }
+
+    @Override public int hashCode() {
+        int result = 17;
+        result = 31 * result + serviceId;
+        result = 31 * result + mappingServiceId;
+        result = 31 * result + (int)getTimeBucket();
+        return result;
+    }
+
+    @Override public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        ServiceMappingIndicator indicator = (ServiceMappingIndicator)obj;
+        if (serviceId != indicator.serviceId)
+            return false;
+        if (mappingServiceId != indicator.mappingServiceId)
+            return false;
+
+        if (getTimeBucket() != indicator.getTimeBucket())
+            return false;
+
+        return true;
+    }
+
+    @Override public RemoteData.Builder serialize() {
+        RemoteData.Builder remoteBuilder = RemoteData.newBuilder();
+
+        remoteBuilder.setDataLongs(0, getTimeBucket());
+
+        remoteBuilder.setDataIntegers(0, getServiceId());
+        remoteBuilder.setDataIntegers(1, getMappingServiceId());
+
+        return remoteBuilder;
+    }
+
+    @Override public void deserialize(RemoteData remoteData) {
+        setTimeBucket(remoteData.getDataLongs(0));
+
+        setServiceId(remoteData.getDataIntegers(0));
+        setMappingServiceId(remoteData.getDataIntegers(1));
+    }
+
+    @Override public void calculate() {
+    }
+
+    @Override public final void combine(Indicator indicator) {
+    }
+
+    public static class Builder implements StorageBuilder<ServiceMappingIndicator> {
+
+        @Override public Map<String, Object> data2Map(ServiceMappingIndicator storageData) {
+            Map<String, Object> map = new HashMap<>();
+            map.put(SERVICE_ID, storageData.getServiceId());
+            map.put(MAPPING_SERVICE_ID, storageData.getMappingServiceId());
+            map.put(TIME_BUCKET, storageData.getTimeBucket());
+            return map;
+        }
+
+        @Override public ServiceMappingIndicator map2Data(Map<String, Object> dbMap) {
+            ServiceMappingIndicator indicator = new ServiceMappingIndicator();
+            indicator.setServiceId(((Number)dbMap.get(SERVICE_ID)).intValue());
+            indicator.setMappingServiceId(((Number)dbMap.get(MAPPING_SERVICE_ID)).intValue());
+            indicator.setTimeBucket(((Number)dbMap.get(TIME_BUCKET)).longValue());
+            return indicator;
+        }
+    }
+}
