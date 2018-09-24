e881421d600093e014656d6651ec59deb5c3c7bf
chenpengfei <langyu0826@gmail.com>
2018-09-24 20:56:20 +0800

    Add Undertow 2.x plugin (#1697)
    * Add Undertow 2.x plugin
    
    * Undertow: Interceptor method executeRootHandler in class Connectors

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -98,6 +98,8 @@ public class ComponentsDefine {
 
     public static final OfficialComponent TRANSPORT_CLIENT =  new OfficialComponent(48, "transport-client");
 
+    public static final OfficialComponent UNDERTOW =  new OfficialComponent(49, "Undertow");
+
     private static ComponentsDefine INSTANCE = new ComponentsDefine();
 
     private String[] components;
@@ -107,7 +109,7 @@ public class ComponentsDefine {
     }
 
     public ComponentsDefine() {
-        components = new String[49];
+        components = new String[50];
         addComponent(TOMCAT);
         addComponent(HTTPCLIENT);
         addComponent(DUBBO);
@@ -144,6 +146,7 @@ public class ComponentsDefine {
         addComponent(ACTIVEMQ_PRODUCER);
         addComponent(ACTIVEMQ_CONSUMER);
         addComponent(TRANSPORT_CLIENT);
+        addComponent(UNDERTOW);
     }
 
     private void addComponent(OfficialComponent component) {
