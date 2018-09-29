9b25ba490c746793ee05b7e339bd1e8794590ea1
WithLin <withlin@yeah.net>
2018-09-29 09:54:43 +0800

    fix roolback grpc  plugin

--- apm-sniffer/apm-sdk-plugin/grpc-1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/grpc/v1/define/AbstractStubInstrumentation.java
+++ apm-sniffer/apm-sdk-plugin/grpc-1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/grpc/v1/define/AbstractStubInstrumentation.java
@@ -31,9 +31,9 @@ import static org.apache.skywalking.apm.agent.core.plugin.match.NameMatch.byName
 
 public class AbstractStubInstrumentation extends ClassInstanceMethodsEnhancePluginDefine {
 
-    private static final String INTERCEPT_CLASS = "org.apache.skywalking.apm.plugin.grpc.v1.AbstractStubInterceptor";
-    private static final String ENHANCE_METHOD = "getChannel";
-    private static final String ENHANCE_CLASS = "io.grpc.stub.AbstractStub";
+    public static final String INTERCEPT_CLASS = "org.apache.skywalking.apm.plugin.grpc.v1.AbstractStubInterceptor";
+    public static final String ENHANCE_METHOD = "getChannel";
+    public static final String ENHANCE_CLASS = "io.grpc.stub.AbstractStub";
 
     @Override
     protected ConstructorInterceptPoint[] getConstructorsInterceptPoints() {
