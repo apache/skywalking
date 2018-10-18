d743a904dae37d788c42017d602f6f52a90fc8cf
Liu-XinYuan <879928098@qq.com>
2018-10-18 18:40:58 +0800

    Revert "solve conflic"
    This reverts commit 6409f40f72161cd81800d8875afada7ba0c07eb4.

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/register/service/ServiceInstanceInventoryRegister.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/register/service/ServiceInstanceInventoryRegister.java
@@ -102,6 +102,15 @@ public class ServiceInstanceInventoryRegister implements IServiceInstanceInvento
 
     }
 
+    @Override public void heartBeat(int instanceID, long time) {
+        ServiceInstanceInventory serviceInstanceInventory = new ServiceInstanceInventory();
+        serviceInstanceInventory.setServiceId(instanceID);
+        serviceInstanceInventory.setHeartbeatTime(time);
+
+        InventoryProcess.INSTANCE.in(serviceInstanceInventory);
+
+
+    }
 
     @Override public void heartbeat(int serviceInstanceId, long heartBeatTime) {
         ServiceInstanceInventory serviceInstanceInventory = getServiceInstanceInventoryCache().get(serviceInstanceId);
