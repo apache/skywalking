87f7fb2198aca9574ced9e0b9fb8ab92d7f28d36
Jared.Tan <jian.tan@daocloud.io>
2018-12-06 15:22:33 +0800

    fix conflicts and test case.

--- apm-sniffer/optional-plugins/trace-ignore-plugin/src/main/java/org/apache/skywalking/apm/plugin/trace/ignore/conf/IgnoreConfigInitializer.java
+++ apm-sniffer/optional-plugins/trace-ignore-plugin/src/main/java/org/apache/skywalking/apm/plugin/trace/ignore/conf/IgnoreConfigInitializer.java
@@ -18,28 +18,15 @@
 
 package org.apache.skywalking.apm.plugin.trace.ignore.conf;
 
-import org.apache.skywalking.apm.agent.core.boot.AgentPackageNotFoundException;
-import org.apache.skywalking.apm.agent.core.boot.AgentPackagePath;
+import java.io.*;
+import java.util.*;
+import org.apache.skywalking.apm.agent.core.boot.*;
 import org.apache.skywalking.apm.agent.core.conf.ConfigNotFoundException;
-import org.apache.skywalking.apm.agent.core.logging.api.ILog;
-import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
-import org.apache.skywalking.apm.util.ConfigInitializer;
-import org.apache.skywalking.apm.util.PlaceholderConfigurerSupport;
-import org.apache.skywalking.apm.util.PropertyPlaceholderHelper;
-import org.apache.skywalking.apm.util.StringUtil;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
+import org.apache.skywalking.apm.agent.core.logging.api.*;
+import org.apache.skywalking.apm.util.*;
 
 /**
- *
  * @author liujc [liujunc1993@163.com]
- *
  */
 public class IgnoreConfigInitializer {
     private static final ILog LOGGER = LogManager.getLogger(IgnoreConfigInitializer.class);
@@ -47,7 +34,8 @@ public class IgnoreConfigInitializer {
     private static String ENV_KEY_PREFIX = "skywalking.";
 
     /**
-     * Try to locate `apm-trace-ignore-plugin.config`, which should be in the /optional-plugins/apm-trace-ignore-plugin/ dictionary of agent package.
+     * Try to locate `apm-trace-ignore-plugin.config`, which should be in the /optional-plugins/apm-trace-ignore-plugin/
+     * dictionary of agent package.
      * <p>
      * Also try to override the config by system.env and system.properties. All the keys in these two places should
      * start with {@link #ENV_KEY_PREFIX}. e.g. in env `skywalking.trace.ignore_path=your_path` to override
@@ -60,17 +48,11 @@ public class IgnoreConfigInitializer {
             configFileStream = loadConfigFromAgentFolder();
             Properties properties = new Properties();
             properties.load(configFileStream);
-            PropertyPlaceholderHelper helper =
-                    new PropertyPlaceholderHelper(PlaceholderConfigurerSupport.DEFAULT_PLACEHOLDER_PREFIX,
-                            PlaceholderConfigurerSupport.DEFAULT_PLACEHOLDER_SUFFIX,
-                            PlaceholderConfigurerSupport.DEFAULT_VALUE_SEPARATOR, true);
+            PropertyPlaceholderHelper helper = PropertyPlaceholderHelper.INSTANCE;
             for (String key : properties.stringPropertyNames()) {
                 String value = (String)properties.get(key);
                 //replace the key's value. properties.replace(key,value) in jdk8+
-                value = helper.replacePlaceholders(value, properties);
-                if (!StringUtil.isEmpty(value)) {
-                    properties.put(key, helper.replacePlaceholders(value, properties));
-                }
+                properties.put(key, PropertyPlaceholderHelper.INSTANCE.replacePlaceholders(value, properties));
             }
             ConfigInitializer.initialize(properties, IgnoreConfig.class);
         } catch (Exception e) {
@@ -78,13 +60,13 @@ public class IgnoreConfigInitializer {
         }
 
         try {
-            overrideConfigBySystemEnv();
+            overrideConfigBySystemProp();
         } catch (Exception e) {
             LOGGER.error(e, "Failed to read the system env.");
         }
     }
 
-    private static void overrideConfigBySystemEnv() throws IllegalAccessException {
+    private static void overrideConfigBySystemProp() throws IllegalAccessException {
         Properties properties = new Properties();
         Properties systemProperties = System.getProperties();
         Iterator<Map.Entry<Object, Object>> entryIterator = systemProperties.entrySet().iterator();
@@ -100,7 +82,6 @@ public class IgnoreConfigInitializer {
             ConfigInitializer.initialize(properties, IgnoreConfig.class);
         }
     }
-
 
     /**
      * Load the config file, where the agent jar is.
