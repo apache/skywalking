ec839d22f111791b025dfc1711a99b3244117d6a
Jared.Tan <jian.tan@daocloud.io>
2018-12-25 09:50:24 +0800

    fix logical.

---
+++ apm-sniffer/apm-sdk-plugin/spring-plugins/reactor-netty-http-client-0.7.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/reactor/netty/http/client/OnInboundNextInterceptor.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.skywalking.apm.plugin.reactor.netty.http.client;
+
+import io.netty.handler.codec.http.*;
+import java.lang.reflect.Method;
+import org.apache.skywalking.apm.agent.core.context.ContextManager;
+import org.apache.skywalking.apm.agent.core.context.tag.Tags;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.*;
+
+public class OnInboundNextInterceptor implements InstanceMethodsAroundInterceptor {
+    @Override
+    public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
+        MethodInterceptResult result) throws Throwable {
+        if (allArguments[0] != null && allArguments[1] != null) {
+            Object msg = allArguments[1];
+            if (msg instanceof HttpResponse) {
+                HttpResponse response = (HttpResponse)msg;
+                HttpResponseStatus status = response.status();
+                if (status != null && status.code() > 400) {
+                    ContextManager.activeSpan().errorOccurred();
+                    Tags.STATUS_CODE.set(ContextManager.activeSpan(), String.valueOf(status.code()));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
+        Object ret) throws Throwable {
+        HttpRequest request = (HttpRequest)ContextManager.getRuntimeContext().get("SW_NETTY_HTTP_CLIENT_REQUEST");
+        if (request != null) {
+            ContextManager.stopSpan();
+            ContextManager.getRuntimeContext().remove("SW_NETTY_HTTP_CLIENT_REQUEST");
+        }
+        return ret;
+    }
+
+    @Override public void handleMethodException(EnhancedInstance objInst, Method method, Object[] allArguments,
+        Class<?>[] argumentsTypes, Throwable t) {
+        ContextManager.activeSpan().errorOccurred().log(t);
+    }
+}
---
+++ apm-sniffer/apm-sdk-plugin/spring-plugins/reactor-netty-http-client-0.7.x-plugin/src/test/java/org/apache/skywalking/apm/plugin/reactor/netty/http/client/NettyHttpInterceptorTest.java
@@ -0,0 +1,104 @@
+package org.apache.skywalking.apm.plugin.reactor.netty.http.client;
+
+import io.netty.handler.codec.http.*;
+import java.util.List;
+import org.apache.skywalking.apm.agent.core.context.trace.*;
+import org.apache.skywalking.apm.agent.core.context.util.TagValuePair;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
+import org.apache.skywalking.apm.agent.test.helper.*;
+import org.apache.skywalking.apm.agent.test.tools.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+/**
+ * @author jian.tan
+ */
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(TracingSegmentRunner.class)
+public class NettyHttpInterceptorTest {
+    @SegmentStoragePoint
+    private SegmentStorage segmentStorage;
+
+    @Mock
+    private MockHttpClientOperations enhancedInstance;
+
+    @Mock
+    private HttpResponseStatus httpResponseStatus;
+
+    private HttpClientOperationsInterceptor httpClientOperationsInterceptor;
+
+    private OnOutboundCompleteInterceptor outboundCompleteInterceptor;
+
+    private OnInboundNextInterceptor onInboundNextInterceptor;
+
+    @Before
+    public void setUp() throws Exception {
+        enhancedInstance = new MockHttpClientOperations();
+
+        httpClientOperationsInterceptor = new HttpClientOperationsInterceptor();
+        outboundCompleteInterceptor = new OnOutboundCompleteInterceptor();
+        onInboundNextInterceptor = new OnInboundNextInterceptor();
+        when(httpResponseStatus.code()).thenReturn(200);
+
+    }
+
+    @Test
+    public void testInterceptor() throws Throwable {
+
+        httpClientOperationsInterceptor.onConstruct(enhancedInstance, null);
+        outboundCompleteInterceptor.afterMethod(null, null, null, null, null);
+        onInboundNextInterceptor.afterMethod(null, null, null, null, null);
+
+        assertThat(segmentStorage.getTraceSegments().size(), is(0));
+        TraceSegment traceSegment = segmentStorage.getTraceSegments().get(0);
+
+        Assert.assertEquals(1, SegmentHelper.getSpans(traceSegment).size());
+        AbstractTracingSpan finishedSpan = SegmentHelper.getSpans(traceSegment).get(0);
+
+        List<TagValuePair> tags = SpanHelper.getTags(finishedSpan);
+        assertThat(tags.size(), is(2));
+        assertThat(tags.get(0).getValue(), is("GET"));
+        assertThat(tags.get(1).getValue(), is(enhancedInstance.nettyRequest.uri()));
+
+        Assert.assertEquals(false, SpanHelper.getErrorOccurred(finishedSpan));
+    }
+
+    private class MockHttpRequest extends DefaultHttpRequest implements EnhancedInstance {
+
+        public MockHttpRequest(HttpVersion httpVersion, HttpMethod method, String uri) {
+            super(httpVersion, method, uri);
+        }
+
+        @Override public Object getSkyWalkingDynamicField() {
+            return null;
+        }
+
+        @Override public void setSkyWalkingDynamicField(Object value) {
+
+        }
+    }
+
+    private class MockHttpClientOperations implements EnhancedInstance {
+        private MockHttpRequest nettyRequest;
+
+        public MockHttpClientOperations() {
+            this.nettyRequest = new MockHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/localhost");
+        }
+
+        @Override public Object getSkyWalkingDynamicField() {
+            return null;
+        }
+
+        @Override public void setSkyWalkingDynamicField(Object value) {
+
+        }
+
+    }
+}
