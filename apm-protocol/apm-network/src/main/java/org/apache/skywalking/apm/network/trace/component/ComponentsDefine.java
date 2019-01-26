a36394d9bc2d66de37a50a01ea7fb0edecd5db3e
于玉桔 <769213327@qq.com>
2019-01-26 13:09:03 +0800

    Support lettuce plugin (#2152)
    * Support lettuce plugin
    
    * lettuce plugin bugfix
    
    * lettuce plugin fix license issue
    
    * lettuce plugin fix cluster and config issue
    
    * plugin lettuce compatible with low 5.0.x version
    
    * lettuce plugin support version accurately describes on Supported-list.md
    
    * plugin lettuce compatible with low 5.0.2 version
    
    * lettuce plugin fix wrong batch method intercept point
    
    * move lettuce to optional plugins
    
    * fix ci issue
    
    * fix ci issue
    
    * Modify the description file
    
    * Pom code optimization
    
    * Optimization operation name
    
    * Optimization operation name2

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -110,6 +110,8 @@ public class ComponentsDefine {
   
     public static final OfficialComponent REDISSON =  new OfficialComponent(56, "Redisson");
 
+    public static final OfficialComponent LETTUCE =  new OfficialComponent(57, "Lettuce");
+
     private static ComponentsDefine INSTANCE = new ComponentsDefine();
 
     private String[] components;
@@ -119,7 +121,7 @@ public class ComponentsDefine {
     }
 
     public ComponentsDefine() {
-        components = new String[57];
+        components = new String[58];
         addComponent(TOMCAT);
         addComponent(HTTPCLIENT);
         addComponent(DUBBO);
@@ -161,6 +163,7 @@ public class ComponentsDefine {
         addComponent(CANAL);
         addComponent(GSON);
         addComponent(REDISSON);
+        addComponent(LETTUCE);
     }
 
     private void addComponent(OfficialComponent component) {
