6bb0c10318b5fe61c075c33333433af23469c10f
彭勇升 pengys <8082209@qq.com>
2019-04-11 20:08:51 +0800

    Provide elasticsearch template operation method. (#2474)
    * Provide elasticsearch template operation method.
    
    * Fixed compile failure issue.

--- oap-server/server-library/library-client/src/test/java/org/apache/skywalking/oap/server/library/client/elasticsearch/ITElasticSearchClient.java
+++ oap-server/server-library/library-client/src/test/java/org/apache/skywalking/oap/server/library/client/elasticsearch/ITElasticSearchClient.java
@@ -18,29 +18,167 @@
 
 package org.apache.skywalking.oap.server.library.client.elasticsearch;
 
+import com.google.gson.JsonObject;
 import java.io.IOException;
-import org.elasticsearch.action.get.GetResponse;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.get.*;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.xcontent.*;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.*;
+import org.slf4j.*;
 
 /**
  * @author peng-yongsheng
  */
 public class ITElasticSearchClient {
 
-    @Test
-    public void test() throws IOException {
-        ElasticSearchClient client = new ElasticSearchClient("localhost:9200", null, null, null);
+    private static final Logger logger = LoggerFactory.getLogger(ITElasticSearchClient.class);
+
+    private ElasticSearchClient client;
+
+    @Before
+    public void before() throws IOException {
+        client = new ElasticSearchClient("localhost:9200", "", "test", "test");
         client.connect();
+    }
 
-        XContentBuilder builder = XContentFactory.jsonBuilder().startObject();
-        builder.field("key", "value");
-        builder.endObject();
-        client.forceInsert("test_index", "201904091521", builder);
+    @After
+    public void after() throws IOException {
+        client.shutdown();
+    }
 
-        GetResponse response = client.get("test_index", "201904091521");
+    @Test
+    public void indexOperate() throws IOException {
+        JsonObject settings = new JsonObject();
+        settings.addProperty("number_of_shards", 2);
+        settings.addProperty("number_of_replicas", 2);
 
-        Assert.assertTrue(response.getSource().containsKey("key"));
-        Assert.assertEquals("value", response.getSource().get("key"));
+        JsonObject mapping = new JsonObject();
+        mapping.add("_doc", new JsonObject());
+
+        JsonObject doc = mapping.getAsJsonObject("_doc");
+
+        JsonObject properties = new JsonObject();
+        doc.add("properties", properties);
+
+        JsonObject column = new JsonObject();
+        column.addProperty("type", "text");
+        properties.add("column1", column);
+
+        String indexName = "test_index_operate";
+        client.createIndex(indexName, settings, doc);
+        Assert.assertTrue(client.isExistsIndex(indexName));
+
+        JsonObject index = client.getIndex(indexName);
+        logger.info(index.toString());
+
+        Assert.assertEquals(2, index.getAsJsonObject(indexName).getAsJsonObject("settings").getAsJsonObject("index").get("number_of_shards").getAsInt());
+        Assert.assertEquals(2, index.getAsJsonObject(indexName).getAsJsonObject("settings").getAsJsonObject("index").get("number_of_replicas").getAsInt());
+
+        Assert.assertEquals("text", index.getAsJsonObject(indexName).getAsJsonObject("mappings").getAsJsonObject("type").getAsJsonObject("properties").getAsJsonObject("column1").get("type").getAsString());
+
+        Assert.assertTrue(client.deleteIndex(indexName));
+    }
+
+    @Test
+    public void documentOperate() throws IOException {
+        String id = String.valueOf(System.currentTimeMillis());
+
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field("user", "kimchy")
+            .field("post_date", "2009-11-15T14:12:12")
+            .field("message", "trying out Elasticsearch")
+            .endObject();
+
+        String indexName = "test_document_operate";
+        client.forceInsert(indexName, id, builder);
+
+        GetResponse response = client.get(indexName, id);
+        Assert.assertEquals("kimchy", response.getSource().get("user"));
+        Assert.assertEquals("trying out Elasticsearch", response.getSource().get("message"));
+
+        builder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field("user", "pengys")
+            .endObject();
+        client.forceUpdate(indexName, id, builder);
+
+        response = client.get(indexName, id);
+        Assert.assertEquals("pengys", response.getSource().get("user"));
+        Assert.assertEquals("trying out Elasticsearch", response.getSource().get("message"));
+
+        List<String> ids = new ArrayList<>();
+        ids.add(id);
+        MultiGetResponse responses = client.multiGet(indexName, ids);
+        Assert.assertEquals(1, responses.getResponses().length);
+        Assert.assertEquals("pengys", responses.getResponses()[0].getResponse().getSource().get("user"));
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.termQuery("user", "pengys"));
+        SearchResponse searchResponse = client.search(indexName, sourceBuilder);
+        Assert.assertEquals("trying out Elasticsearch", searchResponse.getHits().getHits()[0].getSourceAsMap().get("message"));
+    }
+
+    @Test
+    public void templateOperate() throws IOException {
+        JsonObject settings = new JsonObject();
+        settings.addProperty("number_of_shards", 1);
+        settings.addProperty("number_of_replicas", 0);
+        settings.addProperty("index.refresh_interval", "3s");
+        settings.addProperty("analysis.analyzer.oap_analyzer.type", "stop");
+
+        JsonObject mapping = new JsonObject();
+        mapping.add("type", new JsonObject());
+        JsonObject doc = mapping.getAsJsonObject("type");
+
+        JsonObject properties = new JsonObject();
+        doc.add("properties", properties);
+
+        JsonObject column = new JsonObject();
+        column.addProperty("type", "text");
+        properties.add("name", column);
+
+        String indexName = "template_operate";
+
+        client.createTemplate(indexName, settings, mapping);
+
+        Assert.assertTrue(client.isExistsTemplate(indexName));
+
+        XContentBuilder builder = XContentFactory.jsonBuilder().startObject()
+            .field("name", "pengys")
+            .endObject();
+        client.forceInsert(indexName + "_2019", "testid", builder);
+
+        JsonObject index = client.getIndex(indexName + "_2019");
+        logger.info(index.toString());
+        Assert.assertEquals(1, index.getAsJsonObject(indexName + "_2019").getAsJsonObject("settings").getAsJsonObject("index").get("number_of_shards").getAsInt());
+        Assert.assertEquals(0, index.getAsJsonObject(indexName + "_2019").getAsJsonObject("settings").getAsJsonObject("index").get("number_of_replicas").getAsInt());
+
+        client.deleteTemplate(indexName);
+        Assert.assertFalse(client.isExistsTemplate(indexName));
+    }
+
+    @Test
+    public void bulk() throws InterruptedException {
+        BulkProcessor bulkProcessor = client.createBulkProcessor(2000, 200, 10, 2);
+
+        Map<String, String> source = new HashMap<>();
+        source.put("column1", "value1");
+        source.put("column2", "value2");
+
+        for (int i = 0; i < 100; i++) {
+            IndexRequest indexRequest = new IndexRequest("bulk_insert_test", "type", String.valueOf(i));
+            indexRequest.source(source);
+            bulkProcessor.add(indexRequest);
+        }
+
+        bulkProcessor.flush();
+        bulkProcessor.awaitClose(2, TimeUnit.SECONDS);
     }
 }
