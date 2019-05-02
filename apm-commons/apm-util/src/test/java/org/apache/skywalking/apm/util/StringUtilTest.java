93d1037602d829100ed9d4b91a409e246f0490e3
Michael Hausegger <hausegger.michael@googlemail.com>
2019-05-02 16:40:02 +0200

    Added Unit Tests to increase code coverage

--- apm-commons/apm-util/src/test/java/org/apache/skywalking/apm/util/StringUtilTest.java
+++ apm-commons/apm-util/src/test/java/org/apache/skywalking/apm/util/StringUtilTest.java
@@ -39,5 +39,18 @@ public class StringUtilTest {
         Assert.assertNull(StringUtil.join('.'));
         Assert.assertEquals("Single part.", StringUtil.join('.', "Single part."));
         Assert.assertEquals("part1.part2.p3", StringUtil.join('.', "part1", "part2", "p3"));
+        Assert.assertEquals("E", StringUtil.join('E', new String[2]));
     }
+
+    @Test
+    public void testSubstringMatchReturningTrue() {
+        StringBuffer stringBuffer = new StringBuffer("ZP~>xz1;");
+        Assert.assertTrue(StringUtil.substringMatch(stringBuffer, 0, stringBuffer));
+    }
+
+    @Test
+    public void testSubstringMatchWithPositive() {
+        Assert.assertFalse(StringUtil.substringMatch("", 4770, ""));
+    }
+
 }
