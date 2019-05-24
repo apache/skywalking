1f6f6caff26556b852b9fc6cbbf06a58182968bb
FuCheng,Yan <yanfch.china@gmail.com>
2019-05-24 22:14:43 +0800

    Support RESTEasy 3.x (#2665)
    * feat(agent sdk): resteasy plugin
    
    * fix(agent sdk): fix component define
    
    * fix(agent sdk): fix file license
    
    * fix(agent sdk): fix file license
    
    * docs(agent sdk): add supported list
    
    * fix(agent sdk): fix file license
    
    * fix(agent sdk): fix response error tag
    
    * fix(agent sdk): fix
    
    * fix(agent sdk): adapter 3.5+
    
    * fix(agent sdk): remove unreasonable async handling

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -120,6 +120,8 @@ public class ComponentsDefine {
 
     public static final OfficialComponent SPRING_CLOUD_GATEWAY =  new OfficialComponent(61, "spring-cloud-gateway");
 
+    public static final OfficialComponent RESTEASY =  new OfficialComponent(62, "RESTEasy");
+
     private static ComponentsDefine INSTANCE = new ComponentsDefine();
 
     private String[] components;
@@ -176,6 +178,7 @@ public class ComponentsDefine {
         addComponent(ZOOKEEPER);
         addComponent(VERTX);
         addComponent(SPRING_CLOUD_GATEWAY);
+        addComponent(RESTEASY);
     }
 
     private void addComponent(OfficialComponent component) {
