a0c6b9d28638e3ce7dc8cc80dff7e19c72d315bd
dmsolr <zteny@foxmail.com>
2019-06-16 13:02:13 +0800

    modify the error message

--- tools/check/agent/plugin/PluginImportedCheck.sh
+++ tools/check/agent/plugin/PluginImportedCheck.sh
@@ -23,7 +23,7 @@ for dir in `ls "./apm-sniffer/$plugin_dir/"`; do
 	for f in `find ./apm-sniffer/$plugin_dir/$dir -name *Instrumentation.java `; do
 		NUM=`head -400 $f | grep ^import |grep -v net.bytebuddy. | grep -v org.apache.skywalking. |grep -v java.| wc -l`;
 		if [ $NUM -gt 0 ] ; then
-			echo "Plugin: $dir, never import any class unless JDK and ByteBuddy!";
+			echo "Plugin: $dir,  only allow to import JDK and ByteBuddy classes.";
 			exit 1;
 		fi
 	done
