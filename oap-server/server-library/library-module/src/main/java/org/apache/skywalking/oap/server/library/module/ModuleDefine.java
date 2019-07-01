0b5a44d634744d1dba8a0212e967e9216e3d134b
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2019-07-01 15:54:12 +0800

    Check duplicate providers in bootstrap (#2976)
    * Enhance module manager

--- oap-server/server-library/library-module/src/main/java/org/apache/skywalking/oap/server/library/module/ModuleDefine.java
+++ oap-server/server-library/library-module/src/main/java/org/apache/skywalking/oap/server/library/module/ModuleDefine.java
@@ -19,8 +19,11 @@
 package org.apache.skywalking.oap.server.library.module;
 
 import java.lang.reflect.Field;
-import java.util.*;
-import org.slf4j.*;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.ServiceLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A module definition.
@@ -31,7 +34,7 @@ public abstract class ModuleDefine implements ModuleProviderHolder {
 
     private static final Logger logger = LoggerFactory.getLogger(ModuleDefine.class);
 
-    private final LinkedList<ModuleProvider> loadedProviders = new LinkedList<>();
+    private ModuleProvider loadedProvider = null;
 
     private final String name;
 
@@ -60,39 +63,35 @@ public abstract class ModuleDefine implements ModuleProviderHolder {
      */
     void prepare(ModuleManager moduleManager, ApplicationConfiguration.ModuleConfiguration configuration,
         ServiceLoader<ModuleProvider> moduleProviderLoader) throws ProviderNotFoundException, ServiceNotProvidedException, ModuleConfigException, ModuleStartException {
-        boolean providerExist = false;
         for (ModuleProvider provider : moduleProviderLoader) {
             if (!configuration.has(provider.name())) {
                 continue;
             }
 
-            providerExist = true;
             if (provider.module().equals(getClass())) {
-                ModuleProvider newProvider;
-                try {
-                    newProvider = provider.getClass().newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    throw new ProviderNotFoundException(e);
+                if (loadedProvider == null) {
+                    loadedProvider = provider;
+                    loadedProvider.setManager(moduleManager);
+                    loadedProvider.setModuleDefine(this);
+                } else {
+                    throw new DuplicateProviderException(this.name() + " module has one " + loadedProvider.name() + "[" + loadedProvider.getClass().getName() + "] provider already, "
+                        + provider.name() + "[" + provider.getClass().getName() + "] is defined as 2nd provider.");
                 }
-                newProvider.setManager(moduleManager);
-                newProvider.setModuleDefine(this);
-                loadedProviders.add(newProvider);
             }
+
         }
 
-        if (!providerExist) {
+        if (loadedProvider == null) {
             throw new ProviderNotFoundException(this.name() + " module no provider exists.");
         }
 
-        for (ModuleProvider moduleProvider : loadedProviders) {
-            logger.info("Prepare the {} provider in {} module.", moduleProvider.name(), this.name());
-            try {
-                copyProperties(moduleProvider.createConfigBeanIfAbsent(), configuration.getProviderConfiguration(moduleProvider.name()), this.name(), moduleProvider.name());
-            } catch (IllegalAccessException e) {
-                throw new ModuleConfigException(this.name() + " module config transport to config bean failure.", e);
-            }
-            moduleProvider.prepare();
+        logger.info("Prepare the {} provider in {} module.", loadedProvider.name(), this.name());
+        try {
+            copyProperties(loadedProvider.createConfigBeanIfAbsent(), configuration.getProviderConfiguration(loadedProvider.name()), this.name(), loadedProvider.name());
+        } catch (IllegalAccessException e) {
+            throw new ModuleConfigException(this.name() + " module config transport to config bean failure.", e);
         }
+        loadedProvider.prepare();
     }
 
     private void copyProperties(ModuleConfig dest, Properties src, String moduleName,
@@ -129,20 +128,11 @@ public abstract class ModuleDefine implements ModuleProviderHolder {
         throw new NoSuchFieldException();
     }
 
-    /**
-     * @return providers of this module
-     */
-    final List<ModuleProvider> providers() {
-        return loadedProviders;
-    }
-
     @Override public final ModuleProvider provider() throws DuplicateProviderException, ProviderNotFoundException {
-        if (loadedProviders.size() > 1) {
-            throw new DuplicateProviderException(this.name() + " module exist " + loadedProviders.size() + " providers");
-        } else if (loadedProviders.size() == 0) {
+        if (loadedProvider == null) {
             throw new ProviderNotFoundException("There is no module provider in " + this.name() + " module!");
         }
 
-        return loadedProviders.getFirst();
+        return loadedProvider;
     }
 }
