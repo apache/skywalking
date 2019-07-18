5b039e9747fac98444b4ec423a5d57524202f286
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2019-07-18 19:47:23 +0800

    All NEW OAL Runtime Engine (#3109)
    * Change OAL generator tool to runtime.
    
    Step 1. change project structure. API links and maven pom.
    
    * Part of metrics class generation
    
    * Metrcis class generated.
    
    * Set up the basic structure of new OAL engine.
    
    * Finish metrics generation.
    
    * Support dispatcher generation.
    
    * Format codes.
    
    * Generate dispatcher all methods.
    
    * Implement disable in hardcode.
    
    * Clear up
    
    * Fix compile startup.
    
    * Update license and document of new OAL engine.
    
    * Fix rat.
    
    * Disable class output in default
    
    * Copy from @kezhenxu94 's test PR, https://github.com/apache/skywalking/pull/3110
    
    * Wait for the mount command to finish

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/metrics/expression/EqualMatch.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/metrics/expression/EqualMatch.java
@@ -19,12 +19,50 @@
 package org.apache.skywalking.oap.server.core.analysis.metrics.expression;
 
 /**
- *
- *
  * @author wusheng
  */
-public class EqualMatch extends BinaryMatchExpression {
-    @Override public boolean match() {
+public class EqualMatch {
+    public boolean match(int left, int right) {
+        return left == right;
+    }
+
+    public boolean match(long left, long right) {
+        return left == right;
+    }
+
+    public boolean match(float left, float right) {
+        return left == right;
+    }
+
+    public boolean match(double left, double right) {
+        return left == right;
+    }
+
+    public boolean match(Integer left, Integer right) {
+        return left == right;
+    }
+
+    public boolean match(Long left, Long right) {
+        return left == right;
+    }
+
+    public boolean match(Float left, Float right) {
+        return left == right;
+    }
+
+    public boolean match(Double left, Double right) {
+        return left == right;
+    }
+
+    public boolean match(Boolean left, Boolean right) {
+        return left == right;
+    }
+
+    public boolean match(boolean left, boolean right) {
+        return left == right;
+    }
+
+    public boolean match(Object left, Object right) {
         return left.equals(right);
     }
 }
