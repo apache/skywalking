34366234cbd691c9d437d5090fdcda501eed3d11
田雪峰 <tianxuefeng@shuidihuzhu.com>
2019-08-10 16:33:10 +0800

    update format

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/remote/GRPCChannelManager.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/remote/GRPCChannelManager.java
@@ -117,7 +117,7 @@ public class GRPCChannelManager implements BootService, Runnable {
                             .addChannelDecorator(new AuthenticationDecorator())
                             .build();
                         notify(GRPCChannelStatus.CONNECTED);
-                        reconnectCount =0;
+                        reconnectCount = 0;
                         reconnect = false;
                     } else if (managedChannel.isConnected(++reconnectCount > Config.Agent.MAX_WAIT_CHANNEL_AUTO_CONNECT_COUNT)) {
                         // Reconnect to the same server is automatically done by GRPC,
