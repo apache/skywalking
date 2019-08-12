1344887d2a754c0b7e6c045cc6a1d348f8c737a6
田雪峰 <tianxuefeng@shuidihuzhu.com>
2019-08-12 21:10:10 +0800

    add license

--- apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/logging/core/PatternLoggerTest.java
+++ apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/logging/core/PatternLoggerTest.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package org.apache.skywalking.apm.agent.core.logging.core;
 
 import com.google.common.collect.Lists;
