a0e816771c3fcd2e1bc66ca60ecc738e7a579be6
于玉桔 <zhaoyuguang@apache.org>
2019-08-13 22:52:01 +0800

    Webflux plugin optimization (#3243)
    * webflux

--- apm-sniffer/apm-sdk-plugin/spring-plugins/webflux-5.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/spring/webflux/v5/DefaultServerWebExchangeConstructorInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/spring-plugins/webflux-5.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/spring/webflux/v5/DefaultServerWebExchangeConstructorInterceptor.java
@@ -6,25 +6,27 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
+package org.apache.skywalking.apm.plugin.spring.webflux.v5;
 
-package org.apache.skywalking.apm.plugin.spring.webflux.v5.define;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceConstructorInterceptor;
 
-import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.ClassInstanceMethodsEnhancePluginDefine;
-
-public abstract class AbstractSpringWebflux5Instrumentation extends ClassInstanceMethodsEnhancePluginDefine {
-    public static final String WITHNESS_CLASSES = "org.springframework.web.reactive.BindingContext";
-
+/**
+ *
+ * @author zhaoyuguang
+ */
+public class DefaultServerWebExchangeConstructorInterceptor implements InstanceConstructorInterceptor {
     @Override
-    protected final String[] witnessClasses() {
-        return new String[] {WITHNESS_CLASSES};
+    public void onConstruct(EnhancedInstance objInst, Object[] allArguments) {
     }
 }
