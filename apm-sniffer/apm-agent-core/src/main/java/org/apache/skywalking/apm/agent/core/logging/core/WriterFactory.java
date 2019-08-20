0730f9e16d91ec881b7c6c1e5be611f1d908b9eb
liuweiyi <liuweiyi@cmss.chinamobile.com>
2019-08-20 12:07:59 +0800

    Give the default writer.

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/logging/core/WriterFactory.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/logging/core/WriterFactory.java
@@ -48,6 +48,8 @@ public class WriterFactory {
                         }
                     }
                     WRITER = FileWriter.get();
+                } else {
+                    return SystemOutWriter.INSTANCE;
                 }
                 break;
             default:
