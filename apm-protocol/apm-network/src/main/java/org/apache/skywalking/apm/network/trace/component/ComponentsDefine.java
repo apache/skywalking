1523fda79e841b228903bf94cddfe97f13c7ed80
stone <stone.wlg@gmail.com>
2019-08-30 09:07:05 +0800

    remove cassandra-java-driver constant

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -130,6 +130,6 @@ public class ComponentsDefine {
 
     public static final OfficialComponent SPRING_WEBFLUX = new OfficialComponent(67, "spring-webflux");
 
-    public static final OfficialComponent CASSANDRA_JAVA_DRIVER = new OfficialComponent(68, "cassandra-java-driver");
+    public static final OfficialComponent CASSANDRA_JAVA_DRIVER = new OfficialComponent(69, "cassandra-java-driver");
 
 }
