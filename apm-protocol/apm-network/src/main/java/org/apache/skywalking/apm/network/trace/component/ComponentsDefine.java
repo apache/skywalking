ec7b75299ec4fe8f073252f57fed429e5ecd787b
lipenghui <penghui@apache.org>
2019-09-19 14:29:12 +0800

    Add Pulsar to ComponentsDefine and component-libraries.yml

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -136,7 +136,9 @@ public class ComponentsDefine {
   
     public static final OfficialComponent LIGHT_4J = new OfficialComponent(71, "Light4J");
 
-    public static final OfficialComponent PULSAR_PRODUCER = new OfficialComponent(72, "pulsar-producer");
+    public static final OfficialComponent PULSAR = new OfficialComponent(72, "Pulsar");
 
-    public static final OfficialComponent PULSAR_CONSUMER = new OfficialComponent(73, "pulsar-consumer");
+    public static final OfficialComponent PULSAR_PRODUCER = new OfficialComponent(73, "pulsar-producer");
+
+    public static final OfficialComponent PULSAR_CONSUMER = new OfficialComponent(74, "pulsar-consumer");
 }
