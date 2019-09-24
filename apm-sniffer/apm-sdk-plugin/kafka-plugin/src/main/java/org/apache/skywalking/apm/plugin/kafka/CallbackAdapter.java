8de9f7d74ce2429391f92afbfc86360ffb529746
Stalary <stalary@163.com>
2019-09-24 10:21:33 +0800

    MOD: Kafka-plugin Compatible with KafkaTemplate (#3505)
    * MOD: Compatible with KafkaTemplate
    
    * MOD: modify test

--- apm-sniffer/apm-sdk-plugin/kafka-plugin/src/main/java/org/apache/skywalking/apm/plugin/kafka/CallbackAdapter.java
+++ apm-sniffer/apm-sdk-plugin/kafka-plugin/src/main/java/org/apache/skywalking/apm/plugin/kafka/CallbackAdapter.java
@@ -15,26 +15,48 @@
  * limitations under the License.
  *
  */
-
 package org.apache.skywalking.apm.plugin.kafka;
 
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
-import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceConstructorInterceptor;
-import org.apache.skywalking.apm.util.StringUtil;
-
-import java.util.Map;
 
 /**
+ * implements Callback and EnhancedInstance, for transformation kafkaTemplate.buildCallback
  * @author stalary
  */
-public class ProducerConstructorMapInterceptor implements InstanceConstructorInterceptor {
+public class CallbackAdapter implements Callback, EnhancedInstance {
+
+    private Object instance;
+
+    private Callback userCallback;
+
+    public CallbackAdapter(Callback userCallback, Object instance) {
+        this.userCallback = userCallback;
+        this.instance = instance;
+    }
+
+    public CallbackAdapter() {
+    }
 
     @Override
-    public void onConstruct(EnhancedInstance objInst, Object[] allArguments) {
-        Map<String, Object> config = (Map<String, Object>) allArguments[0];
-        // prevent errors caused by secondary interception in kafkaTemplate
-        if (objInst.getSkyWalkingDynamicField() == null) {
-            objInst.setSkyWalkingDynamicField(StringUtil.join(';', ((String) config.get("bootstrap.servers")).split(",")));
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if (userCallback != null) {
+            userCallback.onCompletion(metadata, exception);
         }
+    }
+
+    @Override
+    public Object getSkyWalkingDynamicField() {
+        return instance;
+    }
+
+    @Override
+    public void setSkyWalkingDynamicField(Object value) {
+        this.instance = value;
+    }
+
+    public Callback getUserCallback() {
+        return userCallback;
     }
 }
