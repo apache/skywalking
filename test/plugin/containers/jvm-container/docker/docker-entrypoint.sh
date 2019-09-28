8d27386885f4e1e26286918c7022a444af7970fa
dmsolr <zteny@foxmail.com>
2019-09-28 17:50:01 +0800

    specify interpreter's command must be at the top of file

--- test/plugin/containers/jvm-container/docker/docker-entrypoint.sh
+++ test/plugin/containers/jvm-container/docker/docker-entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/sh
 /usr/local/skywalking-agent-scenario/skywalking-mock-collector/collector-startup.sh &
 sleep 30
 # start applications
