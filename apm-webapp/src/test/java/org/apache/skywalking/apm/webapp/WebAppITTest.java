edcfc3a3ad4dd3196d0e98e3d93d2daf0dd571e2
kezhenxu94 <kezhenxu94@163.com>
2019-10-02 21:02:06 +0800

    Fix unit tests

--- apm-webapp/src/test/java/org/apache/skywalking/apm/webapp/WebAppITTest.java
+++ apm-webapp/src/test/java/org/apache/skywalking/apm/webapp/WebAppITTest.java
@@ -17,14 +17,20 @@
 
 package org.apache.skywalking.apm.webapp;
 
+import org.apache.skywalking.apm.webapp.proxy.NotFoundHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -38,19 +44,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class WebAppITTest {
     @Autowired
     private MockMvc mvc;
+    @MockBean
+    private NotFoundHandler notFoundHandler;
 
     @Test
     public void shouldGetStaticResources() throws Exception {
+        when(notFoundHandler.renderDefaultPage()).thenCallRealMethod();
+
         mvc.perform(get("/index.html"))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().string(containsString("<title>SkyWalking</title>")));
+
+        verify(notFoundHandler, never()).renderDefaultPage();
     }
 
     @Test
     public void shouldRedirectToIndexWhenResourcesIsAbsent() throws Exception {
+        when(notFoundHandler.renderDefaultPage()).thenCallRealMethod();
+
         mvc.perform(get("/absent.html"))
             .andDo(print())
-            .andExpect(status().isNotFound());
+            .andExpect(status().isOk());
+
+        verify(notFoundHandler, only()).renderDefaultPage();
     }
 }
