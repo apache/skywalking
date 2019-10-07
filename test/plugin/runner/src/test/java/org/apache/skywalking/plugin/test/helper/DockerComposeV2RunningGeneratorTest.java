def2e48c9ca541bb2bf220b55b7c6ceca458f3cc
dmsolr <zteny@foxmail.com>
2019-10-07 14:36:46 +0800

    checkstyle

--- test/plugin/runner/src/test/java/org/apache/skywalking/plugin/test/helper/DockerComposeV2RunningGeneratorTest.java
+++ test/plugin/runner/src/test/java/org/apache/skywalking/plugin/test/helper/DockerComposeV2RunningGeneratorTest.java
@@ -1,14 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.skywalking.plugin.test.helper;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import freemarker.template.Configuration;
-import freemarker.template.TemplateExceptionHandler;
-import org.apache.skywalking.plugin.test.helper.exception.ConfigureFileNotFoundException;
 import org.apache.skywalking.plugin.test.helper.exception.GenerateFailedException;
 import org.apache.skywalking.plugin.test.helper.vo.CaseConfiguration;
 import org.apache.skywalking.plugin.test.helper.vo.CaseIConfigurationTest;
-import org.apache.skywalking.plugin.test.helper.vo.DockerService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,22 +28,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
-/**
- * Author Daming
- * Email zteny@foxmail.com
- **/
 @RunWith(MockitoJUnitRunner.class)
 public class DockerComposeV2RunningGeneratorTest {
     private DockerComposeV2RunningGenerator dockerComposeRunningGenerator;
