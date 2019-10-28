a54708b1e596655fdfd35848b3a1581cc0a2bd0e
aderm <zhusheng_xu@163.com>
2019-10-28 15:27:15 +0800

    Refactor:extract common module postgresql-common (#3725)
    * refactor:extract common module postgresql-common
    
    * remove sql param tag.
    
    * delete the deploy plugin

--- apm-sniffer/apm-sdk-plugin/postgresql-common/src/main/java/org/apache/skywalking/apm/plugin/jdbc/postgresql/interceptor/StatementExecuteMethodsInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/postgresql-common/src/main/java/org/apache/skywalking/apm/plugin/jdbc/postgresql/interceptor/StatementExecuteMethodsInterceptor.java
@@ -16,7 +16,7 @@
  *
  */
 
-package org.apache.skywalking.apm.plugin.jdbc.postgresql;
+package org.apache.skywalking.apm.plugin.jdbc.postgresql.interceptor;
 
 import java.lang.reflect.Method;
 import org.apache.skywalking.apm.agent.core.context.ContextManager;
