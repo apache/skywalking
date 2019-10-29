7e7987c76f72b77d403dcf935906a3b5de03ef9a
Mrproliu <mrproliu@lagou.com>
2019-10-29 15:20:48 +0800

    change socket io server and client start to health check servlet

--- test/plugin/scenarios/netty-socketio-scenario/src/main/java/org/apache/skywalking/apm/testcase/netty/socketio/SocketIOStarter.java
+++ test/plugin/scenarios/netty-socketio-scenario/src/main/java/org/apache/skywalking/apm/testcase/netty/socketio/SocketIOStarter.java
@@ -17,12 +17,8 @@
 
 package org.apache.skywalking.apm.testcase.netty.socketio;
 
-import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -44,6 +40,9 @@ public class SocketIOStarter {
     public static Socket client;
 
     public static void startServer() {
+        if (server != null) {
+            return;
+        }
         Configuration config = new Configuration();
         config.setHostname("localhost");
         config.setPort(SERVER_PORT);
@@ -56,6 +55,9 @@ public class SocketIOStarter {
     }
 
     public static void startClientAndWaitConnect() throws URISyntaxException, InterruptedException {
+        if (client != null) {
+            return;
+        }
         client = IO.socket("http://localhost:" + SERVER_PORT);
         LinkedBlockingQueue<Boolean> connected = new LinkedBlockingQueue<>(1);
         client.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
