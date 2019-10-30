6b45a91fb40b5ad92b3882d98eef58b3a6721133
zhangwei <pknfe@outlook.com>
2019-10-30 11:36:16 +0800

    toolkit-scenario (#3739)

---
+++ test/plugin/scenarios/apm-toolkit-trace-scenario/src/main/java/test/org/apache/skywalking/apm/testcase/toolkit/controller/TestService.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package test.org.apache.skywalking.apm.testcase.toolkit.controller;
+
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+import org.apache.skywalking.apm.toolkit.trace.CallableWrapper;
+import org.apache.skywalking.apm.toolkit.trace.RunnableWrapper;
+import org.apache.skywalking.apm.toolkit.trace.Trace;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author caoyixiong
+ */
+@Component
+public class TestService {
+
+    private static final ExecutorService SERVICE = Executors.newSingleThreadExecutor(r -> {
+        Thread thread = new Thread(r);
+        thread.setDaemon(true);
+        return thread;
+    });
+
+    @Trace
+    public void testTag() {
+        ActiveSpan.tag("key", "value");
+    }
+
+    @Trace
+    public void testError() {
+        ActiveSpan.error();
+    }
+
+    @Trace
+    public void testErrorMsg() {
+        ActiveSpan.error("TestErrorMsg");
+    }
+
+    @Trace
+    public void testErrorThrowable() {
+        ActiveSpan.error(new RuntimeException("Test-Exception"));
+    }
+
+    @Trace
+    public void testDebug() {
+        ActiveSpan.debug("TestDebugMsg");
+    }
+
+    @Trace
+    public void testInfo() {
+        ActiveSpan.info("TestInfoMsg");
+    }
+
+
+    public void asyncRunnable(Runnable runnable) {
+        SERVICE.submit(RunnableWrapper.of(runnable));
+    }
+
+    public void asyncCallable(Callable<Boolean> callable) {
+        SERVICE.submit(CallableWrapper.of(callable));
+    }
+}
