403736781542e9c07a58012f70ae7fede5834354
赵禹光 <769213327@qq.com>
2019-11-04 20:58:24 +0800

    add comment

--- apm-sniffer/apm-sdk-plugin/redisson-3.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/redisson/v3/util/ClassUtil.java
+++ apm-sniffer/apm-sdk-plugin/redisson-3.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/redisson/v3/util/ClassUtil.java
@@ -24,6 +24,10 @@ import java.lang.reflect.Field;
  */
 public class ClassUtil {
 
+    /**
+     * This method should only be used in low frequency.
+     * It should not use in trace context, but just in the metadata preparation stage.
+     */
     public static Object getObjectField(Object obj, String name) throws NoSuchFieldException, IllegalAccessException {
         Field field = obj.getClass().getDeclaredField(name);
         field.setAccessible(true);
