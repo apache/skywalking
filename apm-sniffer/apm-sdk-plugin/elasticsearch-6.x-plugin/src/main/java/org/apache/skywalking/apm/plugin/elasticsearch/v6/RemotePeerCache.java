7a342f6a684c75ec9f4a56a7fce129756cce0b6c
aderm <zhusheng_xu@163.com>
2019-11-07 17:01:32 +0800

    Add elasticsearch 6.x plugin & elasticsearch 6.x test scenario

---
+++ apm-sniffer/apm-sdk-plugin/elasticsearch-6.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/elasticsearch/v6/RemotePeerCache.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.apache.skywalking.apm.plugin.elasticsearch.v6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author aderm
+ */
+public class RemotePeerCache {
+
+    private List<String> remotePeerList = Collections.synchronizedList(new ArrayList<String>());
+    private String remotePeers = "";
+
+    public void addRemotePeer(String host, int port) {
+        String hostPort = host + ":" + String.valueOf(port);
+        remotePeerList.add(hostPort);
+
+        if (remotePeers.isEmpty()) {
+            remotePeers = hostPort;
+        } else {
+            remotePeers = "," + hostPort;
+        }
+    }
+
+    public String getRemotePeers() {
+        return remotePeers;
+    }
+}
