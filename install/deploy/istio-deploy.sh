947bd4bb21d97302216b28266e59101473263d76
innerpeacez <innerpeace.zhai@gmail.com>
2019-11-21 19:57:31 +0800

    timeout test

--- install/deploy/istio-deploy.sh
+++ install/deploy/istio-deploy.sh
@@ -76,7 +76,7 @@ deploy_istio() {
   check() {
      kubectl -n ${NAMESPACE}  get deploy | grep istio | awk '{print "deployment/"$1}' | while read line ;
      do
-       kubectl describe deploy $line -n ${NAMESPACE}
+       kubectl describe deploy `echo $line | sed 's/deployment\///g'` -n ${NAMESPACE}
        kubectl rollout status $line -n ${NAMESPACE} --timeout 5m
      done
   }
