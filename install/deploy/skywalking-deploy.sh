84e16cb31aee96564f89dcca5031e4fb8a5a571a
innerpeacez <innerpeace.zhai@gmail.com>
2019-11-26 17:15:42 +0800

    test es

--- install/deploy/skywalking-deploy.sh
+++ install/deploy/skywalking-deploy.sh
@@ -47,7 +47,7 @@ and_stable_repo
 
 helm dep up skywalking
 
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
 
 helm -n $DPELOY_NAMESPACE install skywalking skywalking --set oap.istio.adapter.enabled=$MIXER_ENABLED \
         --set oap.envoy.als.enabled=$ALS_ENABLED --set oap.replicas=1
