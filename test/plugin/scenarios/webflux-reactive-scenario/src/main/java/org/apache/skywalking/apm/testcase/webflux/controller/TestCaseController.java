300afb99325c0ed1170211f0999045fd45b4caee
Born <jing23501@qq.com>
2019-11-28 16:35:22 +0800

    cleanup pom

--- test/plugin/scenarios/webflux-reactive-scenario/src/main/java/org/apache/skywalking/apm/testcase/webflux/controller/TestCaseController.java
+++ test/plugin/scenarios/webflux-reactive-scenario/src/main/java/org/apache/skywalking/apm/testcase/webflux/controller/TestCaseController.java
@@ -19,20 +19,9 @@ package org.apache.skywalking.apm.testcase.webflux.controller;
 
 import java.io.IOException;
 import java.util.Objects;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
