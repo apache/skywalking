d5f01a62dd712faf4db30b19158ff3db62a99bd3
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2019-12-15 19:15:47 +0800

    Remove the local/exit span register mechanism in Java agent scenario (#4059)
    * Remove the local span and exit span register mechanism in Java agent scenario.
    
    * Fix config, plugin, and document, as exit span is not required register anymore.
    
    * Try to fix the test case expected data due to no register for exit span.
    
    * Fix toolkit case.
    
    * Remove exit span from plugin test framework document.
    
    * Update protocol, FAQ, and CHANGELOG documents to highlight this change.
    
    * Fix NPE when no entry span in the whole segment. Set endpoint to null in segment entity.
    
    * Fix the snapshot and add inexistence operation name id.
    
    * Fix several async cases.
    
    * Fix undertow test case.
    
    * Fix wrong error log name.
    
    * Keep name consistent with 6.x concepts.
    
    * Try to fix 3 async scenarios.
    
    * Fix okhttp case.
    
    * Fix Spring Async case.
    
    * Fix another NPE
    
    * Fix NPE when first span has no endpoint id when it is local or exit span.
    
    * Still fill first operation name when no endpoint id.

--- test/plugin/validator/src/main/java/org/apache/skywalking/plugin/test/agent/tool/validator/assertor/SegmentRefAssert.java
+++ test/plugin/validator/src/main/java/org/apache/skywalking/plugin/test/agent/tool/validator/assertor/SegmentRefAssert.java
@@ -57,17 +57,17 @@ public class SegmentRefAssert {
 
     private static boolean segmentRefEquals(SegmentRef expected, SegmentRef actual) {
         try {
-            ExpressParser.parse(expected.entryServiceName()).assertValue("entry service name", actual.entryServiceName());
+            ExpressParser.parse(expected.entryEndpointName()).assertValue("entry endpoint name", actual.entryEndpointName());
             ExpressParser.parse(expected.networkAddress()).assertValue("network address", actual.networkAddress());
             ExpressParser.parse(expected.parentTraceSegmentId()).assertValue("parent segment id", actual.parentTraceSegmentId());
             ExpressParser.parse(expected.parentSpanId()).assertValue("span id", actual.parentSpanId());
-            ExpressParser.parse(expected.entryServiceId()).assertValue("entry service id", actual.entryServiceId());
+            ExpressParser.parse(expected.entryEndpointId()).assertValue("entry endpoint id", actual.entryEndpointId());
             ExpressParser.parse(expected.networkAddressId()).assertValue("network address id", actual.networkAddressId());
-            ExpressParser.parse(expected.parentApplicationInstanceId()).assertValue("parent application instance id", actual.parentApplicationInstanceId());
-            ExpressParser.parse(expected.parentServiceId()).assertValue("parent service id", actual.parentServiceId());
-            ExpressParser.parse(expected.parentServiceName()).assertValue("parent service name", actual.parentServiceName());
+            ExpressParser.parse(expected.parentServiceInstanceId()).assertValue("parent application instance id", actual.parentServiceInstanceId());
+            ExpressParser.parse(expected.parentEndpointId()).assertValue("parent endpoint id", actual.parentEndpointId());
+            ExpressParser.parse(expected.parentEndpointName()).assertValue("parent endpoint name", actual.parentEndpointName());
             ExpressParser.parse(expected.refType()).assertValue("ref type", actual.refType());
-            ExpressParser.parse(expected.entryApplicationInstanceId()).assertValue("entry application instance id", actual.entryApplicationInstanceId());
+            ExpressParser.parse(expected.entryServiceInstanceId()).assertValue("entry application instance id", actual.entryServiceInstanceId());
             return true;
         } catch (ValueAssertFailedException e) {
             throw new SegmentRefAssertFailedException(e, expected, actual);
