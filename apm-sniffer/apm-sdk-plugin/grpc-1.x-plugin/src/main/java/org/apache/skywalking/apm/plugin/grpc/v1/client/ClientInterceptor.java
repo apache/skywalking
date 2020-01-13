9f3c9e1ab09f0cb23cf936d1ca9fd8f098bfb00d
higan <higan@live.cn>
2020-01-13 21:57:48 +0800

    Remove simple tracing

--- apm-sniffer/apm-sdk-plugin/grpc-1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/grpc/v1/client/ClientInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/grpc-1.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/grpc/v1/client/ClientInterceptor.java
@@ -22,56 +22,15 @@ import io.grpc.CallOptions;
 import io.grpc.Channel;
 import io.grpc.ClientCall;
 import io.grpc.MethodDescriptor;
-import org.apache.skywalking.apm.agent.core.conf.Config;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
- * {@link ClientInterceptor} determines the returned Interceptor based on the client tracing config.
- * If the peer tracing enable, {@link ClientInterceptor} returns {@link TracingClientCall}, or it returns
- * {@link SimpleTracingClientCall} for performance.
- * <p>
- * It is usefully for tracing external api which not include in your Skywalking system.
- *
  * @author zhang xin, kanro
  */
 public class ClientInterceptor implements io.grpc.ClientInterceptor {
-    private final Boolean defaultClientTracingEnable;
-    private final Set<String> includeClientTracingPeers;
-    private final Set<String> excludeClientTracingPeers;
-
-    ClientInterceptor() {
-        defaultClientTracingEnable = Config.Plugin.Grpc.DEFAULT_CLIENT_TRACING_ENABLE;
-        includeClientTracingPeers = new HashSet<>(Config.Plugin.Grpc.INCLUDED_CLIENT_TRACING_PEERS);
-        excludeClientTracingPeers = new HashSet<>(Config.Plugin.Grpc.EXCLUDED_CLIENT_TRACING_PEERS);
-    }
 
     @Override
     public <REQUEST, RESPONSE> ClientCall<REQUEST, RESPONSE> interceptCall(MethodDescriptor<REQUEST, RESPONSE> method,
                                                                            CallOptions callOptions, Channel channel) {
-        String peer = channel.authority();
-
-        if (defaultClientTracingEnable) {
-            if (includeClientTracingPeers.contains(peer)) {
-                return new TracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-            }
-
-            if (excludeClientTracingPeers.contains(peer)) {
-                return new SimpleTracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-            }
-
-            return new TracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-        } else {
-            if (excludeClientTracingPeers.contains(peer)) {
-                return new SimpleTracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-            }
-
-            if (includeClientTracingPeers.contains(peer)) {
-                return new TracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-            }
-
-            return new SimpleTracingClientCall<>(channel.newCall(method, callOptions), method, channel);
-        }
+        return new TracingClientCall<>(channel.newCall(method, callOptions), method, channel);
     }
 }
