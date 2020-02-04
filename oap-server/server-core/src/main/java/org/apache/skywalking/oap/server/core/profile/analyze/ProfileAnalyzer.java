4e17cce9674718f6c046e0f440e75e715e7b0fa8
mrproliu <741550557@qq.com>
2020-02-04 16:24:05 +0800

    provide thread stack analyzes (#4308)
    * provide thread analyzes
    
    Co-authored-by: 吴晟 Wu Sheng <wu.sheng@foxmail.com>

---
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/profile/analyze/ProfileAnalyzer.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.oap.server.core.profile.analyze;
+
+import org.apache.skywalking.oap.server.core.query.entity.ProfileAnalyzation;
+import org.apache.skywalking.oap.server.core.query.entity.ProfileStackElement;
+import org.apache.skywalking.oap.server.library.util.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Analyze {@link ProfileStack} data to {@link ProfileAnalyzation}
+ *
+ * See: https://github.com/apache/skywalking/blob/docs/en/guides/backend-profile.md#thread-analyst
+ */
+public class ProfileAnalyzer {
+
+    private static final ProfileAnalyzeCollector ANALYZE_COLLECTOR = new ProfileAnalyzeCollector();
+
+    /**
+     * Analyze records
+     * @param stacks
+     * @return
+     */
+    public static ProfileAnalyzation analyze(List<ProfileStack> stacks) {
+        if (CollectionUtils.isEmpty(stacks)) {
+            return null;
+        }
+
+        // using parallel stream
+        Map<String, ProfileStackElement> stackTrees = stacks.parallelStream()
+                // stack list cannot be empty
+                .filter(s -> CollectionUtils.isNotEmpty(s.getStack()))
+                .collect(Collectors.groupingBy(s -> s.getStack().get(0), ANALYZE_COLLECTOR));
+
+        ProfileAnalyzation analyzer = new ProfileAnalyzation();
+        analyzer.setStack(new ArrayList<>(stackTrees.values()));
+        return analyzer;
+    }
+
+}
