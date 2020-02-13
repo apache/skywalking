0300048d9281ae52477d7dc6286b140b22c551d7
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2020-02-13 11:27:17 +0800

    Fill some comments for core. (#4356)
    * Fill some comments for core.
    
    * Fix a comment issue.
    
    Co-authored-by: kezhenxu94 <kezhenxu94@163.com>

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/data/Window.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/data/Window.java
@@ -20,6 +20,14 @@ package org.apache.skywalking.oap.server.core.analysis.data;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Data cache window. Window holds two data collections(A and B). They are switchable based on outside command. In any
+ * time, one collection is accepting the input data, and the other is immutable.
+ *
+ * This window makes sure there is not concurrency read-write situation.
+ *
+ * @param <DATA> type in the Window and internal collection.
+ */
 public abstract class Window<DATA> {
 
     private AtomicInteger windowSwitch = new AtomicInteger(0);
