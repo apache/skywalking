ad0ab402d031697b9e708107942835d6eded218f
lsyf <liufei@lianlianpay.com>
2020-02-19 17:23:33 +0800

    Fix ehcache: format

--- apm-sniffer/apm-sdk-plugin/ehcache-2.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/ehcache/v2/define/EhcachePluginInstrumentation.java
+++ apm-sniffer/apm-sdk-plugin/ehcache-2.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/ehcache/v2/define/EhcachePluginInstrumentation.java
@@ -119,8 +119,8 @@ public class EhcachePluginInstrumentation extends ClassInstanceMethodsEnhancePlu
 
                 @Override
                 public boolean isOverrideArgs() {
-                        return false;
-                    }
+                    return false;
+                }
 
             },
             new InstanceMethodsInterceptPoint() {
