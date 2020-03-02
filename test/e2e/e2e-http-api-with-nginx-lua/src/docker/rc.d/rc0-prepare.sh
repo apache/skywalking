49d148483b8e9c5562bdfb15a355c81060feeeb4
JaredTan95 <jian.tan@daocloud.io>
2020-03-02 10:50:44 +0800

    e2e:fix lua agent register ip port.

--- test/e2e/e2e-http-api-with-nginx-lua/src/docker/rc.d/rc0-prepare.sh
+++ test/e2e/e2e-http-api-with-nginx-lua/src/docker/rc.d/rc0-prepare.sh
@@ -15,6 +15,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#!/bin/bash
+
+set -e
+
+var_application_file="/var/nginx/conf.d/nginx.conf"
+
 apt-get update && apt-get -y install git
 
 echo 'git clone skyalking-nginx-lua lib from https://github.com/apache/skywalking-nginx-lua.git'
@@ -24,5 +30,80 @@ git clone https://github.com/apache/skywalking-nginx-lua.git /usr/share/skywalki
   && git checkout ${SKYWALKING_NINGX_LUA_GIT_COMMIT_ID} \
   && ls ./
 
-/usr/bin/openresty -c /var/nginx/conf.d/nginx.conf
+
+generateNginxConf() {
+     cat <<EOT >> ${var_application_file}
+worker_processes  1;
+daemon off;
+error_log /dev/stdout error;
+
+events {
+    worker_connections 1024;
+}
+http {
+    lua_package_path "/usr/share/skywalking-nginx-lua/lib/skywalking/?.lua;;";
+    # Buffer represents the register inform and the queue of the finished segment
+    lua_shared_dict tracing_buffer 100m;
+
+    # Init is the timer setter and keeper
+    # Setup an infinite loop timer to do register and trace report.
+    init_worker_by_lua_block {
+        local metadata_buffer = ngx.shared.tracing_buffer
+
+        metadata_buffer:set('serviceName', 'User_Service_Name')
+        -- Instance means the number of Nginx deloyment, does not mean the worker instances
+        metadata_buffer:set('serviceInstanceName', 'User_Service_Instance_Name')
+
+        require("client"):startBackendTimer("http://${SKYALKING_BACKEND_ADDRESS}:12800")
+    }
+    log_format sw_trace escape=json "$uri $request_body";
+
+    server {
+        listen 8080;
+
+        location /e2e/users {
+
+            rewrite_by_lua_block {
+                require("tracer"):startBackendTimer()
+            }
+
+            proxy_pass http://upstream:9090/e2e/users;
+
+            body_filter_by_lua_block {
+                require("tracer"):finish()
+            }
+
+            log_by_lua_block {
+                require("tracer"):prepareForReport()
+            }
+        }
+
+        location /health-check {
+
+            rewrite_by_lua_block {
+                require("tracer"):startBackendTimer()
+            }
+
+            proxy_pass http://upstream:9090/e2e/health-check;
+
+            body_filter_by_lua_block {
+                require("tracer"):finish()
+            }
+
+            log_by_lua_block {
+                require("tracer"):prepareForReport()
+            }
+        }
+    }
+}
+EOT
+}
+
+generateNginxConf;
+
+echo 'generated nginx.conf:'
+
+cat ${var_application_file}
+
+/usr/bin/openresty -c ${var_application_file}
 sync
