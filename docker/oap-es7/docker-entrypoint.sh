625a50940bb839ee410e8d735746dde26b2a7efb
kezhenxu94 <kezhenxu94@163.com>
2020-03-10 23:23:23 +0800

    Fix unclear doc and add missing variable

--- docker/oap-es7/docker-entrypoint.sh
+++ docker/oap-es7/docker-entrypoint.sh
@@ -302,7 +302,7 @@ generateApplicationYaml() {
 
     validateVariables "SW_CLUSTER" "$SW_CLUSTER" "standalone zookeeper kubernetes consul etcd nacos"
 
-    validateVariables "SW_STORAGE" "$SW_STORAGE" "elasticsearch h2 mysql"
+    validateVariables "SW_STORAGE" "$SW_STORAGE" "elasticsearch h2 mysql influxdb"
 
     validateVariables "SW_CONFIGURATION" "$SW_CONFIGURATION" "none apollo nacos zookeeper"
 
@@ -355,6 +355,7 @@ EOT
     elasticsearch) generateStorageElastisearch;;
     h2) generateStorageH2;;
     mysql) generateStorageMySQL;;
+    influxdb) generateStorageInfluxDB;;
     esac
 
     cat <<EOT >> ${var_application_file}
