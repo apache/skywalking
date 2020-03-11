e3c0dc172232addee17501fc4a048c363b96ad97
Gao Hongtao <hanahmily@gmail.com>
2020-03-11 13:00:58 +0800

    Decrease timeout
    Signed-off-by: Gao Hongtao <hanahmily@gmail.com>

--- test/e2e/e2e-ssl/src/test/java/org/apache/skywalking/e2e/SampleVerificationITCase.java
+++ test/e2e/e2e-ssl/src/test/java/org/apache/skywalking/e2e/SampleVerificationITCase.java
@@ -92,7 +92,7 @@ public class SampleVerificationITCase {
         instrumentedServiceUrl = "http://" + instrumentedServiceHost + ":" + instrumentedServicePort;
     }
 
-    @Test(timeout = 1200000)
+    @Test(timeout = 3 * 60 * 1000)
     @DirtiesContext
     public void verify() throws Exception {
         final LocalDateTime minutesAgo = LocalDateTime.now(ZoneOffset.UTC);
