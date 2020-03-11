728a7d93be4ce3a34aa2978a8fac39abf1a7d9c8
innerpeacez <innerpeace.zhai@gmail.com>
2020-03-11 16:10:29 +0800

    ci debug

--- install/deploy/skywalking-deploy.sh
+++ install/deploy/skywalking-deploy.sh
@@ -64,6 +64,9 @@ helm -n $DPELOY_NAMESPACE install skywalking skywalking --set oap.istio.adapter.
 for component in $NEED_CHECK_PREFIX"oap" ; do
   sleep 60
   kubectl get deploy -o wide -n $DPELOY_NAMESPACE
+  echo "--------------------------------------------"
+  cat ./tmp/skywalkingminikube-start-log.txt
+  echo "--------------------------------------------"
   kubectl describe pod/`kubectl get pod -n $DPELOY_NAMESPACE  | grep skywalking-oap | awk '{print $1}'` -n $DPELOY_NAMESPACE
   kubectl get deploy skywalking-skywalking-oap -oyaml -n $DPELOY_NAMESPACE
   kubectl get pod `kubectl get pod -n $DPELOY_NAMESPACE  | grep skywalking-oap | awk '{print $1}'` -n $DPELOY_NAMESPACE -oyaml
