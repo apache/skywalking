9f1e315b287244f3bcac010f59c7a03f92b9a7f1
cngdkxw <cngdkxw@gmail.com>
2020-03-13 23:13:51 +0800

    RestTemplate async plugin ClassCastException (#4507)
    * ClassCastException
    
    * resttemplate 4.x scenario
    
    * add support versions & fix expectedData format-
    
    Co-authored-by: 吴晟 Wu Sheng <wu.sheng@foxmail.com>

---
+++ test/plugin/scenarios/resttemplate-4.x-scenario/src/main/java/org/apache/skywalking/testcase/resttemplate/BackController.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.testcase.resttemplate;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resttemplate")
+public class BackController {
+    @RequestMapping(value = "/syncback", method = RequestMethod.GET)
+    public String syncBack() {
+        return "Hello back";
+    }
+
+    @RequestMapping(value = "/asyncback", method = RequestMethod.GET)
+    public String asyncBack() {
+        return "Hello back";
+    }
+}
