5fb447377ea8750faf202a57404543ad7862676d
zhangwei <pknfe@outlook.com>
2020-04-15 08:50:46 +0800

    follow review

--- test/e2e/e2e-test/docker/go/Dockerfile.go
+++ test/e2e/e2e-test/docker/go/Dockerfile.go
@@ -16,16 +16,17 @@
 FROM golang:1.12 AS builder
 
 ARG COMMIT_HASH=fdb185d66faddad1651c18150d63bae32610f3ac
-ARG GO2SKY_REPO=${COMMIT_HASH}.tar.gz
+ARG GO2SKY_CODE=${COMMIT_HASH}.tar.gz
+ARG GO2SKY_CODE_URL=https://github.com/SkyAPM/go2sky/archive/${GO2SKY_CODE}
 
 ENV CGO_ENABLED=0
 ENV GO111MODULE=on
 
 WORKDIR /go2sky
 
-ADD https://github.com/SkyAPM/go2sky/archive/${GO2SKY_REPO} .
-RUN tar -xf ${GO2SKY_REPO} --strip 1
-RUN rm ${GO2SKY_REPO}
+ADD ${GO2SKY_CODE_URL} .
+RUN tar -xf ${GO2SKY_CODE} --strip 1
+RUN rm ${GO2SKY_CODE}
 
 WORKDIR /go2sky/test/e2e/example-server
 RUN go build -o main
