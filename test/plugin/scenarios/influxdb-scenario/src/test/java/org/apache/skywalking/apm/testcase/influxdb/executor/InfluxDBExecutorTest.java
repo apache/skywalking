2e0c63495ae626275d66ea729acce708f1eb9d3b
dagmom <dev_guhao@163.com>
2020-06-03 23:00:58 +0800

    plugin-test : upgrade influxdb-scenario support-version.list form 2.5 ~ 2.17

--- test/plugin/scenarios/influxdb-scenario/src/test/java/org/apache/skywalking/apm/testcase/influxdb/executor/InfluxDBExecutorTest.java
+++ test/plugin/scenarios/influxdb-scenario/src/test/java/org/apache/skywalking/apm/testcase/influxdb/executor/InfluxDBExecutorTest.java
@@ -10,7 +10,7 @@ public class InfluxDBExecutorTest {
 
 //  @Test
 //  public void testPing(){
-//    InfluxDBExecutor executor = new InfluxDBExecutor("http://influxdb:8086");
+//    InfluxDBExecutor executor = new InfluxDBExecutor("http://localhost:8086");
 //    Pong pong = executor.ping();
 //    System.out.println(pong.getVersion());
 //    Assert.assertNotNull(pong.getVersion());
