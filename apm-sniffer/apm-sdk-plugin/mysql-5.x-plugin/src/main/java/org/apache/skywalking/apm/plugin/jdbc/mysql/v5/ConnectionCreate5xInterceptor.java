f4c2b11f1c4f6d739ce5f93bb616bb6b7b33d610
陈曜 <chenyao@dxy.cn>
2020-06-17 18:41:55 +0800

    1 丁香园 add to powered-by.md

--- apm-sniffer/apm-sdk-plugin/mysql-5.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/jdbc/mysql/v5/ConnectionCreate5xInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/mysql-5.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/jdbc/mysql/v5/ConnectionCreate5xInterceptor.java
@@ -42,7 +42,11 @@ public class ConnectionCreate5xInterceptor implements StaticMethodsAroundInterce
     public Object afterMethod(Class clazz, Method method, Object[] allArguments, Class<?>[] parameterTypes,
         Object ret) {
         if (ret instanceof EnhancedInstance) {
-            ((EnhancedInstance) ret).setSkyWalkingDynamicField(URLParser.parser(allArguments[4].toString()));
+            ConnectionInfo connectionInfo = ConnectionCache.get(allArguments[0].toString(), allArguments[1].toString());
+            if (connectionInfo == null) {
+                connectionInfo = URLParser.parser(allArguments[4].toString());
+            }
+            ((EnhancedInstance) ret).setSkyWalkingDynamicField(connectionInfo);
         }
         return ret;
     }
