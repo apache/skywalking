08897ce5f8962d118d7da795b0d3decd6d3db851
dagmom <43675197+dagmom@users.noreply.github.com>
2020-06-21 22:35:45 +0800

    New feature provided : new plugin for influxdb-java client (#4846)

--- apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
+++ apm-protocol/apm-network/src/main/java/org/apache/skywalking/apm/network/trace/component/ComponentsDefine.java
@@ -160,4 +160,6 @@ public class ComponentsDefine {
     public static final OfficialComponent MARIADB_JDBC = new OfficialComponent(87, "mariadb-jdbc");
 
     public static final OfficialComponent QUASAR = new OfficialComponent(88, "quasar");
+
+    public static final OfficialComponent INFLUXDB_JAVA = new OfficialComponent(90, "influxdb-java");
 }
