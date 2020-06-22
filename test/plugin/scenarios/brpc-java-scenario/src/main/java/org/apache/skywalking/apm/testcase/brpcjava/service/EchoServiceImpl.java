6415d3c7847b6048e5a4f532f674dc606eeac942
IanCao <caoyixiong@apache.org>
2020-06-22 16:44:35 +0800

    add brpc-java plugin

---
+++ test/plugin/scenarios/brpc-java-scenario/src/main/java/org/apache/skywalking/apm/testcase/brpcjava/service/EchoServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018 Baidu, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.skywalking.apm.testcase.brpcjava.service;
+
+
+import com.baidu.brpc.RpcContext;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.apache.skywalking.apm.testcase.brpcjava.interfaces.Echo;
+import org.apache.skywalking.apm.testcase.brpcjava.interfaces.EchoService;
+
+/**
+ * Copy from brpc-java-example
+ */
+public class EchoServiceImpl implements EchoService {
+
+    @Override
+    public Echo.EchoResponse echo(Echo.EchoRequest request) {
+        if (RpcContext.isSet()) {
+            RpcContext rpcContext = RpcContext.getContext();
+            ByteBuf attachment = rpcContext.getRequestBinaryAttachment();
+            if (attachment != null) {
+                rpcContext.setResponseBinaryAttachment(Unpooled.copiedBuffer(attachment));
+            }
+        }
+        String message = request.getMessage();
+        Echo.EchoResponse response = Echo.EchoResponse.newBuilder()
+                .setMessage(message).build();
+        return response;
+    }
+}
