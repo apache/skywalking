a962e85b777a43baadf657937e9d44cd6fe8d1fd
daming <zteny@foxmail.com>
2020-07-27 20:57:52 +0800

    fix ut

--- oap-server/server-receiver-plugin/skywalking-trace-receiver-plugin/src/test/java/org/apache/skywalking/oap/server/receiver/trace/provider/handler/v8/rest/TraceSegmentReportServletHandlerTest.java
+++ oap-server/server-receiver-plugin/skywalking-trace-receiver-plugin/src/test/java/org/apache/skywalking/oap/server/receiver/trace/provider/handler/v8/rest/TraceSegmentReportServletHandlerTest.java
@@ -19,6 +19,11 @@
 package org.apache.skywalking.oap.server.receiver.trace.provider.handler.v8.rest;
 
 import org.apache.skywalking.apm.network.language.agent.v3.SegmentObject;
+import org.apache.skywalking.oap.server.analyzer.module.AnalyzerModule;
+import org.apache.skywalking.oap.server.analyzer.provider.AnalyzerModuleConfig;
+import org.apache.skywalking.oap.server.analyzer.provider.AnalyzerModuleProvider;
+import org.apache.skywalking.oap.server.analyzer.provider.trace.parser.ISegmentParserService;
+import org.apache.skywalking.oap.server.analyzer.provider.trace.parser.SegmentParserServiceImpl;
 import org.apache.skywalking.oap.server.library.module.ModuleManager;
 import org.apache.skywalking.oap.server.telemetry.TelemetryModule;
 import org.apache.skywalking.oap.server.telemetry.api.MetricsCreator;
@@ -31,6 +36,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.stubbing.OngoingStubbing;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -53,14 +59,24 @@ public class TraceSegmentReportServletHandlerTest {
     @Mock
     private NoneTelemetryProvider telemetryProvider;
 
+    @Mock
+    private AnalyzerModuleProvider analyzerModuleProvider;
+
     @Before
     public void init() throws IOException {
         TelemetryModule telemetryModule = Mockito.spy(TelemetryModule.class);
         Whitebox.setInternalState(telemetryModule, "loadedProvider", telemetryProvider);
         Mockito.when(moduleManager.find(TelemetryModule.NAME)).thenReturn(telemetryModule);
 
+        AnalyzerModule analyzerModule = Mockito.spy(AnalyzerModule.class);
+        Whitebox.setInternalState(analyzerModule, "loadedProvider", analyzerModuleProvider);
+        Mockito.when(moduleManager.find(AnalyzerModule.NAME)).thenReturn(analyzerModule);
+
         Mockito.when(telemetryProvider.getService(MetricsCreator.class))
             .thenReturn(new MetricsCreatorNoop());
+
+        when(analyzerModuleProvider.getService(ISegmentParserService.class))
+            .thenReturn(new SegmentParserServiceImpl(moduleManager, new AnalyzerModuleConfig()));
     }
 
     @Test
