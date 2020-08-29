4ce3cf927d2b8d9eb31303a72fba6961a4ec8543
Megrez Lu <lujiajing1126@gmail.com>
2020-08-29 21:08:39 +0800

    use LogMatch to do the test

--- apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/logging/core/JsonLoggerTest.java
+++ apm-sniffer/apm-agent-core/src/test/java/org/apache/skywalking/apm/agent/core/logging/core/JsonLoggerTest.java
@@ -19,9 +19,12 @@
 package org.apache.skywalking.apm.agent.core.logging.core;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import org.apache.skywalking.apm.agent.core.conf.Config;
 import org.apache.skywalking.apm.agent.core.logging.core.converters.ThrowableConverter;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,7 +34,6 @@ import org.mockito.Mockito;
 import java.lang.reflect.Type;
 import java.util.Map;
 
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 
 public class JsonLoggerTest {
@@ -55,67 +57,127 @@ public class JsonLoggerTest {
         Assert.assertTrue(logger.isWarnEnable());
         Assert.assertTrue(logger.isErrorEnable());
 
+        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
         logger.debug("hello world");
+        Mockito.verify(output, times(1)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "DEBUG", null));
         logger.debug("hello {}", "world");
+        Mockito.verify(output, times(2)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "DEBUG", null));
         logger.info("hello world");
+        Mockito.verify(output, times(3)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "INFO", null));
         logger.info("hello {}", "world");
+        Mockito.verify(output, times(4)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "INFO", null));
 
         logger.warn("hello {}", "world");
+        Mockito.verify(output, times(5)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "WARN", null));
         logger.warn("hello world");
+        Mockito.verify(output, times(6)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "WARN", null));
         logger.error("hello world");
-        logger.error("hello world", new NullPointerException());
-        logger.error(new NullPointerException(), "hello {}", "world");
-
-        Mockito.verify(output, times(9)).write(anyString());
+        Mockito.verify(output, times(7)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "ERROR", null));
+        Throwable t = new NullPointerException();
+        logger.error("hello world", t);
+        Mockito.verify(output, times(8)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "ERROR", t));
+        logger.error(t, "hello {}", "world");
+        Mockito.verify(output, times(9)).write(argument.capture());
+        Assert.assertThat(argument.getValue(), createMatcher("hello world", "ERROR", t));
     }
 
-    @Test
-    public void testJsonLogKeys() {
-        final IWriter output = Mockito.mock(IWriter.class);
-        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-        JsonLogger logger = new JsonLogger(JsonLoggerTest.class, new Gson()) {
-            @Override
-            protected void logger(LogLevel level, String message, Throwable e) {
-                output.write(format(level, message, e));
-            }
-        };
-        logger.info("hello world");
-        Mockito.verify(output).write(argument.capture());
-        Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, String>>() {
+    private static class LogMatcher extends TypeSafeDiagnosingMatcher<String> {
+        private static final Gson gson = new Gson();
+        private static final Type type = new TypeToken<Map<String, String>>() {
         }.getType();
-        Map<String, String> logMap = gson.fromJson(argument.getValue(), type);
-        Assert.assertTrue(logMap.containsKey("@timestamp"));
-        Assert.assertTrue(logMap.containsKey("message"));
-        Assert.assertEquals(logMap.get("message"), "hello world");
-        Assert.assertTrue(logMap.containsKey("thread"));
-        Assert.assertTrue(logMap.containsKey("level"));
-        Assert.assertTrue(logMap.containsKey("agent_name"));
-        Assert.assertTrue(logMap.containsKey("throwable"));
-        Assert.assertEquals(logMap.get("throwable"), "");
-        Assert.assertTrue(logMap.containsKey("logger"));
-        Assert.assertEquals(logMap.get("logger"), "JsonLoggerTest");
+
+        private final String agent;
+        private final String logger;
+        private final String message;
+        private final String level;
+        private final Throwable t;
+
+        public LogMatcher(String agent, String logger, String message, String level, Throwable t) {
+            this.agent = agent;
+            this.logger = logger;
+            this.message = message;
+            this.level = level;
+            this.t = t;
+        }
+
+        @Override
+        protected boolean matchesSafely(String item, Description mismatchDescription) {
+            try {
+                Map<String, String> logMap = gson.fromJson(item, type);
+                if (!logMap.containsKey("agent_name")) {
+                    mismatchDescription.appendText("agent_name did not exist");
+                    return false;
+                }
+                if (!this.agent.equals(logMap.get("agent_name"))) {
+                    mismatchDescription.appendText("agent_name was " + this.agent);
+                    return false;
+                }
+                if (!logMap.containsKey("message")) {
+                    mismatchDescription.appendText("agent_name did not exist");
+                    return false;
+                }
+                if (!this.message.equals(logMap.get("message"))) {
+                    mismatchDescription.appendText("message was " + this.message);
+                    return false;
+                }
+
+                if (!logMap.containsKey("@timestamp")) {
+                    mismatchDescription.appendText("@timestamp did not exist");
+                    return false;
+                }
+
+                if (!logMap.containsKey("level")) {
+                    mismatchDescription.appendText("level did not exist");
+                    return false;
+                }
+                if (!this.level.equals(logMap.get("level"))) {
+                    mismatchDescription.appendText("level was " + this.level);
+                    return false;
+                }
+
+                if (!logMap.containsKey("logger")) {
+                    mismatchDescription.appendText("logger did not exist");
+                    return false;
+                }
+                if (!this.logger.equals(logMap.get("logger"))) {
+                    mismatchDescription.appendText("logger was " + this.logger);
+                    return false;
+                }
+
+                if (!logMap.containsKey("throwable")) {
+                    mismatchDescription.appendText("throwable did not exist");
+                    return false;
+                }
+                final String tStr = this.t == null ? "" : ThrowableConverter.format(this.t);
+                if (!tStr.equals(logMap.get("throwable"))) {
+                    mismatchDescription.appendText("throwable was " + tStr);
+                    return false;
+                }
+
+            } catch (JsonSyntaxException ex) {
+                mismatchDescription.appendText("not a valid json string");
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public void describeTo(Description expectation) {
+            expectation.appendText(
+                    String.format("a valid json log format with {message=%s,level=%s,logger=%s,@timestamp=*,agent=%s}",
+                            message, level, logger, agent));
+        }
     }
 
-    @Test
-    public void testExceptionLogging() {
-        final IWriter output = Mockito.mock(IWriter.class);
-        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
-        JsonLogger logger = new JsonLogger(JsonLoggerTest.class, new Gson()) {
-            @Override
-            protected void logger(LogLevel level, String message, Throwable e) {
-                output.write(format(level, message, e));
-            }
-        };
-        Exception e = new IllegalArgumentException();
-        String throwableStr = ThrowableConverter.format(e);
-        logger.error(e, "int cannot be negative");
-        Mockito.verify(output).write(argument.capture());
-        Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, String>>() {
-        }.getType();
-        Map<String, String> logMap = gson.fromJson(argument.getValue(), type);
-        Assert.assertTrue(logMap.containsKey("throwable"));
-        Assert.assertEquals(logMap.get("throwable"), throwableStr);
+    public static LogMatcher createMatcher(String message, String level, Throwable t) {
+        return new LogMatcher("testAppFromConfig", JsonLoggerTest.class.getSimpleName(), message, level, t);
     }
 }
