e6903959835dbe60d65594e2803763f8f289623e
zhangwei <pknfe@outlook.com>
2020-09-02 22:37:42 +0800

    support http api

--- oap-server/server-receiver-plugin/skywalking-browser-receiver-plugin/src/test/java/org/apache/skywalking/oap/server/receiver/browser/provider/handler/rest/BrowserReportServletHandlerTest.java
+++ oap-server/server-receiver-plugin/skywalking-browser-receiver-plugin/src/test/java/org/apache/skywalking/oap/server/receiver/browser/provider/handler/rest/BrowserReportServletHandlerTest.java
@@ -20,15 +20,12 @@ package org.apache.skywalking.oap.server.receiver.browser.provider.handler.rest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.skywalking.apm.network.language.agent.v3.BrowserErrorLog;
 import org.apache.skywalking.apm.network.language.agent.v3.BrowserPerfData;
 import org.apache.skywalking.apm.network.language.agent.v3.ErrorCategory;
 import org.apache.skywalking.oap.server.library.module.ModuleManager;
-import org.apache.skywalking.oap.server.library.util.ProtoBufJsonUtils;
 import org.apache.skywalking.oap.server.telemetry.TelemetryModule;
 import org.apache.skywalking.oap.server.telemetry.api.MetricsCreator;
 import org.apache.skywalking.oap.server.telemetry.none.MetricsCreatorNoop;
@@ -67,95 +64,134 @@ public class BrowserReportServletHandlerTest {
 
     @Test
     public void testPerfData() throws IOException {
-        BrowserPerfData browserPerfData = BrowserPerfData.newBuilder()
-                                                         .setService("test")
-                                                         .setServiceVersion("v0.0.1")
-                                                         .setPagePath("/e2e-browser")
-                                                         .setRedirectTime(10)
-                                                         .setDnsTime(10)
-                                                         .setTtfbTime(10)
-                                                         .setTcpTime(10)
-                                                         .setTransTime(10)
-                                                         .setDomAnalysisTime(10)
-                                                         .setFptTime(10)
-                                                         .setDomReadyTime(10)
-                                                         .setLoadPageTime(10)
-                                                         .setResTime(10)
-                                                         .setSslTime(10)
-                                                         .setTtlTime(10)
-                                                         .setFirstPackTime(10)
-                                                         .setFmpTime(10)
-                                                         .build();
+        final String json = "{\n" +
+            "  \"service\": \"test\",\n" +
+            "  \"serviceVersion\": \"v0.0.1\",\n" +
+            "  \"pagePath\": \"/e2e-browser\",\n" +
+            "  \"redirectTime\": 1,\n" +
+            "  \"dnsTime\": 2,\n" +
+            "  \"ttfbTime\": 3,\n" +
+            "  \"tcpTime\": 4,\n" +
+            "  \"transTime\": 5,\n" +
+            "  \"domAnalysisTime\": 6,\n" +
+            "  \"fptTime\": 7,\n" +
+            "  \"domReadyTime\": 8,\n" +
+            "  \"loadPageTime\": 9,\n" +
+            "  \"resTime\": 10,\n" +
+            "  \"sslTime\": 11,\n" +
+            "  \"ttlTime\": 12,\n" +
+            "  \"firstPackTime\": 13,\n" +
+            "  \"fmpTime\": 14\n" +
+            "}";
         final BrowserPerfDataReportServletHandler reportServletHandler = new BrowserPerfDataReportServletHandler(
             moduleManager, null, null);
 
         when(request.getReader()).thenReturn(
-            new BufferedReader(new StringReader(ProtoBufJsonUtils.toJSON(browserPerfData))));
+            new BufferedReader(new StringReader(json)));
         final BrowserPerfData result = reportServletHandler.parseBrowserPerfData(request);
-        Assert.assertEquals(result, browserPerfData);
+        Assert.assertEquals("test", result.getService());
+        Assert.assertEquals("v0.0.1", result.getServiceVersion());
+        Assert.assertEquals("/e2e-browser", result.getPagePath());
+        Assert.assertEquals(1, result.getRedirectTime());
+        Assert.assertEquals(2, result.getDnsTime());
+        Assert.assertEquals(3, result.getTtfbTime());
+        Assert.assertEquals(4, result.getTcpTime());
+        Assert.assertEquals(5, result.getTransTime());
+        Assert.assertEquals(6, result.getDomAnalysisTime());
+        Assert.assertEquals(7, result.getFptTime());
+        Assert.assertEquals(8, result.getDomReadyTime());
+        Assert.assertEquals(9, result.getLoadPageTime());
+        Assert.assertEquals(10, result.getResTime());
+        Assert.assertEquals(11, result.getSslTime());
+        Assert.assertEquals(12, result.getTtlTime());
+        Assert.assertEquals(13, result.getFirstPackTime());
+        Assert.assertEquals(14, result.getFmpTime());
     }
 
     @Test
     public void testErrorLogSingle() throws IOException {
-        BrowserErrorLog errorLog = BrowserErrorLog.newBuilder()
-                                                  .setUniqueId(UUID.randomUUID().toString())
-                                                  .setService("test")
-                                                  .setServiceVersion("v0.0.1")
-                                                  .setPagePath("/e2e-browser")
-                                                  .setCategory(ErrorCategory.ajax)
-                                                  .setMessage("test")
-                                                  .setLine(1)
-                                                  .setCol(1)
-                                                  .setStack("e2e")
-                                                  .setErrorUrl("/e2e-browser").build();
+        final String singleJson = "{\n" +
+            "  \"uniqueId\": \"55ec6178-3fb7-43ef-899c-a26944407b0e\",\n" +
+            "  \"service\": \"test\",\n" +
+            "  \"serviceVersion\": \"v0.0.1\",\n" +
+            "  \"pagePath\": \"/e2e-browser\",\n" +
+            "  \"category\": \"ajax\",\n" +
+            "  \"message\": \"test\",\n" +
+            "  \"line\": 1,\n" +
+            "  \"col\": 1,\n" +
+            "  \"stack\": \"e2e\",\n" +
+            "  \"errorUrl\": \"/e2e-browser\"\n" +
+            "}";
 
         final BrowserErrorLogReportSingleServletHandler singleServletHandler = new BrowserErrorLogReportSingleServletHandler(
             moduleManager, null, null);
 
         when(request.getReader()).thenReturn(
-            new BufferedReader(new StringReader(ProtoBufJsonUtils.toJSON(errorLog))));
+            new BufferedReader(new StringReader(singleJson)));
         final List<BrowserErrorLog> browserErrorLogs = singleServletHandler.parseBrowserErrorLog(request);
         Assert.assertEquals(1, browserErrorLogs.size());
-        Assert.assertEquals(errorLog, browserErrorLogs.get(0));
+        BrowserErrorLog errorLog = browserErrorLogs.get(0);
+        Assert.assertEquals("55ec6178-3fb7-43ef-899c-a26944407b0e", errorLog.getUniqueId());
+        Assert.assertEquals("test", errorLog.getService());
+        Assert.assertEquals("v0.0.1", errorLog.getServiceVersion());
+        Assert.assertEquals("/e2e-browser", errorLog.getPagePath());
+        Assert.assertEquals(ErrorCategory.ajax, errorLog.getCategory());
+        Assert.assertEquals("test", errorLog.getMessage());
+        Assert.assertEquals(1, errorLog.getLine());
+        Assert.assertEquals(1, errorLog.getCol());
+        Assert.assertEquals("e2e", errorLog.getStack());
+        Assert.assertEquals("/e2e-browser", errorLog.getErrorUrl());
     }
 
     @Test
     public void testErrorLogList() throws IOException {
-        List<BrowserErrorLog> errorLogs = Arrays.asList(
-            BrowserErrorLog.newBuilder()
-                           .setUniqueId("1")
-                           .setService("test")
-                           .setServiceVersion("v0.0.1")
-                           .setPagePath("/e2e-browser")
-                           .setCategory(ErrorCategory.ajax)
-                           .setMessage("test")
-                           .setLine(1)
-                           .setCol(1)
-                           .setStack("e2e")
-                           .setErrorUrl("/e2e-browser").build(),
-            BrowserErrorLog.newBuilder()
-                           .setUniqueId("2")
-                           .setService("test")
-                           .setServiceVersion("v0.0.1")
-                           .setPagePath("/e2e-browser")
-                           .setCategory(ErrorCategory.ajax)
-                           .setMessage("test")
-                           .setLine(1)
-                           .setCol(1)
-                           .setStack("e2e")
-                           .setErrorUrl("/e2e-browser").build()
-        );
+        final String listJson = "[\n" +
+            "    {\n" +
+            "        \"uniqueId\": \"55ec6178-3fb7-43ef-899c-a26944407b01\",\n" +
+            "        \"service\": \"test\",\n" +
+            "        \"serviceVersion\": \"v0.0.1\",\n" +
+            "        \"pagePath\": \"/e2e-browser\",\n" +
+            "        \"category\": \"ajax\",\n" +
+            "        \"message\": \"test\",\n" +
+            "        \"line\": 1,\n" +
+            "        \"col\": 1,\n" +
+            "        \"stack\": \"e2e\",\n" +
+            "        \"errorUrl\": \"/e2e-browser\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"uniqueId\": \"55ec6178-3fb7-43ef-899c-a26944407b02\",\n" +
+            "        \"service\": \"test\",\n" +
+            "        \"serviceVersion\": \"v0.0.1\",\n" +
+            "        \"pagePath\": \"/e2e-browser\",\n" +
+            "        \"category\": \"ajax\",\n" +
+            "        \"message\": \"test\",\n" +
+            "        \"line\": 1,\n" +
+            "        \"col\": 1,\n" +
+            "        \"stack\": \"e2e\",\n" +
+            "        \"errorUrl\": \"/e2e-browser\"\n" +
+            "    }\n" +
+            "]";
 
         final BrowserErrorLogReportListServletHandler listServletHandler = new BrowserErrorLogReportListServletHandler(
             moduleManager, null, null);
 
         when(request.getReader()).thenReturn(
-            new BufferedReader(new StringReader(
-                "[" + ProtoBufJsonUtils.toJSON(errorLogs.get(0)) + "," +
-                    ProtoBufJsonUtils.toJSON(errorLogs.get(1)) + "]")));
+            new BufferedReader(new StringReader(listJson)));
 
         final List<BrowserErrorLog> browserErrorLogs = listServletHandler.parseBrowserErrorLog(request);
         Assert.assertEquals(2, browserErrorLogs.size());
-        Assert.assertEquals(errorLogs, browserErrorLogs);
+        BrowserErrorLog errorLog1 = browserErrorLogs.get(0);
+        Assert.assertEquals("55ec6178-3fb7-43ef-899c-a26944407b01", errorLog1.getUniqueId());
+        Assert.assertEquals("test", errorLog1.getService());
+        Assert.assertEquals("v0.0.1", errorLog1.getServiceVersion());
+        Assert.assertEquals("/e2e-browser", errorLog1.getPagePath());
+        Assert.assertEquals(ErrorCategory.ajax, errorLog1.getCategory());
+        Assert.assertEquals("test", errorLog1.getMessage());
+        Assert.assertEquals(1, errorLog1.getLine());
+        Assert.assertEquals(1, errorLog1.getCol());
+        Assert.assertEquals("e2e", errorLog1.getStack());
+        Assert.assertEquals("/e2e-browser", errorLog1.getErrorUrl());
+        BrowserErrorLog errorLog2 = browserErrorLogs.get(1);
+        Assert.assertEquals("55ec6178-3fb7-43ef-899c-a26944407b02", errorLog2.getUniqueId());
     }
 }
