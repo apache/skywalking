c3edd3dad052c81a0f11d3ca1afe07e8dbcd674c
harvies <cr0n6e@gmail.com>
2020-09-22 09:50:44 +0800

    Better witness class for mongo3.8.x (#5524)
    * modify witness class
    
    * modify witness class & add test case
    
    * replace DB_STATEMENT to DB_BIND_VARIABLES & remove executeMethod prefix
    
    * fix ci
    
    Co-authored-by: 吴晟 Wu Sheng <wu.sheng@foxmail.com>

--- apm-sniffer/apm-sdk-plugin/mongodb-3.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/mongodb/v3/define/v38/MongoDBOperationExecutorInstrumentation.java
+++ apm-sniffer/apm-sdk-plugin/mongodb-3.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/mongodb/v3/define/v38/MongoDBOperationExecutorInstrumentation.java
@@ -35,7 +35,7 @@ import org.apache.skywalking.apm.agent.core.plugin.match.NameMatch;
  */
 public class MongoDBOperationExecutorInstrumentation extends ClassInstanceMethodsEnhancePluginDefine {
 
-    private static final String WITNESS_CLASS = "com.mongodb.client.ClientSession";
+    private static final String WITNESS_CLASS = "com.mongodb.operation.AggregateOperationImpl";
 
     private static final String ENHANCE_CLASS = "com.mongodb.client.internal.MongoClientDelegate$DelegateOperationExecutor";
 
