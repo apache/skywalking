f69fc08c3716e09d7238923da398bb58a2785363
vcjmhg <zhaojianheng618@163.com>
2020-10-04 18:29:49 +0800

    create plugin project for logger-plugin

--- test/plugin/scenarios/logger-scenario/src/main/java/org/apache/skywalking/apm/testcase/logger/controller/CaseController.java
+++ test/plugin/scenarios/logger-scenario/src/main/java/org/apache/skywalking/apm/testcase/logger/controller/CaseController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/case")
 public class CaseController {
 
     private static final String SUCCESS = "Success";
