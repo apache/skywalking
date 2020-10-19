5749e28a5e5eff719e7b374af19dbc7d690b52f3
jinzhentao <jinzhentao@yy.com>
2020-10-19 16:57:59 +0800

    feat:del useless code

--- apm-sniffer/apm-sdk-plugin/asyncClient-2.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/asyncClient/v1/NettyRequestSenderInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/asyncClient-2.x-plugin/src/main/java/org/apache/skywalking/apm/plugin/asyncClient/v1/NettyRequestSenderInterceptor.java
@@ -9,8 +9,6 @@ import org.apache.skywalking.apm.agent.core.context.ContextSnapshot;
 import org.apache.skywalking.apm.agent.core.context.tag.Tags;
 import org.apache.skywalking.apm.agent.core.context.trace.AbstractSpan;
 import org.apache.skywalking.apm.agent.core.context.trace.SpanLayer;
-import org.apache.skywalking.apm.agent.core.logging.api.ILog;
-import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
