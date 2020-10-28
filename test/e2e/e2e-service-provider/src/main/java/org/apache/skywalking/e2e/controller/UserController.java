267e1357d5daab9bf65382f596a53e1f64c27141
HumbertZhang <504490160@qq.com>
2020-10-28 23:38:33 +0800

    set up random sleep in e2e test provider

--- test/e2e/e2e-service-provider/src/main/java/org/apache/skywalking/e2e/controller/UserController.java
+++ test/e2e/e2e-service-provider/src/main/java/org/apache/skywalking/e2e/controller/UserController.java
@@ -25,23 +25,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
 @SuppressWarnings("SameReturnValue")
 public class UserController {
     private final UserRepo userRepo;
+    private final int sleepMin = 500, sleepMax=1000;
 
     @PostMapping("/info")
     public String info() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(1);
+        Thread.sleep(randomSleepLong(sleepMin, sleepMax));
         return "whatever";
     }
 
     @PostMapping("/users")
     public User createAuthor(@RequestBody final User user) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(1);
+        Thread.sleep(randomSleepLong(sleepMin, sleepMax));
         return userRepo.save(user);
+    }
+
+    private long randomSleepLong(int min, int max) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt((max - min) + 1) + min;
+        return (long) randomNumber;
     }
 }
