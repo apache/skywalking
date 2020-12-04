a3803db5df582fc1882e41673db552d16560d85f
osiriswd <osiriswd82@gmail.com>
2020-12-04 13:40:40 +0800

    Update KafkaFetcherConfig.java
    Add support for Kafka MirrorMaker 2.0 to replicate topics between Kafka clusters.

--- oap-server/server-fetcher-plugin/kafka-fetcher-plugin/src/main/java/org/apache/skywalking/oap/server/analyzer/agent/kafka/module/KafkaFetcherConfig.java
+++ oap-server/server-fetcher-plugin/kafka-fetcher-plugin/src/main/java/org/apache/skywalking/oap/server/analyzer/agent/kafka/module/KafkaFetcherConfig.java
@@ -84,5 +84,29 @@ public class KafkaFetcherConfig extends ModuleConfig {
     private int kafkaHandlerThreadPoolSize;
 
     private int kafkaHandlerThreadPoolQueueSize;
+    
+    private String MM2SourceAlias = "";
 
+    private String MM2SourceSeparator = "";
+    
+    public String getTopicNameOfMetrics() {
+        return this.getMM2SourceAlias() + this.getMM2SourceSeparator() + this.topicNameOfMetrics;
+    }
+
+    public String getTopicNameOfProfiling() {
+        return this.getMM2SourceAlias() + this.getMM2SourceSeparator() + this.topicNameOfProfiling;
+    }
+
+    public String getTopicNameOfTracingSegments() {
+        return this.getMM2SourceAlias() + this.getMM2SourceSeparator() + this.topicNameOfTracingSegments;
+    }
+
+    public String getTopicNameOfManagements() {
+        return this.getMM2SourceAlias() + this.getMM2SourceSeparator() + this.topicNameOfManagements;
+    }
+
+    public String getTopicNameOfMeters() {
+        return this.getMM2SourceAlias() + this.getMM2SourceSeparator() + this.topicNameOfMeters;
+    }
+    
 }
