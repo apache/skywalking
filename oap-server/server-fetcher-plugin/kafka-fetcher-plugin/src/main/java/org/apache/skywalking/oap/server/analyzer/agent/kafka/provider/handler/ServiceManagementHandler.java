fa252384d320b807a43ae3a87057691ddb75c64c
osiriswd <osiriswd82@gmail.com>
2020-12-04 23:10:52 +0800

    Update ServiceManagementHandler.java

--- oap-server/server-fetcher-plugin/kafka-fetcher-plugin/src/main/java/org/apache/skywalking/oap/server/analyzer/agent/kafka/provider/handler/ServiceManagementHandler.java
+++ oap-server/server-fetcher-plugin/kafka-fetcher-plugin/src/main/java/org/apache/skywalking/oap/server/analyzer/agent/kafka/provider/handler/ServiceManagementHandler.java
@@ -124,7 +124,7 @@ public class ServiceManagementHandler implements KafkaHandler {
 
     @Override
     public String getTopic() {
-        return config.getMM2SourceAlias() + config.getMM2SourceSeparator() + config.getTopicNameOfManagements();
+        return config.getMm2SourceAlias() + config.getMm2SourceSeparator() + config.getTopicNameOfManagements();
     }
 
     @Override
