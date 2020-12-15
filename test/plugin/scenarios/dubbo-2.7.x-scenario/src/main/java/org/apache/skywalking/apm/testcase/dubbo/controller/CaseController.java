64484279723df0af7d75c93cf465bec16a98bccd
vcjmhg <vcjmhg@163.com>
2020-12-15 16:14:16 +0800

    format dubbo-2.5.x-scenario and dubbo-2.5.7.x-scenario

--- test/plugin/scenarios/dubbo-2.7.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/dubbo/controller/CaseController.java
+++ test/plugin/scenarios/dubbo-2.7.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/dubbo/controller/CaseController.java
@@ -44,7 +44,6 @@ public class CaseController {
     @ResponseBody
     public String dubbo() {
         GreetService greetService = referenceConfig.get();
-        System.out.println(greetService.doBusiness("helloWorld"));
         return SUCCESS;
     }
 }
