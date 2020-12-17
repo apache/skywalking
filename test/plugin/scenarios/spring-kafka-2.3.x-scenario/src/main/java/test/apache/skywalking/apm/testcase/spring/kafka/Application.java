f0a9d70bb3cb7062a2bb867c7c67f6ca4e666567
vcjmhg <vcjmhg@163.com>
2020-12-17 19:54:32 +0800

    format spring-kafka-2.3.x-scenario

--- test/plugin/scenarios/spring-kafka-2.3.x-scenario/src/main/java/test/apache/skywalking/apm/testcase/spring/kafka/Application.java
+++ test/plugin/scenarios/spring-kafka-2.3.x-scenario/src/main/java/test/apache/skywalking/apm/testcase/spring/kafka/Application.java
@@ -16,7 +16,7 @@
  *
  */
 
-package test.org.apache.skywalking.apm.testcase.spring.kafka;
+package test.apache.skywalking.apm.testcase.spring.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
