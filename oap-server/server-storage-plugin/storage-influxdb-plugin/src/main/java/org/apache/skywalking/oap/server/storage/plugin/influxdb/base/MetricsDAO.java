97d8fbe4e8c817d60ffdf83d45c8b033b1fe3118
daming <zteny@foxmail.com>
2020-12-25 23:23:29 +0800

    revert

--- oap-server/server-storage-plugin/storage-influxdb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/influxdb/base/MetricsDAO.java
+++ oap-server/server-storage-plugin/storage-influxdb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/influxdb/base/MetricsDAO.java
@@ -28,7 +28,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.skywalking.oap.server.core.analysis.TimeBucket;
-import org.apache.skywalking.oap.server.core.analysis.manual.service.ServiceTraffic;
 import org.apache.skywalking.oap.server.core.analysis.metrics.Metrics;
 import org.apache.skywalking.oap.server.core.storage.IMetricsDAO;
 import org.apache.skywalking.oap.server.core.storage.StorageBuilder;
@@ -37,7 +36,6 @@ import org.apache.skywalking.oap.server.core.storage.type.StorageDataComplexObje
 import org.apache.skywalking.oap.server.library.client.request.InsertRequest;
 import org.apache.skywalking.oap.server.library.client.request.UpdateRequest;
 import org.apache.skywalking.oap.server.storage.plugin.influxdb.InfluxClient;
-import org.apache.skywalking.oap.server.storage.plugin.influxdb.InfluxConstants.TagName;
 import org.apache.skywalking.oap.server.storage.plugin.influxdb.TableMetaInfo;
 import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
@@ -62,26 +60,12 @@ public class MetricsDAO implements IMetricsDAO {
     public List<Metrics> multiGet(Model model, List<Metrics> metrics) throws IOException {
         final TableMetaInfo metaInfo = TableMetaInfo.get(model.getName());
 
-        final String queryStr;
-        if (metaInfo.isTrafficTable()) {
-            queryStr = metrics.stream()
-                              .map(m -> (ServiceTraffic) m)
-                              .map(m -> select().raw(ALL_FIELDS)
-                                                .from(client.getDatabase(), model.getName())
-                                                .where(eq(TagName.NAME, m.getName()))
-                                                .and(eq(ID_COLUMN, m.id()))
-                                                .buildQueryString()
-                              ).collect(Collectors.joining(";"));
-        } else {
-            queryStr = metrics.stream()
-                              .map(m ->
-                                       select().raw(ALL_FIELDS)
-                                               .from(client.getDatabase(), model.getName())
-                                               .where(eq(TagName.TIME_BUCKET, String.valueOf(m.getTimeBucket())))
-                                               .and(eq(ID_COLUMN, m.id()))
-                                               .buildQueryString()
-                              ).collect(Collectors.joining(";"));
-        }
+        final String queryStr = metrics.stream()
+                                       .map(m -> select().raw(ALL_FIELDS)
+                                                         .from(client.getDatabase(), model.getName())
+                                                         .where(eq(ID_COLUMN, m.id()))
+                                                         .buildQueryString()
+                                       ).collect(Collectors.joining(";"));
 
         final Query query = new Query(queryStr);
         QueryResult.Series series = client.queryForSingleSeries(query);
