0a246d19de8baf909c424c3f29e7831b39b54668
zhangwei <pknfe@outlook.com>
2021-01-04 00:52:23 +0800

    fix kafka log e2e

--- test/e2e/e2e-test/src/test/java/org/apache/skywalking/e2e/kafka/KafkaLogE2E.java
+++ test/e2e/e2e-test/src/test/java/org/apache/skywalking/e2e/kafka/KafkaLogE2E.java
@@ -17,34 +17,13 @@
 
 package org.apache.skywalking.e2e.kafka;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.DescribeTopicsResult;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.BytesSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.utils.Bytes;
-import org.apache.skywalking.apm.network.common.v3.KeyStringValuePair;
-import org.apache.skywalking.apm.network.logging.v3.LogData;
-import org.apache.skywalking.apm.network.logging.v3.LogDataBody;
-import org.apache.skywalking.apm.network.logging.v3.TextLog;
-import org.apache.skywalking.apm.network.logging.v3.TraceContext;
 import org.apache.skywalking.e2e.annotation.ContainerHostAndPort;
 import org.apache.skywalking.e2e.annotation.DockerCompose;
 import org.apache.skywalking.e2e.base.SkyWalkingE2E;
 import org.apache.skywalking.e2e.base.SkyWalkingTestAdapter;
-import org.apache.skywalking.e2e.base.TrafficController;
 import org.apache.skywalking.e2e.common.HostAndPort;
 import org.apache.skywalking.e2e.log.Log;
 import org.apache.skywalking.e2e.log.LogsMatcher;
@@ -64,14 +43,11 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.DockerComposeContainer;
 
-import static java.util.Objects.nonNull;
 import static org.apache.skywalking.e2e.utils.Yamls.load;
 
 @Slf4j
 @SkyWalkingE2E
 public class KafkaLogE2E extends SkyWalkingTestAdapter {
-
-    private static final String TOPIC = "skywalking-logs";
 
     @SuppressWarnings("unused")
     @DockerCompose({
@@ -89,25 +65,19 @@ public class KafkaLogE2E extends SkyWalkingTestAdapter {
     private HostAndPort oapHostPost;
 
     @SuppressWarnings("unused")
-    @ContainerHostAndPort(name = "broker-a", port = 9092)
-    private HostAndPort kafkaBrokerA;
-
-    private KafkaProducer<String, Bytes> producer;
+    @ContainerHostAndPort(name = "provider_kafka", port = 8080)
+    private HostAndPort serviceHostPort;
 
     @BeforeAll
-    public void setUp() {
+    public void setUp() throws Exception {
         queryClient(swWebappHostPort);
-        initKafkaProducer();
-        generateTraffic();
+        trafficController(serviceHostPort, "/sendLog");
     }
 
     @AfterAll
     public void tearDown() {
         if (trafficController != null) {
             trafficController.stop();
-        }
-        if (producer != null) {
-            producer.close();
         }
     }
 
@@ -160,69 +130,5 @@ public class KafkaLogE2E extends SkyWalkingTestAdapter {
         LOGGER.info("endpoints: {}", endpoints);
 
         load("expected/log/endpoints.yml").as(EndpointsMatcher.class).verify(endpoints);
-    }
-
-    private void initKafkaProducer() {
-        Properties properties = new Properties();
-        properties.setProperty(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerA.toString());
-        producer = new KafkaProducer<>(properties, new StringSerializer(), new BytesSerializer());
-        AdminClient adminClient = AdminClient.create(properties);
-        DescribeTopicsResult topicsResult = adminClient.describeTopics(Collections.singletonList(TOPIC));
-        Set<String> topics = topicsResult.values().entrySet().stream()
-                                         .map(entry -> {
-                                             try {
-                                                 entry.getValue().get(10, TimeUnit.SECONDS);
-                                                 return null;
-                                             } catch (InterruptedException | ExecutionException | TimeoutException e) {
-                                                 LOGGER.error("Get KAFKA topic:" + entry.getKey() + " error", e);
-                                             }
-                                             return entry.getKey();
-                                         }).filter(Objects::nonNull).collect(Collectors.toSet());
-        if (!topics.isEmpty()) {
-            throw new RuntimeException("These topics" + topics + " don't exist.");
-        }
-    }
-
-    private void generateTraffic() {
-        trafficController = TrafficController.builder()
-                                             .sender(this::sendLog)
-                                             .build();
-        trafficController.start();
-    }
-
-    private boolean sendLog() {
-        try {
-            LogData logData = LogData.newBuilder()
-                                     .setService("e2e")
-                                     .setServiceInstance("e2e-instance")
-                                     .setEndpoint("/traffic")
-                                     .setBody(
-                                         LogDataBody.newBuilder()
-                                                    .setText(TextLog.newBuilder().setText("log").build())
-                                                    .build())
-                                     .addTags(
-                                         KeyStringValuePair.newBuilder().setKey("status_code").setValue("200").build())
-                                     .setTraceContext(TraceContext.newBuilder()
-                                                                  .setTraceId("ac81b308-0d66-4c69-a7af-a023a536bd3e")
-                                                                  .setTraceSegmentId(
-                                                                      "6024a2b1fcff48e4a641d69d388bac53.41.16088574455279608")
-                                                                  .setSpanId(0)
-                                                                  .build())
-                                     .build();
-
-            producer.send(
-                new ProducerRecord<>(TOPIC, logData.getService(), Bytes.wrap(logData.toByteArray())),
-                (m, e) -> {
-                    if (nonNull(e)) {
-                        LOGGER.error("Failed to report logs.", e);
-                    }
-                }
-            );
-            return true;
-        } catch (Exception e) {
-            LOGGER.warn(e.getMessage(), e);
-            return false;
-        }
     }
 }
