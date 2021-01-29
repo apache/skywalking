cf8651ac6fe1cef39073e75b024f0bd1c9120a7c
dalekliuhan <dalekliuhan@didiglobal.com>
2021-01-29 10:56:51 +0800

    Fix UT

--- oap-server/server-core/src/test/java/org/apache/skywalking/oap/server/core/CoreModuleTest.java
+++ oap-server/server-core/src/test/java/org/apache/skywalking/oap/server/core/CoreModuleTest.java
@@ -26,6 +26,6 @@ public class CoreModuleTest {
     public void testOpenServiceList() {
         CoreModule coreModule = new CoreModule();
 
-        Assert.assertEquals(32, coreModule.services().length);
+        Assert.assertEquals(33, coreModule.services().length);
     }
 }
