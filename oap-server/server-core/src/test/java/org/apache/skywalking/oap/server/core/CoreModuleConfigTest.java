542738f5f80bba3681781538173006b50aaf3f1c
shijingwen <shijingwen@pmlabs.com.cn>
2021-03-12 11:48:55 +0800

    improve codecov

---
+++ oap-server/server-core/src/test/java/org/apache/skywalking/oap/server/core/CoreModuleConfigTest.java
@@ -0,0 +1,28 @@
+package org.apache.skywalking.oap.server.core;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author shijingwen
+ * @version 1.0
+ * @date 2021/3/12 11:19
+ */
+public class CoreModuleConfigTest {
+    @Test
+    public void testRoleFromNameNormalSituation() {
+        Assert.assertEquals(CoreModuleConfig.Role.Mixed, CoreModuleConfig.Role.fromName("Mixed"));
+        Assert.assertEquals(CoreModuleConfig.Role.Receiver, CoreModuleConfig.Role.fromName("Receiver"));
+        Assert.assertEquals(CoreModuleConfig.Role.Aggregator, CoreModuleConfig.Role.fromName("Aggregator"));
+    }
+    @Test
+    public void testRoleFromNameBlockParameter() {
+        Assert.assertEquals(CoreModuleConfig.Role.Mixed, CoreModuleConfig.Role.fromName(StringUtils.EMPTY));
+        Assert.assertEquals(CoreModuleConfig.Role.Mixed, CoreModuleConfig.Role.fromName(null));
+    }
+    @Test
+    public void testRoleFromNameNotIncludeRole() {
+        Assert.assertEquals(CoreModuleConfig.Role.Mixed, CoreModuleConfig.Role.fromName("a"));
+    }
+}
