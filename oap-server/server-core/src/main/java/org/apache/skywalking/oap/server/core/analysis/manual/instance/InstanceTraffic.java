2025452b61174bf29d98b337b89651c4f60e0c9a
ruixian.wang <ruixian.wang@zhangmen.com>
2021-06-15 13:59:30 +0800

    Polishing.

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/instance/InstanceTraffic.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/instance/InstanceTraffic.java
@@ -69,7 +69,7 @@ public class InstanceTraffic extends Metrics {
     private long lastPingTimestamp;
     @Setter
     @Getter
-    @Column(columnName = PROPERTIES, storageOnly = true, length = 16000)
+    @Column(columnName = PROPERTIES, storageOnly = true)
     private JsonObject properties;
 
     @Override
