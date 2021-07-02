3768cce99a689e644a5ae23619a6094b0f4bdfa4
JaredTan95 <jian.tan@daocloud.io>
2021-07-02 13:19:05 +0800

    fix ui entry point sh.

--- docker/ui/docker-entrypoint.sh
+++ docker/ui/docker-entrypoint.sh
@@ -21,12 +21,11 @@ set -e
 export LOGGING_CONFIG="webapp/logback.xml"
 
 if [[ ! -z "$SW_OAP_ADDRESS" ]]; then
-address_arr=(${SW_OAP_ADDRESS//","/ })
-for i in "${!address_arr[@]}"
-do
-    printf "oap instance $i:  ${address_arr[$i]}\n"
-    JAVA_OPTS="${JAVA_OPTS} -Dspring.cloud.discovery.client.simple.instances.oap-service[$i].uri=${address_arr[$i]}"
-done
+  address_arr=(${SW_OAP_ADDRESS//,/ })
+  for i in "${!address_arr[@]}"
+  do
+      JAVA_OPTS="${JAVA_OPTS} -Dspring.cloud.discovery.client.simple.instances.oap-service[$i].uri=${address_arr[$i]}"
+  done
 fi
 
 exec java  ${JAVA_OPTS} -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -jar webapp/skywalking-webapp.jar "$@"
