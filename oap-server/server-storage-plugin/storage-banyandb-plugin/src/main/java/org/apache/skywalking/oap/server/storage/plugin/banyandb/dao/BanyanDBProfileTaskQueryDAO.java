c48adb9981f31f5e62165c0dbcc27399b5b9af85
Megrez Lu <lujiajing1126@gmail.com>
2021-08-19 18:09:21 +0800

    fix implementation
    Signed-off-by: Megrez Lu <lujiajing1126@gmail.com>

--- oap-server/server-storage-plugin/storage-banyandb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/banyandb/dao/BanyanDBProfileTaskQueryDAO.java
+++ oap-server/server-storage-plugin/storage-banyandb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/banyandb/dao/BanyanDBProfileTaskQueryDAO.java
@@ -18,16 +18,21 @@
 
 package org.apache.skywalking.oap.server.storage.plugin.banyandb.dao;
 
-import org.apache.skywalking.oap.server.core.query.type.ProfileTaskLog;
-import org.apache.skywalking.oap.server.core.storage.profile.IProfileTaskLogQueryDAO;
+import org.apache.skywalking.oap.server.core.query.type.ProfileTask;
+import org.apache.skywalking.oap.server.core.storage.profile.IProfileTaskQueryDAO;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-public class BanyanDBProfileTaskQueryDAO implements IProfileTaskLogQueryDAO {
+public class BanyanDBProfileTaskQueryDAO implements IProfileTaskQueryDAO {
     @Override
-    public List<ProfileTaskLog> getTaskLogList() throws IOException {
+    public List<ProfileTask> getTaskList(String serviceId, String endpointName, Long startTimeBucket, Long endTimeBucket, Integer limit) throws IOException {
         return Collections.emptyList();
+    }
+
+    @Override
+    public ProfileTask getById(String id) throws IOException {
+        return null;
     }
 }
