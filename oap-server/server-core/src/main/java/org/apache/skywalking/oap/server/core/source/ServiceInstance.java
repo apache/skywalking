1f0a49be986d50a27a9caa4283c7cda5207bf712
吴晟 Wu Sheng <wu.sheng@foxmail.com>
2021-10-13 18:42:48 +0800

    Update oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/source/ServiceInstance.java
    Co-authored-by: kezhenxu94 <kezhenxu94@apache.org>

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/source/ServiceInstance.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/source/ServiceInstance.java
@@ -100,6 +100,6 @@ public class ServiceInstance extends Source {
     }
 
     public String getTag(String key) {
-        return originalTags.getOrDefault(key, null);
+        return originalTags.get(key);
     }
 }
