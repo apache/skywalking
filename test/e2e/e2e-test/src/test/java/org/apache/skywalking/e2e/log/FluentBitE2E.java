0579f0b79e43a23562414e0986ad918a181b9192
JaredTan95 <jian.tan@daocloud.io>
2021-10-13 09:19:40 +0800

    update e2e

--- test/e2e/e2e-test/src/test/java/org/apache/skywalking/e2e/log/FluentBitE2E.java
+++ test/e2e/e2e-test/src/test/java/org/apache/skywalking/e2e/log/FluentBitE2E.java
@@ -31,7 +31,7 @@ import org.testcontainers.containers.DockerComposeContainer;
 public class FluentBitE2E extends LogE2E {
     @SuppressWarnings("unused")
     @DockerCompose({
-        "docker/log/docker-compose.yml"
+        "docker/log/docker-compose.fluentbit.yml"
     })
     protected DockerComposeContainer<?> justForSideEffects;
 
