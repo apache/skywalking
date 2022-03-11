9c46d6f8d4d9ef8f5becaa05930fd0a7ac3ad346
Kai <wankai123@foxmail.com>
2022-03-11 10:24:17 +0800

    Fix es normal index couldn't apply mapping and update (#8661)

--- oap-server/server-storage-plugin/storage-elasticsearch-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/elasticsearch/base/StorageEsInstaller.java
+++ oap-server/server-storage-plugin/storage-elasticsearch-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/elasticsearch/base/StorageEsInstaller.java
@@ -68,7 +68,15 @@ public class StorageEsInstaller extends ModelInstaller {
         String tableName = IndexController.INSTANCE.getTableName(model);
         IndexController.LogicIndicesRegister.registerRelation(model.getName(), tableName);
         if (!model.isTimeSeries()) {
-            return esClient.isExistsIndex(tableName);
+            boolean exist = esClient.isExistsIndex(tableName);
+            if (exist) {
+                Mappings historyMapping = esClient.getIndex(tableName)
+                                                  .map(Index::getMappings)
+                                                  .orElseGet(Mappings::new);
+                structures.putStructure(tableName, historyMapping);
+                exist = structures.containsStructure(tableName, createMapping(model));
+            }
+            return exist;
         }
         boolean templateExists = esClient.isExistsTemplate(tableName);
         final Optional<IndexTemplate> template = esClient.getTemplate(tableName);
@@ -102,11 +110,28 @@ public class StorageEsInstaller extends ModelInstaller {
     private void createNormalTable(Model model) throws StorageException {
         ElasticSearchClient esClient = (ElasticSearchClient) client;
         String tableName = IndexController.INSTANCE.getTableName(model);
+        Mappings mapping = createMapping(model);
         if (!esClient.isExistsIndex(tableName)) {
-            boolean isAcknowledged = esClient.createIndex(tableName);
+            Map<String, Object> settings = createSetting(model);
+            boolean isAcknowledged = esClient.createIndex(tableName, mapping, settings);
             log.info("create {} index finished, isAcknowledged: {}", tableName, isAcknowledged);
             if (!isAcknowledged) {
-                throw new StorageException("create " + tableName + " time series index failure, ");
+                throw new StorageException("create " + tableName + " index failure, ");
+            }
+        } else {
+            Mappings historyMapping = esClient.getIndex(tableName)
+                                              .map(Index::getMappings)
+                                              .orElseGet(Mappings::new);
+            structures.putStructure(tableName, mapping);
+            Mappings appendMapping = structures.diffStructure(tableName, historyMapping);
+            if (appendMapping.getProperties() != null && !appendMapping.getProperties().isEmpty()) {
+                boolean isAcknowledged = esClient.updateIndexMapping(tableName, appendMapping);
+                log.info("update {} index finished, isAcknowledged: {}, append mappings: {}", tableName,
+                         isAcknowledged, appendMapping
+                );
+                if (!isAcknowledged) {
+                    throw new StorageException("update " + tableName + " index failure");
+                }
             }
         }
     }
