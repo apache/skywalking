17d62d913a450cacda2d8f1d742f80d5cfa78997
Mrproliu <741550557@qq.com>
2022-05-12 09:44:20 +0800

    Remove unnecessary time range check

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/profiling/ebpf/EBPFProfilingQueryService.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/profiling/ebpf/EBPFProfilingQueryService.java
@@ -123,8 +123,8 @@ public class EBPFProfilingQueryService implements Service {
 
     private EBPFProfilingAnalyzer getProfilingAnalyzer() {
         if (profilingAnalyzer == null) {
-            this.profilingAnalyzer = new EBPFProfilingAnalyzer(moduleManager, config.getMaxDurationOfAnalyzeEBPFProfiling(),
-                    config.getMaxDurationOfQueryEBPFProfilingData(), config.getMaxThreadCountOfQueryEBPFProfilingData());
+            this.profilingAnalyzer = new EBPFProfilingAnalyzer(moduleManager, config.getMaxDurationOfQueryEBPFProfilingData(),
+                    config.getMaxThreadCountOfQueryEBPFProfilingData());
         }
         return profilingAnalyzer;
     }
