ac7cf75b0a9c136586c29b1ccf085fe9b5c5a8a3
Chen Ni <nichen301@sina.com>
2022-06-18 14:36:27 +0800

    fix missadded import

--- oap-server/server-alarm-plugin/src/test/java/org/apache/skywalking/oap/server/core/alarm/provider/RulesReaderTest.java
+++ oap-server/server-alarm-plugin/src/test/java/org/apache/skywalking/oap/server/core/alarm/provider/RulesReaderTest.java
@@ -31,9 +31,9 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.any;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class RulesReaderTest {
