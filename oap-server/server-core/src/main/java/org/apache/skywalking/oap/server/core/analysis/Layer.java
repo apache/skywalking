5c186e2b1150c2fd0f06428cc79bca9a194415f3
yswdqz <945407026@qq.com>
2022-07-29 17:38:07 +0800

    Update Layer.java

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/Layer.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/Layer.java
@@ -116,7 +116,12 @@ public enum Layer {
     /**
      * Kubernetes service
      */
-    K8S_SERVICE(17, true);
+    K8S_SERVICE(17, true),
+
+    /**
+     * Telemetry from MYSQL
+     */
+    MYSQL(18, true);
 
     private final int value;
     /**
