2236a033b650c9b7be355854a4f114678fd5a1fe
yswdqz <945407026@qq.com>
2022-09-07 12:06:09 +0800

    Enhance LAL to support convert LogData to DatabaseSlowStatement

--- oap-server/analyzer/log-analyzer/src/main/java/org/apache/skywalking/oap/log/analyzer/dsl/spec/filter/FilterSpec.java
+++ oap-server/analyzer/log-analyzer/src/main/java/org/apache/skywalking/oap/log/analyzer/dsl/spec/filter/FilterSpec.java
@@ -36,11 +36,18 @@ import org.apache.skywalking.oap.log.analyzer.dsl.spec.parser.JsonParserSpec;
 import org.apache.skywalking.oap.log.analyzer.dsl.spec.parser.TextParserSpec;
 import org.apache.skywalking.oap.log.analyzer.dsl.spec.parser.YamlParserSpec;
 import org.apache.skywalking.oap.log.analyzer.dsl.spec.sink.SinkSpec;
+import org.apache.skywalking.oap.log.analyzer.dsl.spec.slowsql.SlowSqlSpec;
 import org.apache.skywalking.oap.log.analyzer.provider.LogAnalyzerModuleConfig;
 import org.apache.skywalking.oap.log.analyzer.provider.log.listener.LogAnalysisListenerFactory;
 import org.apache.skywalking.oap.log.analyzer.provider.log.listener.RecordAnalysisListener;
 import org.apache.skywalking.oap.log.analyzer.provider.log.listener.TrafficAnalysisListener;
+import org.apache.skywalking.oap.server.analyzer.provider.trace.parser.listener.DatabaseSlowStatementBuilder;
+import org.apache.skywalking.oap.server.core.CoreModule;
+import org.apache.skywalking.oap.server.core.config.NamingControl;
+import org.apache.skywalking.oap.server.core.source.DatabaseSlowStatement;
 import org.apache.skywalking.oap.server.core.source.Log;
+import org.apache.skywalking.oap.server.core.source.ServiceMeta;
+import org.apache.skywalking.oap.server.core.source.SourceReceiver;
 import org.apache.skywalking.oap.server.library.module.ModuleManager;
 import org.apache.skywalking.oap.server.library.module.ModuleStartException;
 import org.slf4j.Logger;
@@ -51,6 +58,8 @@ public class FilterSpec extends AbstractSpec {
 
     private final List<LogAnalysisListenerFactory> factories;
 
+    private final SourceReceiver sourceReceiver;
+
     private final TextParserSpec textParser;
 
     private final JsonParserSpec jsonParser;
@@ -59,9 +68,13 @@ public class FilterSpec extends AbstractSpec {
 
     private final ExtractorSpec extractor;
 
+    private final SlowSqlSpec slowSql;
+
     private final SinkSpec sink;
 
     private final TypeReference<Map<String, Object>> parsedType;
+
+    private final NamingControl namingControl;
 
     public FilterSpec(final ModuleManager moduleManager,
                       final LogAnalyzerModuleConfig moduleConfig) throws ModuleStartException {
@@ -71,8 +84,8 @@ public class FilterSpec extends AbstractSpec {
         };
 
         factories = Arrays.asList(
-            new RecordAnalysisListener.Factory(moduleManager(), moduleConfig()),
-            new TrafficAnalysisListener.Factory(moduleManager(), moduleConfig())
+                new RecordAnalysisListener.Factory(moduleManager(), moduleConfig()),
+                new TrafficAnalysisListener.Factory(moduleManager(), moduleConfig())
         );
 
         textParser = new TextParserSpec(moduleManager(), moduleConfig());
@@ -80,8 +93,14 @@ public class FilterSpec extends AbstractSpec {
         yamlParser = new YamlParserSpec(moduleManager(), moduleConfig());
 
         extractor = new ExtractorSpec(moduleManager(), moduleConfig());
+        slowSql = new SlowSqlSpec(moduleManager(), moduleConfig());
 
         sink = new SinkSpec(moduleManager(), moduleConfig());
+
+        namingControl = moduleManager.find(CoreModule.NAME)
+                .provider()
+                .getService(NamingControl.class);
+        sourceReceiver = moduleManager.find(CoreModule.NAME).provider().getService(SourceReceiver.class);
     }
 
     @SuppressWarnings("unused")
@@ -105,7 +124,7 @@ public class FilterSpec extends AbstractSpec {
         try {
 
             final Map<String, Object> parsed = jsonParser.create().readValue(
-                logData.getBody().getJson().getJson(), parsedType
+                    logData.getBody().getJson().getJson(), parsedType
             );
 
             BINDING.get().parsed(parsed);
@@ -127,7 +146,7 @@ public class FilterSpec extends AbstractSpec {
         final LogData.Builder logData = BINDING.get().log();
         try {
             final Map<String, Object> parsed = yamlParser.create().load(
-                logData.getBody().getYaml().getYaml()
+                    logData.getBody().getYaml().getYaml()
             );
 
             BINDING.get().parsed(parsed);
@@ -145,6 +164,29 @@ public class FilterSpec extends AbstractSpec {
         }
         cl.setDelegate(extractor);
         cl.call();
+    }
+
+    @SuppressWarnings("unused")
+    public void slowSql(@DelegatesTo(SlowSqlSpec.class) final Closure<?> cl) {
+        if (BINDING.get().shouldAbort()) {
+            return;
+        }
+        BINDING.get().databaseSlowStatement(new DatabaseSlowStatementBuilder(namingControl));
+
+        cl.setDelegate(slowSql);
+        cl.call();
+
+        DatabaseSlowStatementBuilder builder = BINDING.get().databaseSlowStatement();
+        ServiceMeta serviceMeta = new ServiceMeta();
+        serviceMeta.setName(namingControl.formatServiceName(builder.getServiceName()));
+        serviceMeta.setLayer(builder.getLayer());
+        serviceMeta.setTimeBucket(builder.getTimeBucket());
+        String entityId = serviceMeta.getEntityId();
+        DatabaseSlowStatement databaseSlowStatement = builder.toDatabaseSlowStatement();
+        databaseSlowStatement.setDatabaseServiceId(entityId);
+        builder.prepare();
+        sourceReceiver.receive(databaseSlowStatement);
+        sourceReceiver.receive(serviceMeta);
     }
 
     @SuppressWarnings("unused")
@@ -169,17 +211,17 @@ public class FilterSpec extends AbstractSpec {
         final Optional<AtomicReference<Log>> container = BINDING.get().logContainer();
         if (container.isPresent()) {
             factories.stream()
-                     .map(LogAnalysisListenerFactory::create)
-                     .filter(it -> it instanceof RecordAnalysisListener)
-                     .map(it -> it.parse(logData, extraLog))
-                     .map(it -> (RecordAnalysisListener) it)
-                     .map(RecordAnalysisListener::getLog)
-                     .findFirst()
-                     .ifPresent(log -> container.get().set(log));
+                    .map(LogAnalysisListenerFactory::create)
+                    .filter(it -> it instanceof RecordAnalysisListener)
+                    .map(it -> it.parse(logData, extraLog))
+                    .map(it -> (RecordAnalysisListener) it)
+                    .map(RecordAnalysisListener::getLog)
+                    .findFirst()
+                    .ifPresent(log -> container.get().set(log));
         } else {
             factories.stream()
-                     .map(LogAnalysisListenerFactory::create)
-                     .forEach(it -> it.parse(logData, extraLog).build());
+                    .map(LogAnalysisListenerFactory::create)
+                    .forEach(it -> it.parse(logData, extraLog).build());
         }
     }
 
