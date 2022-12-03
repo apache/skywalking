25cd5f36fdc7c710b37527aa135f27bd541318e0
Wu Sheng <wu.sheng@foxmail.com>
2022-12-03 18:06:08 +0800

    Fix the test.

--- oap-server/server-core/src/test/java/org/apache/skywalking/oap/server/core/CoreModuleTest.java
+++ oap-server/server-core/src/test/java/org/apache/skywalking/oap/server/core/CoreModuleTest.java
@@ -26,6 +26,6 @@ public class CoreModuleTest {
     public void testOpenServiceList() {
         CoreModule coreModule = new CoreModule();
 
-        Assert.assertEquals(37, coreModule.services().length);
+        Assert.assertEquals(38, coreModule.services().length);
     }
 }
