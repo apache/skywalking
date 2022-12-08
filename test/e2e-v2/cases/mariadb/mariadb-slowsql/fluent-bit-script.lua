6382ef3b708a37734782f0689a1e8f31307beeeb
yswdqz <945407026@qq.com>
2022-12-09 01:26:25 +0800

    optimize code

--- test/e2e-v2/cases/mariadb/mariadb-slowsql/fluent-bit-script.lua
+++ test/e2e-v2/cases/mariadb/mariadb-slowsql/fluent-bit-script.lua
@@ -15,100 +15,125 @@
 -- limitations under the License.
 --
 
+-- The following is an example of a simple mariadb slow log
+-- # Time: 200929 15:42:35
+-- # User@Host: root[root] @  [192.168.1.1]
+-- # Thread_id: 2332  Schema: parkcloud_performancetest  QC_hit: No
+-- # Query_time: 10.93443  Lock_time: 0.000091  Rows_sent: 5  Rows_examined: 9022
+-- # Rows_affected: 0
+-- SET timestamp=1670339460;
 function rewrite_body(tag, timestamp, record)
-  log = record["log"]
-  record["log"] = nil
-  record["date"] = nil
-  record["tags"] = {data={{key="LOG_KIND", value="SLOW_SQL"}}}
-  arr = split(log,"\n")
-  re1 = {}
-  
-  time = string.sub(arr[1], 9)
-  time = string.sub(time,1,19)
-  time = string.gsub(time,"-","");
-  time = string.gsub(time,"T","");
-  time = string.gsub(time,":","");
+  local log = record.log
+  record.log = nil
+  record.tags = { data = { { key = "LOG_KIND", value = "SLOW_SQL" } } }
 
-  y1 = string.sub(time,1,4)
-  m1 = string.sub(time,5,6)
-  d1 = string.sub(time,7,8)
-  h1 = string.sub(time,9,10)
-  min1 = string.sub(time,11,12)
-  s1 = string.sub(time,13,14)
-
-  re1["time"] = os.time() * 1000
-
-  re1["layer"] = "MYSQL"
-  record["layer"] = "MYSQL"
-
-  service = "mysql::db"
-  record["service"]=service
-  re1["service"]= service
-
-  f1,_ = string.find(arr[4],"Lock")
-  query_time = string.sub(arr[4],15,f1-3)
-  local qt,_ = math.modf(query_time*1000)
-  re1["query_time"] = qt
-  re1["statement"] = ""
-
-  re1["id"] = uuid()
-
-  for i=6,#arr,1 do
-      re1["statement"] = re1["statement"]..arr[i]
+  local lines = {}
+  for line in string.gmatch(log, "[^\n]+") do
+    table.insert(lines, line)
   end
-  jsonstr = table2json(re1)
-  record["body"]={json={}}
-  record["body"]["json"]["json"] = jsonstr
+  inner_record = {}
+
+  inner_record.time = os.time() * 1000
+  inner_record.layer = "MYSQL"
+
+  record.layer = "MYSQL"
+
+  -- Here simply set the service name to `mysql::db`, you can also get the part you want from the second line of the log as the service name
+  record.service = "mysql::db"
+  inner_record.service = "mysql::db"
+
+  local query_time = lines[4]:match("%s(%S+)%s+Lock")
+  local qt = math.floor(query_time * 1000)
+  inner_record.query_time = qt
+  inner_record.statement = ""
+
+  inner_record.id = uuid()
+
+  for i = 6, #lines, 1 do
+    inner_record.statement = inner_record.statement .. lines[i]
+  end
+
+  local jsonstr = table2json(inner_record)
+  record.body = { json = { json = jsonstr } }
+
   return 1, timestamp, record
 end
-function split(input, delimiter)
-  input = tostring(input)
-  delimiter = tostring(delimiter)
-  if (delimiter == "") then return false end
-  local pos, arr = 0, {}
-  for st, sp in function() return string.find(input, delimiter, pos, true) end do
-      table.insert(arr, string.sub(input, pos, st - 1))
-      pos = sp + 1
+
+local UUID_TEMPLATE = '%s-%s-%s-%s-%s'
+
+local random_hex = {}
+
+function random_hex.generate(n)
+  local seed = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
+  local s = ""
+  for i = 1, n do
+    s = s .. seed[math.random(1, 16)]
   end
-  table.insert(arr, string.sub(input, pos))
-  return arr
+  return s
 end
 
-function uuid()
-  local seed={'e','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'}
-  local tb={}
-  for i=1,32 do
-      table.insert(tb,seed[math.random(1,16)])
+function uuid(seed)
+  if seed then
+    math.randomseed(seed)
+  else
+    math.randomseed(os.time())
   end
-  local sid=table.concat(tb)
-  return string.format('%s-%s-%s-%s-%s',
-      string.sub(sid,1,8),
-      string.sub(sid,9,12),
-      string.sub(sid,13,16),
-      string.sub(sid,17,20),
-      string.sub(sid,21,32)
+  local sid = string.format(UUID_TEMPLATE,
+          random_hex.generate(4), random_hex.generate(4), random_hex.generate(4),
+          random_hex.generate(4), random_hex.generate(8)
   )
+  return sid
 end
+
 
 function table2json(t)
-local function serialize(tbl)
-  local tmp = {}
-  for k, v in pairs(tbl) do
-    local k_type = type(k)
-    local v_type = type(v)
-    local key = (k_type == "string" and '"' .. k .. '":') or (k_type == "number" and "")
-    local value =
-      (v_type == "table" and serialize(v)) or (v_type == "boolean" and tostring(v)) or
-      (v_type == "string" and '"' .. v .. '"') or
-      (v_type == "number" and v)
-    tmp[#tmp + 1] = key and value and tostring(key) .. tostring(value) or nil
+  local function serialize(tbl, structure)
+    local tmp = {}
+    for k, v in pairs(tbl) do
+      local k_type = type(k)
+      local v_type = type(v)
+      local key
+      if k_type == "string" then
+        key = '"' .. k .. '":'
+      elseif k_type == "number" then
+        key = ""
+        if structure == "array" then
+          key = ""
+        else
+          key = '"' .. k .. '":'
+        end
+      else
+        key = '"' .. tostring(k) .. '":'
+      end
+      local value
+      if v_type == "table" then
+        if next(v) == nil then
+          value = "null"
+        else
+          value = serialize(v, structure)
+        end
+      elseif v_type == "boolean" then
+        value = tostring(v)
+      elseif v_type == "string" then
+        value = '"' .. v .. '"'
+      else
+        value = v
+      end
+      tmp[#tmp + 1] = key and value and tostring(key) .. tostring(value) or nil
+    end
+    if structure == "array" then
+      return "[" .. table.concat(tmp, ",") .. "]"
+    else
+      return "{" .. table.concat(tmp, ",") .. "}"
+    end
   end
-  if table.maxn(tbl) == 0 then
-    return "{" .. table.concat(tmp, ",") .. "}"
+  assert(type(t) == "table")
+  if next(t) == nil then
+    return "null"
+  elseif next(t, next(t)) == nil and type(next(t)) == "number" then
+    return serialize(t, "array")
   else
-    return "[" .. table.concat(tmp, ",") .. "]"
+    return serialize(t, "table")
   end
 end
-assert(type(t) == "table")
-return serialize(t)
-end
+
