89f5cee0dd60913584e072eaaca3c007f5ac9cfd
Wu Sheng <wu.sheng@foxmail.com>
2023-02-07 11:09:02 +0800

    Fix style.

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/query/enumeration/Scope.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/query/enumeration/Scope.java
@@ -20,7 +20,6 @@ package org.apache.skywalking.oap.server.core.query.enumeration;
 
 import lombok.Getter;
 import org.apache.skywalking.oap.server.core.source.DefaultScopeDefine;
-import org.apache.skywalking.oap.server.core.source.ScopeDeclaration;
 
 import static org.apache.skywalking.oap.server.core.source.DefaultScopeDefine.inEndpointCatalog;
 import static org.apache.skywalking.oap.server.core.source.DefaultScopeDefine.inEndpointRelationCatalog;
@@ -32,7 +31,8 @@ import static org.apache.skywalking.oap.server.core.source.DefaultScopeDefine.in
 import static org.apache.skywalking.oap.server.core.source.DefaultScopeDefine.inServiceRelationCatalog;
 
 /**
- * Scope in query stage represents the scope catalog. All scopes with their catalogs are defined in {@link DefaultScopeDefine}.
+ * Scope in query stage represents the scope catalog. All scopes with their catalogs are defined in
+ * {@link DefaultScopeDefine}.
  * Scope IDs could be various due to different OAL/MAL input.
  * Scope catalog provides high dimension classifications for all scopes as a hierarchy structure.
  */
