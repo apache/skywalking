478e494a128f01d61b5044297368542ace09b4d5
wankai123 <wankai123@foxmail.com>
2023-02-20 21:03:41 +0800

    refactor to promQL

---
+++ oap-server/server-query-plugin/promql-plugin/src/test/java/org/apache/skywalking/promql/rt/parser/PromQLMatchVisitorTest.java
@@ -0,0 +1,28 @@
+package org.apache.skywalking.promql.rt.parser;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.apache.skywalking.oap.query.promql.rt.PromQLMatchVisitor;
+import org.apache.skywalking.oap.query.promql.rt.result.MatcherSetResult;
+import org.apache.skywalking.oap.query.promql.rt.result.ParseResultType;
+import org.apache.skywalking.promql.rt.grammar.PromQLLexer;
+import org.apache.skywalking.promql.rt.grammar.PromQLParser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PromQLMatchVisitorTest {
+    @Test
+    public void testMatchVisitor() {
+        PromQLLexer lexer = new PromQLLexer(
+            CharStreams.fromString("service_cpm{service='serviceA', layer='GENERAL'}"));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        PromQLParser parser = new PromQLParser(tokens);
+        ParseTree tree = parser.expression();
+        PromQLMatchVisitor visitor = new PromQLMatchVisitor();
+        MatcherSetResult parseResult = visitor.visit(tree);
+        Assertions.assertEquals(ParseResultType.match, parseResult.getResultType());
+        Assertions.assertEquals("service_cpm", parseResult.getMetricName());
+        Assertions.assertEquals(2, parseResult.getLabelMap().size());
+    }
+}
