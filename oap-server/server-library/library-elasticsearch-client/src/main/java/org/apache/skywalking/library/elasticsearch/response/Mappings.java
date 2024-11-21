dc0ce3a17e1dd7169d08680894147e29ac5c97e0
kezhenxu94 <kezhenxu94@apache.org>
2024-11-21 12:18:45 +0800

    Fix derse

--- oap-server/server-library/library-elasticsearch-client/src/main/java/org/apache/skywalking/library/elasticsearch/response/Mappings.java
+++ oap-server/server-library/library-elasticsearch-client/src/main/java/org/apache/skywalking/library/elasticsearch/response/Mappings.java
@@ -18,6 +18,8 @@
 package org.apache.skywalking.library.elasticsearch.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -43,12 +45,12 @@ public final class Mappings {
 
     @Getter
     @Setter
-    private Map<String, Object> properties;
+    private Map<String, Object> properties = new HashMap<>();
 
     @JsonProperty("_source")
     @Getter
     @Setter
-    private Source source;
+    private Source source = new Source();
 
     @ToString
     @EqualsAndHashCode
