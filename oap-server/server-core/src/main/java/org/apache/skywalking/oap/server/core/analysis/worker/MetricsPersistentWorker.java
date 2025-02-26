2c6b1b3e5aaf16584f7acfe2dc4ab275295d472c
weixiang1862 <652048614@qq.com>
2025-02-26 16:26:40 +0800

    Optimize metrics cache loading when trace latency greater than cache timeout (#13068)

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/worker/MetricsPersistentWorker.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/worker/MetricsPersistentWorker.java
@@ -57,6 +57,7 @@ import org.apache.skywalking.oap.server.telemetry.api.MetricsTag;
 @Slf4j
 public class MetricsPersistentWorker extends PersistenceWorker<Metrics> implements ServerStatusWatcher {
     private final Model model;
+    private final long storageSessionTimeout;
     private final MetricsSessionCache sessionCache;
     private final IMetricsDAO metricsDAO;
     private final Optional<AbstractWorker<Metrics>> nextAlarmWorker;
@@ -107,6 +108,7 @@ public class MetricsPersistentWorker extends PersistenceWorker<Metrics> implemen
                             long storageSessionTimeout, int metricsDataTTL, MetricStreamKind kind) {
         super(moduleDefineHolder, new ReadWriteSafeCache<>(new MergableBufferedData(), new MergableBufferedData()));
         this.model = model;
+        this.storageSessionTimeout = storageSessionTimeout;
         this.sessionCache = new MetricsSessionCache(storageSessionTimeout, supportUpdate);
         this.metricsDAO = metricsDAO;
         this.nextAlarmWorker = Optional.ofNullable(nextAlarmWorker);
@@ -377,19 +379,24 @@ public class MetricsPersistentWorker extends PersistenceWorker<Metrics> implemen
 
         // When
         // (1) the time bucket of the server's latest stability status is provided
-        //     1.1 the OAP has booted successfully
-        //     1.2 the current dimensionality is in minute
-        //     1.3 the OAP cluster is rebalanced due to scaling
-        // (2) the metrics are from the time after the timeOfLatestStabilitySts
-        // (3) the metrics don't exist in the cache
-        // the kernel should NOT try to load it from the database.
+        //     1.1 The OAP has booted successfully
+        //     1.2 The current dimensionality is in minute(timeOfLatestStabilitySts = 0 for other dimensionalities)
+        //     1.3 The OAP cluster is rebalanced due to scaling
+        // (2) the metrics are from the time after the timeOfLatestStabilitySts.
+        // (3) the metrics don't exist in the cache.
+        //The kernel should NOT try to load it from the database.
         //
-        // Notice, about condition (2),
-        // for the specific minute of booted successfully, the metrics are expected to load from database when
-        // it doesn't exist in the cache.
+        // Notice, about the condition (2),
+        // For the specific minutes of metrics before booted, rebalanced(cluster) and expired from cache, 
+        // they are expected to load from the database when don't exist in the cache.
         if (timeOfLatestStabilitySts > 0 &&
-            metrics.getTimeBucket() > timeOfLatestStabilitySts
-            && cached == null) {
+            metrics.getTimeBucket() > timeOfLatestStabilitySts) {
+            final long currentTimeMillis = System.currentTimeMillis();
+            long metricsTimestamp = TimeBucket.getTimestamp(metrics.getTimeBucket());
+            if (currentTimeMillis - metricsTimestamp > storageSessionTimeout) {
+                return null;
+            }
+
             // Return metrics as input to avoid reading from database.
             return metrics;
         }
