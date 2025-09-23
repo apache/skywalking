8aafb80f562d2e286ebf219692b5fc33b6a174c0
peachisai <2581009893@qq.com>
2025-09-23 23:07:52 +0800

    fix

--- oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/database/DatabaseStatementDispatcher.java
+++ oap-server/server-core/src/main/java/org/apache/skywalking/oap/server/core/analysis/manual/database/DatabaseStatementDispatcher.java
@@ -34,7 +34,6 @@ public class DatabaseStatementDispatcher implements SourceDispatcher<DatabaseSlo
         statement.setTimeBucket(source.getTimeBucket());
         statement.setTraceId(source.getTraceId());
         statement.setTimestamp(source.getTimestamp());
-        statement.setTimestamp(source.getTimestamp());
 
         TopNStreamProcessor.getInstance().in(statement);
     }
