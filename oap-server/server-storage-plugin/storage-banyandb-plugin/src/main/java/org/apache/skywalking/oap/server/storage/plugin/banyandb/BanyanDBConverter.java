f99831b26f05cbd3f3ed756c56aaaa27b870b48f
cqhasy <3181940782@qq.com>
2025-10-31 00:44:13 +0800

    fix ci

--- oap-server/server-storage-plugin/storage-banyandb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/banyandb/BanyanDBConverter.java
+++ oap-server/server-storage-plugin/storage-banyandb-plugin/src/main/java/org/apache/skywalking/oap/server/storage/plugin/banyandb/BanyanDBConverter.java
@@ -283,6 +283,11 @@ public class BanyanDBConverter {
             return TagAndValue.stringTagValue(((StorageDataComplexObject<?>) value).toStorageData());
         } else if (Layer.class.equals(clazz)) {
             return TagAndValue.longTagValue(((Integer) value).longValue());
+        } else if (org.apache.skywalking.oap.server.core.profiling.trace.ProfileLanguageType.class.equals(clazz)) {
+            // store enum as int value for compatibility with JDBC/ES
+            return TagAndValue.longTagValue(
+                ((org.apache.skywalking.oap.server.core.profiling.trace.ProfileLanguageType) value).getValue()
+            );
         } else if (JsonObject.class.equals(clazz)) {
             return TagAndValue.stringTagValue((String) value);
         } else if (byte[].class.equals(clazz)) {
