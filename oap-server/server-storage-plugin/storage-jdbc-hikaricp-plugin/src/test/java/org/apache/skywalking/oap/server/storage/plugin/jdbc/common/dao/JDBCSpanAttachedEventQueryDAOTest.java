5e9b9e2c707b0866f878e7df22e1b006ae5ca725
Hyunjin-Jeong <hyun0524e@naver.com>
2026-04-10 21:08:24 +0900

    Add unit tests for 8 more JDBC query DAOs (inline SQL) (#13809)

---
+++ oap-server/server-storage-plugin/storage-jdbc-hikaricp-plugin/src/test/java/org/apache/skywalking/oap/server/storage/plugin/jdbc/common/dao/JDBCSpanAttachedEventQueryDAOTest.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.oap.server.storage.plugin.jdbc.common.dao;
+
+import org.apache.skywalking.oap.server.core.analysis.manual.spanattach.SWSpanAttachedEventRecord;
+import org.apache.skywalking.oap.server.core.analysis.manual.spanattach.SpanAttachedEventRecord;
+import org.apache.skywalking.oap.server.library.client.jdbc.hikaricp.JDBCClient;
+import org.apache.skywalking.oap.server.storage.plugin.jdbc.common.JDBCTableInstaller;
+import org.apache.skywalking.oap.server.storage.plugin.jdbc.common.TableHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class JDBCSpanAttachedEventQueryDAOTest {
+
+    @Mock
+    private JDBCClient jdbcClient;
+    @Mock
+    private TableHelper tableHelper;
+
+    private JDBCSpanAttachedEventQueryDAO dao;
+
+    @BeforeEach
+    void setUp() {
+        dao = new JDBCSpanAttachedEventQueryDAO(jdbcClient, tableHelper);
+    }
+
+    @Test
+    void queryZKSpanAttachedEvents_shouldContainTableColumnAndTraceIdInClause() throws Exception {
+        when(tableHelper.getTablesWithinTTL(SpanAttachedEventRecord.INDEX_NAME))
+            .thenReturn(Collections.singletonList("span_attached_event"));
+
+        final AtomicReference<String> capturedSql = new AtomicReference<>();
+        doAnswer(invocation -> {
+            capturedSql.set(invocation.getArgument(0));
+            return new ArrayList<>();
+        }).when(jdbcClient).executeQuery(anyString(), any(), any(Object[].class));
+
+        dao.queryZKSpanAttachedEvents(Arrays.asList("trace-1", "trace-2"), null);
+
+        assertThat(capturedSql.get()).contains(JDBCTableInstaller.TABLE_COLUMN + " = ?");
+        assertThat(capturedSql.get()).contains(SpanAttachedEventRecord.RELATED_TRACE_ID + " in (?,?)");
+    }
+
+    @Test
+    void queryZKSpanAttachedEvents_shouldContainOrderByStartTime() throws Exception {
+        when(tableHelper.getTablesWithinTTL(SpanAttachedEventRecord.INDEX_NAME))
+            .thenReturn(Collections.singletonList("span_attached_event"));
+
+        final AtomicReference<String> capturedSql = new AtomicReference<>();
+        doAnswer(invocation -> {
+            capturedSql.set(invocation.getArgument(0));
+            return new ArrayList<>();
+        }).when(jdbcClient).executeQuery(anyString(), any(), any(Object[].class));
+
+        dao.queryZKSpanAttachedEvents(Arrays.asList("trace-1"), null);
+
+        assertThat(capturedSql.get()).contains("order by " + SpanAttachedEventRecord.START_TIME_SECOND);
+        assertThat(capturedSql.get()).contains(SpanAttachedEventRecord.START_TIME_NANOS);
+    }
+
+    @Test
+    void querySWSpanAttachedEvents_shouldContainTableColumnAndTraceIdInClause() throws Exception {
+        when(tableHelper.getTablesWithinTTL(SWSpanAttachedEventRecord.INDEX_NAME))
+            .thenReturn(Collections.singletonList("sw_span_attached_event"));
+
+        final AtomicReference<String> capturedSql = new AtomicReference<>();
+        doAnswer(invocation -> {
+            capturedSql.set(invocation.getArgument(0));
+            return new ArrayList<>();
+        }).when(jdbcClient).executeQuery(anyString(), any(), any(Object[].class));
+
+        dao.querySWSpanAttachedEvents(Arrays.asList("trace-a", "trace-b", "trace-c"), null);
+
+        assertThat(capturedSql.get()).contains(JDBCTableInstaller.TABLE_COLUMN + " = ?");
+        assertThat(capturedSql.get()).contains(SWSpanAttachedEventRecord.RELATED_TRACE_ID + " in (?,?,?)");
+    }
+}
